f2ffbe4b8c866bfbb9a32ebb35226e75e0735212
Lechros <0onionion0@gmail.com>
2024-06-24 15:55:18 +0900

    feat: 폰 기물 추가
    폰의 생성자 색상 테스트

---
+++ src/main/piece/Pawn.java
@@ -0,0 +1,15 @@
+package piece;
+
+public class Pawn {
+
+    private final String color;
+
+    public Pawn(final String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+}
