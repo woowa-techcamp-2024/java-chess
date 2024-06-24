47c63a24b3f63fba479a98a5ebd4fc163b1d387f
minseok-oh <minseok.dev@gmail.com>
2024-06-24 19:35:53 +0900

    test: 기본 생성자 test code 추가

--- src/test/java/chess/PawnTest.java
+++ src/test/java/chess/PawnTest.java
@@ -2,6 +2,7 @@ package chess;
 
 import org.junit.jupiter.api.*;
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PawnTest {
     @Test
@@ -14,5 +15,11 @@ public class PawnTest {
     private void verifyPawn(final String color) {
         Pawn pawn = new Pawn(color);
         assertThat(pawn.getColor()).isEqualTo(color);
+    }
+
+    @Test
+    public void create_기본생성자() throws Exception {
+        Pawn pawn = new Pawn();
+        assertEquals("white", pawn.getColor());
     }
 }
