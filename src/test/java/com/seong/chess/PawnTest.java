4a61810e8baaf40943d7dfcdda2cb28da2c4f7e1
hseong3243 <hseong3243@gmail.com>
2024-06-24 16:13:20 +0900

    refactor: 폰 테스트 개선
    폰 생성 시 전달하는 문자열 인수를 변수로 추출함

--- src/test/java/com/seong/chess/PawnTest.java
+++ src/test/java/com/seong/chess/PawnTest.java
@@ -10,8 +10,11 @@ public class PawnTest {
     @Test
     @DisplayName("흰색 폰이 생성되어야 한다")
     public void create() {
-        Pawn whitePawn = new Pawn("white");
-        Pawn blackPawn = new Pawn("black");
+        String whiteColor = "white";
+        String blackColor = "black";
+        
+        Pawn whitePawn = new Pawn(whiteColor);
+        Pawn blackPawn = new Pawn(blackColor);
 
         assertThat(whitePawn.getColor()).isEqualTo("white");
         assertThat(blackPawn.getColor()).isEqualTo("black");
