8e4d38b11661c6f8e17f423b9f1863bc20c832a2
mirageoasis <kimhw0820@naver.com>
2024-06-24 23:52:01 +0900

    feat: 보드 추가 및 테스트 추가

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -1,2 +1,24 @@
-package org.example;public class Board {
+package org.example;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Board {
+
+    private final List<Pawn> pawns = new ArrayList<>();
+
+
+    public void add(Pawn pawn) {
+        pawns.add(pawn);
+    }
+
+    public int size() {
+        return pawns.size();
+    }
+
+    public Pawn findPawn(int id) {
+        // index를 넘어가는 경우 에러를 발생시키는 코드 추가하기
+        return pawns.get(id);
+    }
 }
--- src/test/java/org/example/BoardTest.java
+++ src/test/java/org/example/BoardTest.java
@@ -1,4 +1,25 @@
-import static org.junit.jupiter.api.Assertions.*;
-class BoardTest {
-  
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BoardTest {
+
+    @Test
+    @DisplayName("보드에 폰을 추가할 수 있다")
+    public void create() throws Exception {
+        Board board = new Board();
+
+        Pawn white = new Pawn(Pawn.WHITE_COLOR);
+        board.add(white);
+        assertEquals(1, board.size());
+        assertEquals(white, board.findPawn(0));
+
+        Pawn black = new Pawn(Pawn.BLACK_COLOR);
+        board.add(black);
+        assertEquals(2, board.size());
+        assertEquals(black, board.findPawn(1));
+    }
 }
