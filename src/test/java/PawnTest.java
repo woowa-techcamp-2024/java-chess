233c1835ec8f3d172410c30321b3fa749f56c0ff
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-06-24 18:15:31 +0900

    [Test] Pawn Test
    - Pawn 은 생성시 생성자에 입력된 color에 맞게 생성

---
+++ src/test/java/PawnTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PawnTest {
+    @Test
+    @DisplayName("흰색 폰이 생성되어야 한다")
+    public void create() {
+        Pawn pawn = new Pawn("white");
+        assertEquals("white",pawn.getColor());
+    }
+}
