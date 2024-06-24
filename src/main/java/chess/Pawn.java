962ab18692c23dd558b20c0824854062be889e9b
minseok-oh <minseok.dev@gmail.com>
2024-06-24 19:36:30 +0900

    feat: pawn 기본 생성자 코드 추가

--- src/main/java/chess/Pawn.java
+++ src/main/java/chess/Pawn.java
@@ -3,6 +3,8 @@ package chess;
 public class Pawn {
     private final String color;
 
+    public Pawn() { this.color = Color.WHITE; }
+
     public Pawn(final String color) {
         this.color = color;
     }
