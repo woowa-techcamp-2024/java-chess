ade91d61e8984a5f1456562699542c98860c19b5
Lechros <0onionion0@gmail.com>
2024-06-24 16:07:57 +0900

    test: 폰 테스트 리팩토링
    - 색상 문자열 상수로 저장
    - 중복 코드 분리

--- src/test/piece/PawnTest.java
+++ src/test/piece/PawnTest.java
@@ -7,11 +7,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class PawnTest {
 
+    private Pawn pawn;
+
     @Test
-    @DisplayName("흰색 폰이 생성되어야 한다")
+    @DisplayName("생성자에 전달된 색의 폰이 생성되어야 한다")
     public void create() {
-        Pawn pawn = new Pawn("white");
-        assertThat(pawn.getColor()).isEqualTo("white");
+        final String WHITE = "white";
+        final String BLACK = "black";
+
+        pawn = new Pawn(WHITE);
+        verifyPawn(WHITE);
+
+        pawn = new Pawn(BLACK);
+        verifyPawn(BLACK);
+    }
+
+    private void verifyPawn(final String color) {
+        assertThat(pawn.getColor()).isEqualTo(color);
     }
 
 }
