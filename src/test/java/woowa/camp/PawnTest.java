5a8455bb9b2cab4efb2d45014662126ac5d93c77
june-777 <wlwhswnsrl96@gmail.com>
2024-06-24 19:10:46 +0900

    test: Pawn 흰색 테스트

---
+++ src/test/java/woowa/camp/PawnTest.java
@@ -0,0 +1,16 @@
+package woowa.camp;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PawnTest {
+
+    @Test
+    @DisplayName("흰색 폰이 생성되어야 한다")
+    void create() {
+        Pawn pawn = new Pawn("white");
+        Assertions.assertThat(pawn.getColor()).isEqualTo("white");
+    }
+
+}
