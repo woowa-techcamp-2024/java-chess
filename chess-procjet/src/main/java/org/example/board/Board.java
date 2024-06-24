7c2ffd421222a52f3001ff04495234eea396a2a3
bellringstar <bellringstar@gmail.com>
2024-06-24 20:24:48 +0900

    feat: Board 객체 생성 board 내에 pawn을 추가할 수 있는 메서드, 총 pawn의 개수를 알 수 있는 메서드, pawn을 찾아서 반환하는 메서드 생성

--- chess-procjet/src/main/java/org/example/board/Board.java
+++ chess-procjet/src/main/java/org/example/board/Board.java
@@ -1,2 +1,29 @@
-package org.example.board;public class Board {
+package org.example.board;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.example.piece.Pawn;
+
+public class Board {
+
+    private final int MAX_PAWNS = 32;
+    private final List<Pawn> pawns = new ArrayList<>();
+
+    public void add(Pawn pawn) {
+        if (pawns.size() > MAX_PAWNS) {
+            throw new IllegalArgumentException("한 보드판에는 최대 32개의 말이 존재할 수 있습니다.");
+        }
+        pawns.add(pawn);
+    }
+
+    public int size() {
+        return pawns.size();
+    }
+
+    public Pawn findPawn(int i) {
+        if (i < 0 || i >= pawns.size()) {
+            throw new IllegalArgumentException("존재하지 않는 말입니다.");
+        }
+        return pawns.get(i);
+    }
 }
