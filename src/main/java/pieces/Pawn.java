aeef2cb89416fd2d0e454cecb84a132ef6a9085d
kimhyun5u <22kimhyun5u@gmail.com>
2024-06-24 18:41:15 +0900

    refactor: 색깔을 상수로 - 반복되는 색상을 상수로 만들어 구현함

--- src/main/java/pieces/Pawn.java
+++ src/main/java/pieces/Pawn.java
@@ -1,6 +1,9 @@
 package pieces;
 
 public class Pawn {
+    public static String WHITE_COLOR = "white";
+    public static String BLACK_COLOR = "black";
+
     private String color;
 
     public Pawn() {
