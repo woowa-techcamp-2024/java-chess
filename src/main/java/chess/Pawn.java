106de72ecbbc8d73060a26f5e657e15a5ad3377e
miiiinju1 <gms07073@ynu.ac.kr>
2024-06-24 18:37:33 +0900

    feat: Pawn 클래스 구현

---
+++ src/main/java/chess/Pawn.java
@@ -0,0 +1,13 @@
+package chess;
+
+public class Pawn {
+    String color;
+
+    public String getColor() {
+        return color;
+    }
+
+    public Pawn(String color) {
+        this.color = color;
+    }
+}
