be3f269f136e9674616393519964ff225213ae32
huiseung <whtjddn75@gmail.com>
2024-06-24 17:00:08 +0900

    [feat] Board 구현 - add, getSize, findPawn 메서드 구현

---
+++ chess/src/main/java/chess/Board.java
@@ -0,0 +1,24 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Board {
+    private List<Pawn> pawns;
+
+    public Board() {
+        this.pawns = new ArrayList<>();
+    }
+
+    public void add(Pawn pawn) {
+        pawns.add(pawn);
+    }
+
+    public int getSize() {
+        return pawns.size();
+    }
+
+    public Pawn findPawn(int idx) {
+        return pawns.get(idx);
+    }
+}
