ded95970ef982d3fb90fe2eee576ee0bb6c9a208
seminchoi <choicco89@gmail.com>
2024-06-24 17:05:17 +0900

    feat: Pawn 클래스 생성
    - 멤버변수, 생성자, getter 작성

---
+++ src/main/java/wootecamp/chess/Pawn.java
@@ -0,0 +1,13 @@
+package wootecamp.chess;
+
+public class Pawn {
+    private String color;
+
+    public Pawn(final String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
