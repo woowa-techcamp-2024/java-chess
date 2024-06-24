1354979c3c53a8f99726490bb426c478498514aa
hellomatia <shuaigejp80@gmail.com>
2024-06-24 16:46:48 +0900

    test: 검은 색 폰 생성 검증하는 테스트 코드 추가

--- src/test/java/com/woowatechcamp/PawnTest.java
+++ src/test/java/com/woowatechcamp/PawnTest.java
@@ -5,9 +5,12 @@ import static org.assertj.core.api.Assertions.*;
 public class PawnTest {
 
     @Test
-    @DisplayName("흰색 폰이 생성되어야 한다")
+    @DisplayName("흰색, 검은색 폰이 생성되어야 한다")
     public void create() {
-        Pawn pawn = new Pawn("white");
-        assertThat(pawn.getColor()).isEqualTo("white");
+        Pawn whitePawn = new Pawn("white");
+        assertThat(whitePawn.getColor()).isEqualTo("white");
+
+        Pawn blackPawn = new Pawn("black");
+        assertThat(blackPawn.getColor()).isEqualTo("black");
     }
 }
