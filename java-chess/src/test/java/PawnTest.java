d8d95bcab92d41b547cfa1887357bd594cf299c0
KyuWon  Kim <kyuwon0718@gmail.com>
2024-06-24 16:22:31 +0900

    refactor: Pawn 생성 테스트 중복 코드 리팩토링

--- java-chess/src/test/java/PawnTest.java
+++ java-chess/src/test/java/PawnTest.java
@@ -1,18 +1,28 @@
 import org.example.character.Pawn;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class PawnTest {
-    @Test
-    @DisplayName("흰색 폰이 생성되어야 한다")
-    public void create() {
-        String white = "white";
-        String black = "black";
+    private static final String white = "white";
+    private static final String black = "black";
 
-        Pawn whitePawn = new Pawn(white);
-        Pawn blackPawn = new Pawn(black);
+    @ParameterizedTest
+    @ValueSource(strings = {white, black})
+    @DisplayName("폰이 생성되는 색깔에 맞게 생성되어야 한다")
+    public void create(String colors) {
+        verifyPawn(colors);
+    }
 
-        assertThat(whitePawn.getColor()).isEqualTo(white);
-        assertThat(blackPawn.getColor()).isEqualTo(black);
+    public void verifyPawn(String color) {
+        Pawn pawn = new Pawn(color);
+
+        if (color.equals(white)) {
+            assertThat(pawn.getColor()).isEqualTo(white);
+        } else {
+            assertThat(pawn.getColor()).isEqualTo(black);
+        }
     }
 }
