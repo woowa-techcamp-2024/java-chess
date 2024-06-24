d811baff76540e7f276604f2de6b0c2e274f0e0d
kimhyun5u <22kimhyun5u@gmail.com>
2024-06-24 16:15:31 +0900

    feat: Pawn 클래스 구현 - Pawn 클래스 구현 - 검은색 폰 생성

---
+++ src/main/java/pieces/Pawn.java
@@ -0,0 +1,14 @@
+package pieces;
+
+public class Pawn {
+    // TODO: color String 이 맞는지 고민
+    private String color;
+
+    public Pawn(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
