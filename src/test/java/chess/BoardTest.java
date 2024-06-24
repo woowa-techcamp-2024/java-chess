1b4572d4ac3d404f7a3c7e6b06e0ab0489ac78b8
miiiinju1 <gms07073@ynu.ac.kr>
2024-06-24 19:28:18 +0900

    refactor: BeforeEach 어노테이션을 활용하여 Board와 Pawn 생성 로직을 Setup 메서드로 추출한다.

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -1,5 +1,6 @@
 package chess;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import chess.pieces.Pawn;
@@ -8,17 +9,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BoardTest {
 
+    public Board board;
+    public Pawn white;
+    public Pawn black;
+
+    @BeforeEach
+    void setUp() {
+        board = new Board();
+        white = new Pawn(Pawn.WHITE_COLOR);
+        black = new Pawn(Pawn.BLACK_COLOR);
+    }
+
     @Test
     @DisplayName("보드에 폰을 추가할 수 있다")
     public void create() throws Exception {
-        Board board = new Board();
-
-        Pawn white = new Pawn(Pawn.WHITE_COLOR);
         board.add(white);
         assertEquals(1, board.size());
         assertEquals(white, board.findPawn(0));
 
-        Pawn black = new Pawn(Pawn.BLACK_COLOR);
         board.add(black);
         assertEquals(2, board.size());
         assertEquals(black, board.findPawn(1));
