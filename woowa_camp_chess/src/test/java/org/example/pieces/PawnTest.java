eaec0eda0bb5a1a343bf2dcb5257c045764d2e52
woowatech05 <woowatech05@woowatech05ui-MacBookPro.local>
2024-06-24 18:32:10 +0900

    test : 테스트 코드 리팩토링

--- woowa_camp_chess/src/test/java/org/example/pieces/PawnTest.java
+++ woowa_camp_chess/src/test/java/org/example/pieces/PawnTest.java
@@ -8,7 +8,14 @@ class PawnTest {
     @Test
     @DisplayName("흰색 폰이 생성되어야 한다")
     public void create() {
-        Pawn pawn = new Pawn("white");
-        assertThat(pawn.getColor()).isEqualTo("white");
+        final String BLACK = "black";
+        final String WHITE = "white";
+        verifyPawn(BLACK);
+        verifyPawn(WHITE);
+    }
+
+    private static void verifyPawn(String color) {
+        Pawn pawn1 = new Pawn(color);
+        assertThat(pawn1.getColor()).isEqualTo(color);
     }
 }
