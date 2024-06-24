e3ce9ebe64fdeab1d582a10f3fbdcb392ee07cd5
hseong3243 <hseong3243@gmail.com>
2024-06-24 16:17:48 +0900

    refactor: 폰 색깔 검증 코드 중복 제거

--- src/test/java/com/seong/chess/PawnTest.java
+++ src/test/java/com/seong/chess/PawnTest.java
@@ -2,6 +2,8 @@ package com.seong.chess;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,13 +12,17 @@ public class PawnTest {
     @Test
     @DisplayName("흰색 폰이 생성되어야 한다")
     public void create() {
-        String whiteColor = "white";
-        String blackColor = "black";
-        
-        Pawn whitePawn = new Pawn(whiteColor);
-        Pawn blackPawn = new Pawn(blackColor);
+        Pawn pawn = new Pawn("white");
+        assertThat(pawn.getColor()).isEqualTo("white");
+    }
 
-        assertThat(whitePawn.getColor()).isEqualTo("white");
-        assertThat(blackPawn.getColor()).isEqualTo("black");
+    @ParameterizedTest
+    @CsvSource({
+            "black", "white"
+    })
+    @DisplayName("다양한 색을 가진 폰이 생성되어야 한다.")
+    public void verifyPawn(final String color) {
+        Pawn pawn = new Pawn(color);
+        assertThat(pawn.getColor()).isEqualTo(color);
     }
 }
