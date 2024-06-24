3b4db76a21a9cde7b89f3f9300e5f3f3f2522b9c
miiiinju1 <gms07073@ynu.ac.kr>
2024-06-24 19:05:07 +0900

    test: create_기본생성자에 대한 displayname 추가

--- src/test/java/chess/PawnTest.java
+++ src/test/java/chess/PawnTest.java
@@ -9,6 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PawnTest {
 
     @Test
+    @DisplayName("기본 생성자를 이용하면 흰색 폰이 생성되어야 한다")
     public void create_기본생성자() throws Exception {
         Pawn pawn = new Pawn();
         assertEquals("white", pawn.getColor());
