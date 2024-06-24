7471194b7dbff6f14124e4d39ec5421873d3eb2c
woowatech01 <woowatech01@woowatecBookPro.lan>
2024-06-24 22:16:15 +0900

    feat: 검은색 흰색 폰 테스트 완료

---
+++ src/main/java/org/example/Pawn.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Pawn {
+
+    private String color;
+
+    public Pawn(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+}
