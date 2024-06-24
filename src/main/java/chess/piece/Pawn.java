f328aeccb53b5d94b442a97bbce1476d7cf75962
HyeonJun0530 <hyjn053@gmail.com>
2024-06-24 18:58:50 +0900

    feat: Pawn 클래스 구현
    - color 필드 추가 및 생성자 구현
    - color getter 메서드 구현

---
+++ src/main/java/chess/piece/Pawn.java
@@ -0,0 +1,14 @@
+package chess.piece;
+
+public class Pawn {
+
+    private final String color;
+
+    public Pawn(final String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
