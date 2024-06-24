41d32d67776ffdbbdb22305e5d37abc98c21d638
hellomatia <shuaigejp80@gmail.com>
2024-06-24 16:52:39 +0900

    refactor: "white", "black" 문자 중복 제거를 위해 local variable 추가, 검증하는 코드 중복 제거

--- src/test/java/com/woowatechcamp/PawnTest.java
+++ src/test/java/com/woowatechcamp/PawnTest.java
@@ -1,5 +1,7 @@
 package com.woowatechcamp;import org.junit.jupiter.api.*;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class PawnTest {
@@ -7,10 +9,10 @@ public class PawnTest {
     @Test
     @DisplayName("흰색, 검은색 폰이 생성되어야 한다")
     public void create() {
-        Pawn whitePawn = new Pawn("white");
-        assertThat(whitePawn.getColor()).isEqualTo("white");
-
-        Pawn blackPawn = new Pawn("black");
-        assertThat(blackPawn.getColor()).isEqualTo("black");
+        List<String> colors = List.of("white", "black");
+        for (String color : colors) {
+            Pawn pawn = new Pawn(color);
+            assertThat(pawn.getColor()).isEqualTo(color);
+        }
     }
 }
