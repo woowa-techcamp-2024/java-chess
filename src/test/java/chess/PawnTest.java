8c71e26c6b38c0835390a1d77c1f20b5681d2e1b
miiiinju1 <gms07073@ynu.ac.kr>
2024-06-24 19:01:22 +0900

    test: PawnTest에 대해 색이 없는 Pawn을 생성하는 실패 테스트 추가

--- src/test/java/chess/PawnTest.java
+++ src/test/java/chess/PawnTest.java
@@ -4,8 +4,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PawnTest {
+
+    @Test
+    public void create_기본생성자() throws Exception {
+        Pawn pawn = new Pawn();
+        assertEquals("white", pawn.getColor());
+    }
 
     @Test
     @DisplayName("흰색 폰이 생성되어야 한다")
