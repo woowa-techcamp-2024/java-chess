b052fc1a0feba4acce465f1cdd6c9335c4bbe5d5
seminchoi <choicco89@gmail.com>
2024-06-24 17:34:25 +0900

    test: 중복되는 문자열 지역변수로 추출
    - black, white 문자열 지역변수 추출

--- src/test/java/wootecamp/chess/PawnTest.java
+++ src/test/java/wootecamp/chess/PawnTest.java
@@ -10,10 +10,13 @@ public class PawnTest {
     @Test
     @DisplayName("생성자 파라미터에 색깔을 전달해서 폰을 생성한다.")
     void create() {
-        Pawn whitePawn = new Pawn("white");
-        Pawn blackPawn = new Pawn("black");
+        final String WHITE_COLOR = "white";
+        final String BLACK_COLOR = "black";
 
-        assertThat(whitePawn.getColor()).isEqualTo("white");
-        assertThat(blackPawn.getColor()).isEqualTo("black");
+        final Pawn whitePawn = new Pawn(WHITE_COLOR);
+        final Pawn blackPawn = new Pawn(BLACK_COLOR);
+
+        assertThat(whitePawn.getColor()).isEqualTo(WHITE_COLOR);
+        assertThat(blackPawn.getColor()).isEqualTo(BLACK_COLOR);
     }
 }
