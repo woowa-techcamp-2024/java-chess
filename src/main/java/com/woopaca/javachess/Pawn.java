7b887a9f0810fb1eff606a93c5cfc0ee3c0831ce
woowatech03 <woowatech03@woowatech--MacBook-Pro.local>
2024-06-24 18:49:41 +0900

    feat: Pawn 생성자 및 color 조회 메서드 구현

--- src/main/java/com/woopaca/javachess/Pawn.java
+++ src/main/java/com/woopaca/javachess/Pawn.java
@@ -5,10 +5,11 @@ public class Pawn {
     private String color;
 
     public Pawn(String color) {
+        this.color = color;
     }
 
     public String getColor() {
-        return null;
+        return color;
     }
 
 }
