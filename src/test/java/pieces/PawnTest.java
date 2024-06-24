6dd8b4a5a042fe5ec062dd9ca88475b72474168d
kimhyun5u <22kimhyun5u@gmail.com>
2024-06-24 16:41:49 +0900

    feat: Pawn에 기본 생성자 추가 - 색이 없을 때 white로 생성하도록 수정

--- src/test/java/pieces/PawnTest.java
+++ src/test/java/pieces/PawnTest.java
@@ -1,7 +1,9 @@
 package pieces;
 
 import org.junit.jupiter.api.*;
+
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PawnTest {
 
@@ -10,6 +12,12 @@ public class PawnTest {
     public void create() {
         verifyPawn("black");
         verifyPawn("white");
+    }
+
+    @Test
+    public void create_기본생성자() throws Exception {
+        Pawn pawn = new Pawn();
+        assertEquals("white", pawn.getColor());
     }
 
     public void verifyPawn(final String color) {
