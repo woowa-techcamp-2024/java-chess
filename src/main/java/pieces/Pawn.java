3666bf2f7992b987704d51528421d57fabbd820d
KyungMin Lee <hello.tidavid@gmail.com>
2024-06-24 16:15:49 +0900

    `Pawn`, `PawnTest` 리팩토링 - `@ParameterizedTest` 활용 테스트 중복 제거 - `.verifyPawn()` 구현을 통한 Pawn 색상 검증 중복 방지 리팩토링

--- src/main/java/pieces/Pawn.java
+++ src/main/java/pieces/Pawn.java
@@ -5,10 +5,21 @@ public class Pawn implements Piece {
     private final String color;
 
     public Pawn(String color) {
-        this.color = color;
+        this.color = verifyColor(color);
+    }
+
+    private static String verifyColor(String color) {
+        if (!(color.equals("white") || color.equals("black"))) {
+            throw new IllegalArgumentException("Invalid Color Input");
+        }
+        return color;
     }
 
     public String getColor() {
         return color;
+    }
+
+    public boolean verifyPawn(final String color) {
+        return this.color.equals(color);
     }
 }
--- src/test/java/pieces/PawnTest.java
+++ src/test/java/pieces/PawnTest.java
@@ -1,24 +1,46 @@
 package pieces;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PawnTest {
 
-    @Test
-    @DisplayName("흰색 폰이 생성되어야 한다")
-    public void create() {
-        Pawn pawn = new Pawn("white");
-        assertThat(pawn.getColor()).isEqualTo("white");
+    @MethodSource
+    @ParameterizedTest
+    @DisplayName("폰이 생성되어야 한다")
+    void create(String color) {
+        Pawn pawn = new Pawn(color);
+        assertThat(pawn.verifyPawn(color)).isTrue();
     }
 
-    @Test
-    @DisplayName("검은색 폰이 생성되어야 한다.")
-    public void createBlackPawn() {
-        var pawn = new Pawn("black");
-        assertThat(pawn.getColor()).isEqualTo("black");
+    @MethodSource
+    @ParameterizedTest
+    @DisplayName("잘못된 색상이 입력된 경우 폰은 생성되지 말아야 한다.")
+    void createFailed(String wrongColor) {
+        assertThatThrownBy(() -> new Pawn(wrongColor))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Invalid Color Input");
     }
 
+    private static Stream<Arguments> create() {
+        return Stream.of(
+                Arguments.of("white"),
+                Arguments.of("black")
+        );
+    }
+
+    private static Stream<Arguments> createFailed() {
+        return Stream.of(
+                Arguments.of("whtte"),
+                Arguments.of(""),
+                Arguments.of("blank")
+        );
+    }
 }
