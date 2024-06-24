136e0f2c76386acc8300dc354ebca52b6a1025e2
minnim1010 <pms1010123@daum.net>
2024-06-24 19:17:39 +0900

    refactor: 반복 사용하는 테스트 어노테이션들을 묶은 어노테이션 정의
    [@SuppressWarnings("NonAsciiCharacters")
    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)을 묶어 @TestConfiguration으로 정의]

--- src/test/java/com/wootecam/PawnTest.java
+++ src/test/java/com/wootecam/PawnTest.java
@@ -2,15 +2,13 @@ package com.wootecam;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.wootecam.util.annotation.TestConfiguration;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
-@SuppressWarnings("NonAsciiCharacters")
-@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@TestConfiguration
 @DisplayName("폰 테스트")
 public class PawnTest {
 
@@ -18,12 +16,14 @@ public class PawnTest {
     @ParameterizedTest(name = "{0} 색 폰이 생성되어야 한다")
     public void 해당_색을_가진_폰이_생성되어야_한다(Color color) {
         var pawn = new Pawn(color);
+
         assertThat(pawn.getColor()).isEqualTo(color);
     }
 
     @Test
     public void 색깔이_주어지지_않았다면_하얀색_폰이_생성되어야_한다() {
         var pawn = new Pawn();
+
         assertThat(pawn.getColor()).isEqualTo(Color.WHITE);
     }
 }
---
+++ src/test/java/com/wootecam/util/annotation/TestConfiguration.java
@@ -0,0 +1,15 @@
+package com.wootecam.util.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public @interface TestConfiguration {
+}
