0e518e507103cc4fd49c11866670631cc1d7c60c
miiiinju1 <gms07073@ynu.ac.kr>
2024-06-24 18:38:45 +0900

    test: 검은색 폰 생성 테스트 추가

--- src/test/java/chess/PawnTest.java
+++ src/test/java/chess/PawnTest.java
@@ -10,7 +10,11 @@ public class PawnTest {
     @Test
     @DisplayName("흰색 폰이 생성되어야 한다")
     public void create() {
-        Pawn pawn = new Pawn("white");
-        assertThat(pawn.getColor()).isEqualTo("white");
+        Pawn whitePawn = new Pawn("white");
+        assertThat(whitePawn.getColor()).isEqualTo("white");
+
+        Pawn blackPawn = new Pawn("black");
+        assertThat(blackPawn.getColor()).isEqualTo("black");
     }
+
 }
