f95b4f42dec2a6660e0cb10d350414012e306a8c
seminchoi <choicco89@gmail.com>
2024-06-24 17:23:15 +0900

    test: 생성 테스트 케이스 보강
    - 검은색으로 생성했을 때의 경우 추가

--- src/test/java/wootecamp/chess/PawnTest.java
+++ src/test/java/wootecamp/chess/PawnTest.java
@@ -1,15 +1,19 @@
 package wootecamp.chess;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class PawnTest {
     @Test
-    @DisplayName("흰색 폰이 생성되어야 한다.")
+    @DisplayName("생성자 파라미터에 색깔을 전달해서 폰을 생성한다.")
     void create() {
-        Pawn pawn = new Pawn("white");
-        Assertions.assertThat(pawn.getColor()).isEqualTo("white");
+        Pawn whitePawn = new Pawn("white");
+        Pawn blackPawn = new Pawn("black");
+
+        assertThat(whitePawn.getColor()).isEqualTo("white");
+        assertThat(blackPawn.getColor()).isEqualTo("black");
     }
 }
