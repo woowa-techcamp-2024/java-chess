7aa0333a6be281df61802681c0e1caef20602a1a
mirageoasis <kimhw0820@naver.com>
2024-06-25 00:03:37 +0900

    test: 테스트 수정 및 BeforeAll과 BeforeEach로 반복제거

--- src/test/java/org/example/chess/BoardTest.java
+++ src/test/java/org/example/chess/BoardTest.java
@@ -3,24 +3,51 @@ package org.example.chess;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.example.pieces.Pawn;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BoardTest {
 
+    private Board board;
+    private static Pawn white;
+    private static Pawn black;
+
+    @BeforeAll
+    public static void setUpPawn() {
+        white = new Pawn(Pawn.WHITE_COLOR);
+        black = new Pawn(Pawn.BLACK_COLOR);
+    }
+
+    @BeforeEach
+    public void setUpBoard() {
+        board = new Board();
+    }
+
     @Test
     @DisplayName("보드에 폰을 추가할 수 있다")
     public void create() throws Exception {
-        Board board = new Board();
-
-        Pawn white = new Pawn(Pawn.WHITE_COLOR);
         board.add(white);
         assertEquals(1, board.size());
         assertEquals(white, board.findPawn(0));
 
-        Pawn black = new Pawn(Pawn.BLACK_COLOR);
         board.add(black);
         assertEquals(2, board.size());
         assertEquals(black, board.findPawn(1));
+    }
+
+    @Test
+    @DisplayName("잘못된 index로 폰을 찾을 경우 에러를 발생시킨다")
+    public void findPawn() throws Exception {
+        Board board = new Board();
+        Pawn white = new Pawn(Pawn.WHITE_COLOR);
+        board.add(white);
+        assertEquals(white, board.findPawn(0));
+        try {
+            board.findPawn(1);
+        } catch (Exception e) {
+            assertEquals("IndexOutOfBoundsException", e.getClass().getSimpleName());
+        }
     }
 }
