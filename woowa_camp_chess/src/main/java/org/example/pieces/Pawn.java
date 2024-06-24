84749b1ff78ea4bb3b1f88790c9349ab068990ea
woowatech05 <woowatech05@woowatech05ui-MacBookPro.local>
2024-06-24 18:55:07 +0900

    feat : Pawn 기본 생성자 추가

--- woowa_camp_chess/src/main/java/org/example/pieces/Pawn.java
+++ woowa_camp_chess/src/main/java/org/example/pieces/Pawn.java
@@ -1,11 +1,16 @@
 package org.example.pieces;
 
 public class Pawn {
+    private static final String WHITE_COLOR = "white";
 
     String color;
 
     public Pawn(String color) {
         this.color = color;
+    }
+
+    public Pawn() {
+        this.color = WHITE_COLOR;
     }
 
     public String getColor() {
