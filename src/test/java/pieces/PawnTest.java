1d51abefd136468d128563e2f52c48c4cfa03a1c
KyungMin Lee <hello.tidavid@gmail.com>
2024-06-24 16:49:31 +0900

    색상 Enum 생성 및 리팩토링 - `Color` Enum 구현 - `Pawn`에 `Color` Enum 적용을 통한 생성 로직 최적화 - 불필요한 검증 로직 제거

--- src/test/java/pieces/PawnTest.java
+++ src/test/java/pieces/PawnTest.java
@@ -1,5 +1,7 @@
 package pieces;
 
+import chess.pieces.Pawn;
+import chess.pieces.enums.Color;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -8,39 +10,22 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PawnTest {
 
     @MethodSource
     @ParameterizedTest
     @DisplayName("폰이 생성되어야 한다")
-    void create(String color) {
+    void create(Color color) {
         Pawn pawn = new Pawn(color);
         assertThat(pawn.verifyPawn(color)).isTrue();
     }
 
-    @MethodSource
-    @ParameterizedTest
-    @DisplayName("잘못된 색상이 입력된 경우 폰은 생성되지 말아야 한다.")
-    void createFailed(String wrongColor) {
-        assertThatThrownBy(() -> new Pawn(wrongColor))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("Invalid Color Input");
-    }
-
     private static Stream<Arguments> create() {
         return Stream.of(
-                Arguments.of("white"),
-                Arguments.of("black")
+                Arguments.of(Color.WHITE),
+                Arguments.of(Color.BLACK)
         );
     }
 
-    private static Stream<Arguments> createFailed() {
-        return Stream.of(
-                Arguments.of("whtte"),
-                Arguments.of(""),
-                Arguments.of("blank")
-        );
-    }
 }
