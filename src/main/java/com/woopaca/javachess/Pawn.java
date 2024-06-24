d3b8b18288557d41b0c25528b4e5909e6678a5b5
woowatech03 <woowatech03@woowatech--MacBook-Pro.local>
2024-06-24 18:48:29 +0900

    test: 실패하는 Pawn 생성 테스트 작성

---
+++ src/main/java/com/woopaca/javachess/Pawn.java
@@ -0,0 +1,14 @@
+package com.woopaca.javachess;
+
+public class Pawn {
+
+    private String color;
+
+    public Pawn(String color) {
+    }
+
+    public String getColor() {
+        return null;
+    }
+
+}
---
+++ src/test/java/com/woopaca/javachess/PawnTest.java
@@ -0,0 +1,17 @@
+package com.woopaca.javachess;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PawnTest {
+
+    @DisplayName("흰색 폰이 생성되어야 한다.")
+    @Test
+    void create() {
+        Pawn pawn = new Pawn("white");
+        assertThat(pawn.getColor()).isEqualTo("white");
+    }
+
+}
