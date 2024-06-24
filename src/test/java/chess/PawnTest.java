b74e2183f27299043229d501b3622952525189c1
minseok-oh <minseok.dev@gmail.com>
2024-06-24 19:01:02 +0900

    test: 검은색 pawn 생성자 test code 추가

--- src/test/java/chess/PawnTest.java
+++ src/test/java/chess/PawnTest.java
@@ -1,12 +1,16 @@
-import chess.Pawn;
+package chess;
+
 import org.junit.jupiter.api.*;
 import static org.assertj.core.api.Assertions.*;
 
 public class PawnTest {
     @Test
-    @DisplayName("흰색 폰이 생성되어야 한다")
+    @DisplayName("색상에 맞는 폰이 생성되어야 한다")
     public void create() {
         Pawn pawn = new Pawn("white");
         assertThat(pawn.getColor()).isEqualTo("white");
+
+        pawn = new Pawn("black");
+        assertThat(pawn.getColor()).isEqualTo("black");
     }
 }
