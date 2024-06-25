7c7624ea403f333a7ebb852c01b0d314d44e3861
KoKimSS <seungsu8848@ajou.ac.kr>
2024-06-25 12:45:40 +0900

    refactor : 공통 상수로 변환

--- woowa_camp_chess/src/test/java/org/example/pieces/PawnTest.java
+++ woowa_camp_chess/src/test/java/org/example/pieces/PawnTest.java
@@ -10,17 +10,17 @@ import static org.example.chess.pieces.Pawn.WHITE_COLOR;
 
 class PawnTest {
     @Test
+    @DisplayName("기본 생성자로 폰이 생성되어야 한다")
+    public void create_기본생성자() {
+        Pawn pawn = new Pawn();
+        Assertions.assertEquals(WHITE_COLOR,pawn.getColor());
+    }
+
+    @Test
     @DisplayName("올바른 색의 폰이 생성되어야 한다")
     public void create() {
         verifyPawn(BLACK_COLOR);
         verifyPawn(WHITE_COLOR);
-    }
-
-    @Test
-    @DisplayName("기본 생성자로 폰이 생성되어야 한다")
-    public void create_기본생성자() {
-        Pawn pawn = new Pawn();
-        Assertions.assertEquals("white",pawn.getColor());
     }
 
     private static void verifyPawn(String color) {
