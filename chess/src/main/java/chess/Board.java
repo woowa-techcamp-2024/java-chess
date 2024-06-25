5ab77685b5f389ca04faf4659614da015b3ffea8
huiseung <whtjddn75@gmail.com>
2024-06-25 13:14:43 +0900

    [fix] Board initialize 수정 - 초기화시 체스판에 모든 기물들 정상 배치

--- chess/src/main/java/chess/Board.java
+++ chess/src/main/java/chess/Board.java
@@ -1,8 +1,7 @@
 package chess;
 
 import pieces.Piece;
-import pieces.PieceColor;
-import pieces.PieceUnicode;
+import pieces.PieceFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,45 +11,60 @@ public class Board {
     private final String BLANK = ".";
     private final List<Piece> whitePieces;
     private final List<Piece> blackPieces;
-    private int size = 0;
+    private final List<Piece> whitePawns;
+    private final List<Piece> blackPawns;
+    private int numOfPieces = 0;
+    private final PieceFactory pieceFactory;
 
     public Board() {
         this.whitePieces = new ArrayList<>();
         this.blackPieces = new ArrayList<>();
+        this.whitePawns = new ArrayList<>();
+        this.blackPawns = new ArrayList<>();
+        this.pieceFactory = new PieceFactory();
     }
 
-    public void add(Piece piece) {
-        if (piece.color().equals(PieceColor.WHITE)) {
-            whitePieces.add(piece);
-        } else if (piece.color().equals(PieceColor.BLACK)) {
-            blackPieces.add(piece);
-        }
-        size += 1;
+    public int getNumOfPieces() {
+        return numOfPieces;
     }
-
-    public int getSize() {
-        return size;
-    }
-
 
     public void initialize() {
+        whitePieces.add(pieceFactory.createWhiteRook());
+        whitePieces.add(pieceFactory.createWhiteKnight());
+        whitePieces.add(pieceFactory.createWhiteBishop());
+        whitePieces.add(pieceFactory.createWhiteQueen());
+        whitePieces.add(pieceFactory.createWhiteKing());
+        whitePieces.add(pieceFactory.createWhiteBishop());
+        whitePieces.add(pieceFactory.createWhiteKnight());
+        whitePieces.add(pieceFactory.createWhiteRook());
+
+        blackPieces.add(pieceFactory.createBlackRook());
+        blackPieces.add(pieceFactory.createBlackKnight());
+        blackPieces.add(pieceFactory.createBlackBishop());
+        blackPieces.add(pieceFactory.createBlackQueen());
+        blackPieces.add(pieceFactory.createBlackKing());
+        blackPieces.add(pieceFactory.createBlackBishop());
+        blackPieces.add(pieceFactory.createBlackKnight());
+        blackPieces.add(pieceFactory.createBlackRook());
+
         for (int i = 0; i < NUM_COL; i++) {
-            whitePieces.add(new Piece(PieceColor.WHITE));
-            blackPieces.add(new Piece(PieceColor.BLACK));
+            whitePawns.add(pieceFactory.createWhitePawn());
+            blackPawns.add(pieceFactory.createBlackPawn());
         }
+        numOfPieces = 32;
     }
 
     public String show() {
         StringBuilder sb = new StringBuilder();
         sb
-                .append(getBlankLine()).append("\n")
+                .append(getRepresentation(blackPieces)).append("\n")
                 .append(getBlackPawnsResult()).append("\n")
                 .append(getBlankLine()).append("\n")
                 .append(getBlankLine()).append("\n")
                 .append(getBlankLine()).append("\n")
                 .append(getBlankLine()).append("\n")
                 .append(getWhitePawnsResult()).append("\n")
-                .append(getBlankLine()).append("\n");
+                .append(getRepresentation(whitePieces));
         return sb.toString();
     }
 
@@ -59,21 +73,18 @@ public class Board {
     }
 
     public String getWhitePawnsResult() {
-        return getRepresentation(whitePieces);
+        return getRepresentation(whitePawns);
     }
 
     public String getBlackPawnsResult() {
-        return getRepresentation(blackPieces);
+        return getRepresentation(blackPawns);
     }
 
     private String getRepresentation(List<Piece> pieces) {
         StringBuilder sb = new StringBuilder();
-        for (Piece piece : pieces) {
-            if (piece.color().equals(PieceColor.BLACK)) {
-                sb.append(PieceUnicode.BLACK_PAWN.getUnicode());
-            } else if (piece.color().equals(PieceColor.WHITE)) {
-                sb.append(PieceUnicode.WHITE_PAWN.getUnicode());
-            }
+        for(Piece piece : pieces)
+        {
+            sb.append(piece.represent().getUnicode());
         }
         return sb.toString();
     }
