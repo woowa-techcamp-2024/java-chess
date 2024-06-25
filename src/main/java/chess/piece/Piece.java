fc781dcaa84c192b864032b6590de9f921a8e67f
CodingLuizy <luizy991212@gmail.com>
2024-06-25 14:06:52 +0900

    refactor: 기물 추상화
    Pawn을 기준으로 공통부분 추상화

--- src/main/java/chess/piece/Piece.java
+++ src/main/java/chess/piece/Piece.java
@@ -1,5 +1,30 @@
 package chess.piece;
 
 public abstract class Piece {
-    public abstract char getRepresentation();
+    public static final String WHITE_COLOR = "white";
+    public static final String BLACK_COLOR = "black";
+
+    protected final String color;
+    protected final char representation;
+
+    protected Piece(String color, char representation) {
+        this.color = color;
+        this.representation = representation;
+    }
+
+    public char getRepresentation() {
+        return representation;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public boolean isWhite() {
+        return color.equals(WHITE_COLOR);
+    }
+
+    public boolean isBlack() {
+        return color.equals(BLACK_COLOR);
+    }
 }
