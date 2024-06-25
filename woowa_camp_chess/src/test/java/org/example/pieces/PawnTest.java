61fee65a82c21c6c46c1fe680abc224d2153c0cc
KoKimSS <seungsu8848@ajou.ac.kr>
2024-06-25 12:50:06 +0900

    test : Pawn  출력 테스트 코드 작성

--- woowa_camp_chess/src/test/java/org/example/pieces/PawnTest.java
+++ woowa_camp_chess/src/test/java/org/example/pieces/PawnTest.java
@@ -7,24 +7,27 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.example.chess.pieces.Pawn.BLACK_COLOR;
 import static org.example.chess.pieces.Pawn.WHITE_COLOR;
+import static org.junit.jupiter.api.Assertions.*;
 
 class PawnTest {
     @Test
     @DisplayName("기본 생성자로 폰이 생성되어야 한다")
     public void create_기본생성자() {
         Pawn pawn = new Pawn();
-        Assertions.assertEquals(WHITE_COLOR,pawn.getColor());
+        assertEquals(WHITE_COLOR,pawn.getColor());
+        assertEquals(WHITE_REPRESENTATION,pawn.getRepresentation());
     }
 
     @Test
     @DisplayName("올바른 색의 폰이 생성되어야 한다")
     public void create() {
-        verifyPawn(BLACK_COLOR);
-        verifyPawn(WHITE_COLOR);
+        verifyPawn(BLACK_COLOR, BLACK_REPRESENTATION);
+        verifyPawn(WHITE_COLOR, WHITE_REPRESENTATION);
     }
 
-    private static void verifyPawn(String color) {
-        Pawn pawn1 = new Pawn(color);
+    private static void verifyPawn(final String color,final char representation) {
+        Pawn pawn1 = new Pawn(color, representation);
         assertThat(pawn1.getColor()).isEqualTo(color);
+        assertThat(pawn1.getRepresentation()).isEqualTo(representation);
     }
 }
