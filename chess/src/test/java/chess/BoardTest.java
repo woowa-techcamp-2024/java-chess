ef25cc554a95d58052863903920d91f8550ccb08
huiseung <whtjddn75@gmail.com>
2024-06-25 10:43:17 +0900

    [feature] Board 클래스 initialize, getWhitePawnsResult, getBlackPawnsResult 메서드 추가
    - whitePawns, blackPawns list field 추가
    - Pawn 출력을 위한 unicode 관리 enum 추가

--- chess/src/test/java/chess/BoardTest.java
+++ chess/src/test/java/chess/BoardTest.java
@@ -12,23 +12,6 @@ public class BoardTest {
     private final int NUM_COL = 8;
 
     @Test
-    public void create() throws Exception
-    {
-        Board board = new Board();
-
-        Pawn whitePawn = new Pawn(PawnColor.WHITE);
-        board.add(whitePawn);
-        assertEquals(1, board.getSize());
-        assertEquals(whitePawn, board.findPawn(0));
-
-        Pawn blackPawn = new Pawn(PawnColor.BLACK);
-        board.add(blackPawn);
-        assertEquals(2, board.getSize());
-        assertEquals(blackPawn, board.findPawn(1));
-    }
-
-
-    @Test
     public void initialize() throws Exception
     {
         Board board = new Board();
