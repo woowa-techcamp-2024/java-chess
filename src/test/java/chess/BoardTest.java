142b3e64e7d647bd26a5ea7a3a0cf822a76e252c
lass9436 <lass9436@gmail.com>
2024-06-25 17:30:39 +0900

    [test] BoardTest 작성
    Pawn 만 있던 테스트가 아니라, 이제 모든 기물이 존재하는 테스트를 하기 위해서 BoardTest 를 새로 만들었습니다.
    BeforeEach 로 Board 를 생성하고 모든 기물의 초기 배치를 테스트합니다.

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -1,39 +1,33 @@
 package chess;
 
-import chess.pieces.Pawn;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static utils.StringUtils.appendNewLine;
 
 public class BoardTest {
 
+    private Board board;
+
+    @BeforeEach
+    public void setup() {
+        board = new Board();
+    }
+
     @Test
     public void 보드_생성() {
-        Board board = new Board();
-
-        Pawn white = new Pawn(Pawn.Color.WHITE);
-        board.add(white);
-        assertEquals(1, board.size());
-        assertEquals(white, board.findPawn(0, Pawn.Color.WHITE));
-
-        Pawn black = new Pawn(Pawn.Color.BLACK);
-        board.add(black);
-        assertEquals(2, board.size());
-        assertEquals(black, board.findPawn(0, Pawn.Color.BLACK));
-    }
-
-    @Test
-    public void 보드_초기화() {
-        Board board = new Board();
         board.initialize();
-        assertEquals("pppppppp", board.getWhitePawnsResult());
-        assertEquals("PPPPPPPP", board.getBlackPawnsResult());
-    }
-
-    @Test
-    public void 보드_출력() {
-        Board board = new Board();
-        board.initialize();
-        System.out.println(board.print());
+        assertEquals(32, board.pieceCount());
+        String blankRank = appendNewLine("........");
+        assertEquals(
+                appendNewLine("♜♞♝♛♚♝♞♜") +
+                        appendNewLine("♟♟♟♟♟♟♟♟") +
+                        blankRank + blankRank + blankRank + blankRank +
+                        appendNewLine("♙♙♙♙♙♙♙♙") +
+                appendNewLine("♖♘♗♕♔♗♘♖"),
+                board.showBoard()
+        );
     }
 }
