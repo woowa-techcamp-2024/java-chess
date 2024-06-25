87cbd83d5592caa400dc11ddd0e803835fd8e964
minseok-oh <minseok.dev@gmail.com>
2024-06-25 10:17:37 +0900

    test: pawn 생성 중복성 제거

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -1,26 +1,35 @@
 package chess;
 
-
 import chess.pieces.Pawn;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BoardTest {
+    private Board board;
+
+    @BeforeEach
+    public void setUp() {
+        board = new Board();
+    }
+
     @Test
     @DisplayName("보드에 폰을 추가할 수 있다")
-    public void create() throws Exception {
-        Board board = new Board();
-
-        Pawn white = new Pawn(Pawn.WHITE_COLOR);
-        board.add(white);
+    public void create() {
+        Pawn pawn = createPawn(Pawn.WHITE_COLOR);
         assertEquals(1, board.size());
-        assertEquals(white, board.findPawn(0));
+        assertEquals(pawn, board.findPawn(0));
 
-        Pawn black = new Pawn(Pawn.BLACK_COLOR);
-        board.add(black);
+        pawn = createPawn(Pawn.BLACK_COLOR);
         assertEquals(2, board.size());
-        assertEquals(black, board.findPawn(1));
+        assertEquals(pawn, board.findPawn(1));
+    }
+
+    private Pawn createPawn(final String color) {
+        Pawn pawn = new Pawn(color);
+        board.add(pawn);
+        return pawn;
     }
 }
