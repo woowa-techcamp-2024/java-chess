7452e05edda656fcfd59ea69598562b725ec70a3
HyeonSik Choi <chgy2131@naver.com>
2024-06-25 11:14:12 +0900

    feat: Pawn 색에 따른 출력 문자가 부여된다 - 출력 문자는 유니코드 체스 심볼을 사용한다 - Representations 상수를 통해 출력 문자를 관리한다

--- src/test/java/chess/pieces/PawnTest.java
+++ src/test/java/chess/pieces/PawnTest.java
@@ -3,25 +3,26 @@ package chess.pieces;
 import org.junit.jupiter.api.*;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PawnTest {
     @Test
     @DisplayName("폰이 생성되어야 한다")
     public void create() {
-        verifyPawn(Colors.WHITE);
-        verifyPawn(Colors.BLACK);
+        verifyPawn(Colors.WHITE, Representations.WHITE_PAWN);
+        verifyPawn(Colors.BLACK, Representations.BLACK_PAWN);
     }
 
     @Test
     @DisplayName("폰 생성시 인자가 없다면 흰색으로 생성되어야 한다")
     public void create_기본생성자() throws Exception {
         Pawn pawn = new Pawn();
-        assertEquals("WHITE", pawn.getColor().name());
+        assertThat(pawn.getColor()).isEqualTo(Colors.WHITE);
+        assertThat(pawn.getRepresentation()).isEqualTo(Representations.WHITE_PAWN);
     }
 
-    private void verifyPawn(final Colors color) {
+    private void verifyPawn(final Colors color, final Representations representation) {
         Pawn pawn = new Pawn(color);
         assertThat(pawn.getColor()).isEqualTo(color);
+        assertThat(pawn.getRepresentation()).isEqualTo(representation);
     }
 }
