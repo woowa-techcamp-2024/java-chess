dfd9f1108e684b77976e55f5f9a7a3870a0e2d87
HyeonJun0530 <hyjn053@gmail.com>
2024-06-25 20:13:35 +0900

    refactor: PieceRepresentation.java 삭제 및 Type으로 통합

--- src/main/java/chess/piece/Type.java
+++ src/main/java/chess/piece/Type.java
@@ -1,5 +1,27 @@
 package chess.piece;
 
 public enum Type {
-    PAWN, ROOK, QUEEN, KING, KNIGHT, BISHOP
+    PAWN('♙'), ROOK('♖'), QUEEN('♕'),
+    KING('♔'), KNIGHT('♘'), BISHOP('♗'),
+    NO_PIECE('.');
+
+    private final char representation;
+
+    Type(final char representation) {
+        this.representation = representation;
+    }
+
+    public char getRepresentation(final PieceColor pieceColor) {
+        if(pieceColor.equals(PieceColor.BLACK)) {
+            return (char) (this.representation + 6);
+        } else if(pieceColor.equals(PieceColor.NO_COLOR)) {
+            return NO_PIECE.representation;
+        }
+
+        return this.representation;
+    }
+
+    public char getRepresentation() {
+        return this.representation;
+    }
 }
