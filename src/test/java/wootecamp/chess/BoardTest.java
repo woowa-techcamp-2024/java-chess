242e8220e04c938a81da5b1beda65d3df0b72e8e
seminchoi <choicco89@gmail.com>
2024-06-25 14:46:15 +0900

    test: 체스판을 초기화하는 테스트 케이스 작성
    - 흰색, 검은색 폰이 초기화 되는지 검증하도록 작성

--- src/test/java/wootecamp/chess/BoardTest.java
+++ src/test/java/wootecamp/chess/BoardTest.java
@@ -3,7 +3,6 @@ package wootecamp.chess;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import wootecamp.chess.pieces.Pawn;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,18 +16,10 @@ public class BoardTest {
     }
 
     @Test
-    @DisplayName("체스판을 생성한다.")
-    void create() {
-        Pawn whitePawn = new Pawn(Pawn.WHITE_COLOR);
-        verifyAddPawn(whitePawn, 1, 0);
-
-        Pawn blackPawn = new Pawn(Pawn.BLACK_COLOR);
-        verifyAddPawn(blackPawn, 2, 1);
-    }
-
-    void verifyAddPawn(Pawn pawn, int expectedBoardSize, int pawnIndex) {
-        board.add(pawn);
-        assertThat(board.size()).isEqualTo(expectedBoardSize);
-        assertThat(board.findPawn(pawnIndex)).isEqualTo(pawn);
+    @DisplayName("체스판을 초기화한다.")
+    void initialize() {
+        board.initialize();
+        assertThat(board.getInitialStateWhitePawnResult()).isEqualTo("pppppppp");
+        assertThat(board.getInitialStateBlackPawnResult()).isEqualTo("PPPPPPPP");
     }
 }
