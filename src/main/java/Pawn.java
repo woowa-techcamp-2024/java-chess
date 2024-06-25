793fb6f4e0da688ab97a6c39958a8c3907bfd293
hek-git <ghddmsrl1@naver.com>
2024-06-25 12:38:54 +0900

    feat: Pawn 클래스 구현 Pawn 클래스 구현 및 PawnTest 추가 테스트 구현

--- src/main/java/Pawn.java
+++ src/main/java/Pawn.java
@@ -1,3 +1,14 @@
 public class Pawn {
 
+    private String color;
+
+    public Pawn(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+
 }
