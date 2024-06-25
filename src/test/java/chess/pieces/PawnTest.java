ccf0aaf0f2e8c91ecf47b77e7ad33451f461e0eb
miiiinju1 <gms07073@ynu.ac.kr>
2024-06-25 10:48:39 +0900

    test: pawnTest의 접근 제어자 변경

--- src/test/java/chess/pieces/PawnTest.java
+++ src/test/java/chess/pieces/PawnTest.java
@@ -5,12 +5,11 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class PawnTest {
+class PawnTest {
 
     @Test
     @DisplayName("기본 생성자를 이용하면 흰색 폰이 생성되고, 기본 표현이 설정되어야 한다")
-
-    public void create_기본생성자() throws Exception {
+    void create_기본생성자() throws Exception {
         Pawn pawn = new Pawn();
         assertEquals(Pawn.WHITE_COLOR, pawn.getColor());
         assertEquals(Pawn.WHITE_REPRESENTATION, pawn.getRepresentation());
@@ -18,12 +17,12 @@ public class PawnTest {
 
     @Test
     @DisplayName("생성자를 이용하면 폰이 생성되어야 한다")
-    public void create() {
+    void create() {
         verifyPawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION);
         verifyPawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION);
     }
 
-    void verifyPawn(final String color, final char representation) {
+    private void verifyPawn(final String color, final char representation) {
         Pawn pawn = new Pawn(color, representation);
         assertEquals(color, pawn.getColor());
         assertEquals(representation, pawn.getRepresentation());
