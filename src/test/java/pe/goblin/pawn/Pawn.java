8244a7741444e53c7c25d698de366dcccf988c21
donghar <donghar@naver.com>
2024-06-25 09:02:32 +0900

    [Feat] 폰은 기본 생성자를 가진다.
    폰이 기본 생성자로 생성되면 color는 white로 한다.

--- src/test/java/pe/goblin/pawn/Pawn.java
+++ src/test/java/pe/goblin/pawn/Pawn.java
@@ -3,6 +3,10 @@ package pe.goblin.pawn;
 public class Pawn {
     private String color;
 
+    public Pawn() {
+        this("white");
+    }
+
     public Pawn(String color) {
         this.color = color;
     }
