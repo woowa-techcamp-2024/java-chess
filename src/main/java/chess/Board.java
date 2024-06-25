e80997138a9e36fe0c5855f1b30196dc21a24b47
HyeonSik Choi <chgy2131@naver.com>
2024-06-25 13:00:49 +0900

    feat: Board를 초기화하고 내용을 출력한다. - initialize를 통해 체스판을 초기화한다. - print를 통해 현재 체스판에 상황을 String으로 반환한다.

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -1,22 +1,85 @@
 package chess;
 
+import chess.pieces.Colors;
 import chess.pieces.Pawn;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Board {
-    List<Pawn> pawns = new ArrayList<>(); // <1>
+    List<Pawn> whitePawns = new ArrayList<>();
+    List<Pawn> blackPawns = new ArrayList<>();
 
     public void add(Pawn pawn) {
-        pawns.add(pawn);
+        if (pawn.getColor() == Colors.WHITE) {
+            whitePawns.add(pawn);
+        } else {
+            blackPawns.add(pawn);
+        }
     }
 
     public int size() {
-        return pawns.size();
+        return whitePawns.size() + blackPawns.size();
     }
 
-    public Pawn findPawn(int index) {
-        return pawns.get(index);
+    public Pawn findWhitePawn(int index) {
+        return whitePawns.get(index);
+    }
+
+    public Pawn findBlackPawn(int index) {
+        return blackPawns.get(index);
+    }
+
+    public void initialize() {
+        for (int i=0; i<8; i++) {
+            Pawn white = new Pawn(Colors.WHITE);
+            Pawn black = new Pawn(Colors.BLACK);
+            whitePawns.add(white);
+            blackPawns.add(black);
+        }
+    }
+
+    public String print() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(getBlank());
+        sb.append(getBlackPawnsResult());
+        sb.append("\n");
+        sb.append(getBlank());
+        sb.append(getBlank());
+        sb.append(getBlank());
+        sb.append(getBlank());
+        sb.append(getWhitePawnsResult());
+        sb.append("\n");
+        sb.append(getBlank());
+
+        return sb.toString();
+    }
+
+    private String getBlank() {
+        return "........\n";
+    }
+
+    private String getPawnsResult(Colors color) {
+        StringBuilder sb = new StringBuilder();
+
+        if (color == Colors.WHITE) {
+            for (Pawn p : whitePawns) {
+                sb.append(p.getRepresentation().getSymbol());
+            }
+        } else {
+            for (Pawn p : blackPawns) {
+                sb.append(p.getRepresentation().getSymbol());
+            }
+        }
+        return sb.toString();
+    }
+
+    public String getWhitePawnsResult() {
+        return getPawnsResult(Colors.WHITE);
+    }
+
+    public String getBlackPawnsResult() {
+        return getPawnsResult(Colors.BLACK);
     }
 }
--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -1,5 +1,6 @@
 package chess;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import chess.pieces.Colors;
@@ -21,11 +22,29 @@ public class BoardTest {
         Pawn white = new Pawn(Colors.WHITE);
         board.add(white);
         assertEquals(1, board.size());
-        assertEquals(white, board.findPawn(0));
+        assertEquals(white, board.findWhitePawn(0));
 
         Pawn black = new Pawn(Colors.BLACK);
         board.add(black);
         assertEquals(2, board.size());
-        assertEquals(black, board.findPawn(1));
+        assertEquals(black, board.findBlackPawn(0));
+    }
+
+    @Test
+    @DisplayName("체스판을 초기화하여 기물들을 배치한다")
+    public void initialize() throws Exception {
+        board.initialize();
+        assertEquals("♙♙♙♙♙♙♙♙", board.getWhitePawnsResult());
+        assertEquals("♟♟♟♟♟♟♟♟", board.getBlackPawnsResult());
+    }
+
+    @Test
+    @DisplayName("체스판을 출력한다")
+    void print() {
+        board.initialize();
+
+        String print = board.print();
+        assertThat(print).hasSize(72);
+        System.out.println(print);
     }
 }
