08f50d9638ac4dc6be55d9c61dbf4789c523400d
hellomatia <shuaigejp80@gmail.com>
2024-06-25 16:27:11 +0900

    refactor: 생상과 타입을 한번에 테스트하도록 하여 중복된 코드 제거

--- src/test/java/com/woowatechcamp/chess/pieces/PieceTest.java
+++ src/test/java/com/woowatechcamp/chess/pieces/PieceTest.java
@@ -2,25 +2,18 @@ package com.woowatechcamp.chess.pieces;
 
 import org.junit.jupiter.api.*;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertEquals;
 
 public class PieceTest {
 
     @Test
     @DisplayName("모든 흰색, 검은색 기물이 생성되어야 한다")
     public void create() {
-        verifyPiece(Piece.createWhitePawn(), Piece.Color.WHITE, Piece.Type.PAWN.getWhiteRepresentation());
-        verifyPiece(Piece.createBlackPawn(), Piece.Color.BLACK, Piece.Type.PAWN.getBlackRepresentation());
-        verifyPiece(Piece.createWhiteRook(), Piece.Color.WHITE, Piece.Type.ROOK.getWhiteRepresentation());
-        verifyPiece(Piece.createBlackRook(), Piece.Color.BLACK, Piece.Type.ROOK.getBlackRepresentation());
-        verifyPiece(Piece.createWhiteKnight(), Piece.Color.WHITE, Piece.Type.KNIGHT.getWhiteRepresentation());
-        verifyPiece(Piece.createBlackKnight(), Piece.Color.BLACK, Piece.Type.KNIGHT.getBlackRepresentation());
-        verifyPiece(Piece.createWhiteBishop(), Piece.Color.WHITE, Piece.Type.BISHOP.getWhiteRepresentation());
-        verifyPiece(Piece.createBlackBishop(), Piece.Color.BLACK, Piece.Type.BISHOP.getBlackRepresentation());
-        verifyPiece(Piece.createWhiteQueen(), Piece.Color.WHITE, Piece.Type.QUEEN.getWhiteRepresentation());
-        verifyPiece(Piece.createBlackQueen(), Piece.Color.BLACK, Piece.Type.QUEEN.getBlackRepresentation());
-        verifyPiece(Piece.createWhiteKing(), Piece.Color.WHITE, Piece.Type.KING.getWhiteRepresentation());
-        verifyPiece(Piece.createBlackKing(), Piece.Color.BLACK, Piece.Type.KING.getBlackRepresentation());
+        verifyPiece(Piece.createWhitePawn(), Piece.createBlackPawn(), Piece.Type.PAWN);
+        verifyPiece(Piece.createWhiteRook(), Piece.createBlackRook(), Piece.Type.ROOK);
+        verifyPiece(Piece.createWhiteKnight(), Piece.createBlackKnight(), Piece.Type.KNIGHT);
+        verifyPiece(Piece.createWhiteBishop(), Piece.createBlackBishop(), Piece.Type.BISHOP);
+        verifyPiece(Piece.createWhiteQueen(), Piece.createBlackQueen(), Piece.Type.QUEEN);
+        verifyPiece(Piece.createWhiteKing(), Piece.createBlackKing(), Piece.Type.KING);
 
         Piece blank = Piece.createBlank();
         assertThat(blank.isWhite()).isFalse();
@@ -28,20 +21,11 @@ public class PieceTest {
         assertThat(blank.getType()).isEqualTo(Piece.Type.BLANK);
     }
 
-    void verifyPiece(final Piece piece, final Piece.Color color, final char representation) {
-        assertEquals(color, piece.getColor());
-        assertEquals(representation, piece.getRepresentation());
-    }
+    private void verifyPiece(final Piece whitePiece, final Piece blackPiece, final Piece.Type type) {
+        assertThat(whitePiece.isWhite()).isTrue();
+        assertThat(type).isEqualTo(whitePiece.getType());
 
-    @Test
-    void 기물이_흰색일_경우_isWhite가_true_이다() {
-        Piece piece = Piece.createWhitePawn();
-        assertThat(piece.isWhite()).isTrue();
-    }
-
-    @Test
-    void 기물이_검은색일_경우_isBlack가_true_이다() {
-        Piece piece = Piece.createBlackPawn();
-        assertThat(piece.isBlack()).isTrue();
+        assertThat(blackPiece.isBlack()).isTrue();
+        assertThat(type).isEqualTo(blackPiece.getType());
     }
 }
