cc933ef6a40c4ea419d9c1a9b6ffb07bab87aeac
miiiinju1 <gms07073@ynu.ac.kr>
2024-06-25 15:54:56 +0900

    feat: 한 Rank에 대한 Piece를 관리하는 Rank 클래스 추가

---
+++ src/main/java/chess/Rank.java
@@ -0,0 +1,50 @@
+package chess;
+
+import chess.pieces.Piece;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Rank {
+
+    private final List<Piece> pieces;
+    private int pieceCount;
+
+    private static final int BOARD_WIDTH = 8;
+
+    private Rank(List<Piece> pieces) {
+        validatePieceCount(pieces);
+        for (Piece piece : pieces) {
+            if (!piece.isBlank()) {
+                pieceCount++;
+            }
+        }
+        this.pieces = new ArrayList<>(pieces);
+    }
+
+    public int getPieceCount() {
+        return pieceCount;
+    }
+
+    private void decreaseRankCount() {
+        pieceCount--;
+    }
+
+    public static Rank initializeRank(List<Piece> pieces) {
+        return new Rank(pieces);
+    }
+
+    private void validatePieceCount(List<Piece> pieces) {
+        if (pieces.size() != BOARD_WIDTH) {
+            throw new IllegalArgumentException("한 랭크는 8개의 말을 가집니다.");
+        }
+    }
+
+    public String printRank() {
+        return pieces.stream()
+                .map(Piece::getRepresentation)
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+    }
+}
