addc0085b7fc850a0c5cefcb966e2182ec5e2615
miiiinju1 <gms07073@ynu.ac.kr>
2024-06-25 17:05:57 +0900

    test: 주어진 위치의 기물을 조회하는 메서드의 정상, 예외의 실패 테스트 추가

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -1,14 +1,72 @@
 package chess;
 
+import chess.pieces.Piece;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static chess.pieces.Piece.Color;
 import static chess.pieces.Piece.Type;
 import static chess.utils.StringUtils.NEWLINE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BoardTest {
+
+    @DisplayName("찾고자 하는 좌표를 반대로 보낸 경우 예외가 발생한다.")
+    @Test
+    void findPieceWithInvalidCoordinate() {
+        // given
+        Board board = new Board();
+        String coordinate = "1e";
+
+        // when & then
+        assertThatThrownBy(() -> board.findPiece(coordinate))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("좌표는 알파벳과 숫자의 순서로 이루어져야 합니다.");
+    }
+
+    @DisplayName("해당하는 좌표의 Piece를 반환한다")
+    @Test
+    void findPiece() {
+        // given
+        Board board = new Board();
+        String coordinate = "a1";
+
+        // when
+        Piece piece = board.findPiece(coordinate);
+
+        // then
+        assertThat(piece).isNotNull()
+                .extracting("color", "type")
+                .containsExactly(Color.WHITE, Type.ROOK);
+    }
+
+    @DisplayName("알파벳 좌표의 범위를 벗어난 경우 예외가 발생한다")
+    @Test
+    void findPieceWithInvalidAlphabetRange() {
+        // given
+        Board board = new Board();
+        String coordinate = "i1";
+
+        // when & then
+        assertThatThrownBy(() -> board.findPiece(coordinate))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("범위를 넘어선 좌표입니다.");
+    }
+
+    @DisplayName("숫자 좌표의 범위를 벗어난 경우 예외가 발생한다")
+    @Test
+    void findPieceWithInvalidNumericRange() {
+        // given
+        Board board = new Board();
+        String coordinate = "a9";
+
+        // when & then
+        assertThatThrownBy(() -> board.findPiece(coordinate))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("범위를 넘어선 좌표입니다.");
+    }
 
     @Test
     @DisplayName("보드를 생성할 수 있다")
