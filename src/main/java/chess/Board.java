ad36e7beb77162048b9557ede935a663735a4c6c
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-06-25 14:49:14 +0900

    [Fix] board 위의 기물들의 점수 계산이 편하도록 white를 0~1행, black을 7~8행으로 재 배치

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -10,6 +10,10 @@ import java.util.List;
 public class Board {
     private final int WIDTH = 8;
     private final int HEIGHT = 8;
+    private final int WHITE_START_LINE = 0;
+    private final int WHITE_PAWN_START_LINE = WHITE_START_LINE+1;
+    private final int BLACK_START_LINE = HEIGHT-1;
+    private final int BLACK_PAWN_START_LINE = BLACK_START_LINE-1;
     private List<ChessPiece> pieces;
     private ChessPiece[][] board;
     public Board(){
@@ -40,8 +44,8 @@ public class Board {
         for(int w=0;w<WIDTH;w++){
             Piece whitePawn = Piece.createPiece(PieceTypes.WHITE_PAWN);
             Piece blackPawn = Piece.createPiece(PieceTypes.BLACK_PAWN);
-            setPiece(HEIGHT-2,w,whitePawn);
-            setPiece(1,w,blackPawn);
+            setPiece(WHITE_PAWN_START_LINE,w,whitePawn);
+            setPiece(BLACK_PAWN_START_LINE,w,blackPawn);
         }
 
         //rook 셋팅
@@ -49,45 +53,45 @@ public class Board {
         Piece whiteRookRight = Piece.createPiece(PieceTypes.WHITE_ROOK);
         Piece blackRookLeft = Piece.createPiece(PieceTypes.BLACK_ROOK);
         Piece blackRookRight = Piece.createPiece(PieceTypes.BLACK_ROOK);
-        setPiece(0,0,blackRookLeft);
-        setPiece(0,WIDTH-1,blackRookRight);
-        setPiece(HEIGHT-1,0,whiteRookLeft);
-        setPiece(HEIGHT-1,WIDTH-1,whiteRookRight);
+        setPiece(BLACK_START_LINE,0,blackRookLeft);
+        setPiece(BLACK_START_LINE,WIDTH-1,blackRookRight);
+        setPiece(WHITE_START_LINE,0,whiteRookLeft);
+        setPiece(WHITE_START_LINE,WIDTH-1,whiteRookRight);
 
         //night 셋팅
         Piece whiteNightLeft = Piece.createPiece(PieceTypes.WHITE_KNIGHT);
         Piece whiteNightRight = Piece.createPiece(PieceTypes.WHITE_KNIGHT);
         Piece blackNightLeft = Piece.createPiece(PieceTypes.BLACK_KNIGHT);
         Piece blackNightRight = Piece.createPiece(PieceTypes.BLACK_KNIGHT);
-        setPiece(0,1,blackNightLeft);
-        setPiece(0,WIDTH-2,blackNightRight);
-        setPiece(HEIGHT-1,1,whiteNightLeft);
-        setPiece(HEIGHT-1,WIDTH-2,whiteNightRight);
+        setPiece(BLACK_START_LINE,1,blackNightLeft);
+        setPiece(BLACK_START_LINE,WIDTH-2,blackNightRight);
+        setPiece(WHITE_START_LINE,1,whiteNightLeft);
+        setPiece(WHITE_START_LINE,WIDTH-2,whiteNightRight);
 
         //bishop 셋팅
         Piece whiteBishopLeft = Piece.createPiece(PieceTypes.WHITE_BISHOP);
         Piece whiteBishopRight = Piece.createPiece(PieceTypes.WHITE_BISHOP);
         Piece blackBishopLeft = Piece.createPiece(PieceTypes.BLACK_BISHOP);
         Piece blackBishopRight = Piece.createPiece(PieceTypes.BLACK_BISHOP);
-        setPiece(0,2,blackBishopLeft);
-        setPiece(0,WIDTH-3,blackBishopRight);
-        setPiece(HEIGHT-1,2,whiteBishopLeft);
-        setPiece(HEIGHT-1,WIDTH-3,whiteBishopRight);
+        setPiece(BLACK_START_LINE,2,blackBishopLeft);
+        setPiece(BLACK_START_LINE,WIDTH-3,blackBishopRight);
+        setPiece(WHITE_START_LINE,2,whiteBishopLeft);
+        setPiece(WHITE_START_LINE,WIDTH-3,whiteBishopRight);
 
         //queen & king setting
         Piece whiteQueen = Piece.createPiece(PieceTypes.WHITE_QUEEN);
         Piece whiteKing = Piece.createPiece(PieceTypes.WHITE_KING);
         Piece blackQueen = Piece.createPiece(PieceTypes.BLACK_QUEEN);
         Piece blackKing = Piece.createPiece(PieceTypes.BLACK_KING);
-        setPiece(0,3,blackQueen);
-        setPiece(0,WIDTH-4,blackKing);
-        setPiece(HEIGHT-1,3,whiteQueen);
-        setPiece(HEIGHT-1,WIDTH-4,whiteKing);
+        setPiece(BLACK_START_LINE,3,blackQueen);
+        setPiece(BLACK_START_LINE,WIDTH-4,blackKing);
+        setPiece(WHITE_START_LINE,3,whiteQueen);
+        setPiece(WHITE_START_LINE,WIDTH-4,whiteKing);
     }
 
     public String print(){
         StringBuilder sb = new StringBuilder();
-        for(int h=0;h<HEIGHT;h++){
+        for(int h=HEIGHT-1;h>=0;h--){
             for(int w=0;w<WIDTH;w++){
                 sb.append(board[h][w] == null ? '.' : board[h][w].getRepresentation());
             }
