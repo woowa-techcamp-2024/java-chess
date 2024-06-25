b8c92c666d20a6641f1ba6b1cdf3ca54c8a1e202
HiiWee <wpdlzhf159@gmail.com>
2024-06-25 16:52:42 +0900

    feat: 현재 기물의 상태를 문자열로 반환할 수 있다.
    - 폰, 룩, 비숍, 나이트, 킹, 퀸이 모두 포함
    - 현재 남아있는 기물의 갯수를 반환한다.

--- src/main/java/com/wootecam/chess/Board.java
+++ src/main/java/com/wootecam/chess/Board.java
@@ -5,6 +5,7 @@ import static com.wootecam.chess.utils.StringUtils.appendNewLine;
 import com.wootecam.chess.pieces.Piece;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Board {
@@ -12,15 +13,19 @@ public class Board {
     private static final String EMPTY_PIECES_RESULTS = "........";
     private static final int PIECE_COUNT = 8;
 
-    private final List<Piece> blackPieces = new ArrayList<>();
-    private final List<Piece> whitePieces = new ArrayList<>();
+    private final List<Piece> blackPawnPieces = new ArrayList<>();
+    private final List<Piece> blackOtherPieces = new ArrayList<>();
+    private final List<Piece> whitePawnPieces = new ArrayList<>();
+    private final List<Piece> whiteOtherPieces = new ArrayList<>();
 
     public Board() {
     }
 
     public void initialize() {
-        blackPieces.addAll(createPawns(Piece.COLOR_BLACK, Piece.BLACK_PAWN_REPRESENTATION));
-        whitePieces.addAll(createPawns(Piece.COLOR_WHITE, Piece.WHITE_PAWN_REPRESENTATION));
+        blackPawnPieces.addAll(createPawns(Piece.COLOR_BLACK, Piece.BLACK_PAWN_REPRESENTATION));
+        whitePawnPieces.addAll(createPawns(Piece.COLOR_WHITE, Piece.WHITE_PAWN_REPRESENTATION));
+        blackOtherPieces.addAll((createBlackOtherPieces()));
+        whiteOtherPieces.addAll((createWhiteOtherPieces()));
     }
 
     private List<Piece> createPawns(String color, String representation) {
@@ -29,17 +34,43 @@ public class Board {
                 .toList();
     }
 
+    private List<Piece> createBlackOtherPieces() {
+        return List.of(
+                Piece.createBlackRook(),
+                Piece.createBlackKnight(),
+                Piece.createBlackBishop(),
+                Piece.createBlackQueen(),
+                Piece.createBlackKing(),
+                Piece.createBlackBishop(),
+                Piece.createBlackKnight(),
+                Piece.createBlackRook()
+        );
+    }
+
+    private List<Piece> createWhiteOtherPieces() {
+        return List.of(
+                Piece.createWhiteRook(),
+                Piece.createWhiteKnight(),
+                Piece.createWhiteBishop(),
+                Piece.createWhiteQueen(),
+                Piece.createWhiteKing(),
+                Piece.createWhiteBishop(),
+                Piece.createWhiteKnight(),
+                Piece.createWhiteRook()
+        );
+    }
+
     public void add(final Piece piece) {
-        whitePieces.add(piece);
+        whitePawnPieces.add(piece);
     }
 
     public Piece findPiece(final int pieceIndex) {
-        if (pieceIndex < 0 || whitePieces.size() <= pieceIndex) {
-            String message = String.format("폰 인덱스는 0미만이거나 폰의 개수보다 크거나 같을 수 없습니다. size = %d", whitePieces.size());
+        if (pieceIndex < 0 || whitePawnPieces.size() <= pieceIndex) {
+            String message = String.format("폰 인덱스는 0미만이거나 폰의 개수보다 크거나 같을 수 없습니다. size = %d", whitePawnPieces.size());
             throw new IllegalArgumentException(message);
         }
 
-        return whitePieces.get(pieceIndex);
+        return whitePawnPieces.get(pieceIndex);
     }
 
     public void print() {
@@ -57,26 +88,43 @@ public class Board {
         System.out.println(boardResults);
     }
 
+    public String showBoard() {
+        StringBuilder boardResults = new StringBuilder();
+
+        boardResults.append(appendNewLine(createPiecesResults(blackOtherPieces)))
+                .append(appendNewLine(createPiecesResults(blackPawnPieces)))
+                .append(appendNewLine(EMPTY_PIECES_RESULTS))
+                .append(appendNewLine(EMPTY_PIECES_RESULTS))
+                .append(appendNewLine(EMPTY_PIECES_RESULTS))
+                .append(appendNewLine(EMPTY_PIECES_RESULTS))
+                .append(appendNewLine(createPiecesResults(whitePawnPieces)))
+                .append(appendNewLine(createPiecesResults(whiteOtherPieces)));
+
+        return boardResults.toString();
+    }
+
+    public int pieceCount() {
+        return blackPawnPieces.size()
+                + blackOtherPieces.size()
+                + whitePawnPieces.size()
+                + whiteOtherPieces.size();
+    }
+
     public int size() {
-        return whitePieces.size();
+        return whitePawnPieces.size();
     }
 
     public String getWhitePawnsResults() {
-        StringBuilder results = createPawnsResults(whitePieces);
-
-        return results.toString();
+        return createPiecesResults(whitePawnPieces);
     }
 
     public String getBlackPawnsResults() {
-        StringBuilder results = createPawnsResults(blackPieces);
-
-        return results.toString();
+        return createPiecesResults(blackPawnPieces);
     }
 
-    private StringBuilder createPawnsResults(List<Piece> pieces) {
-        StringBuilder results = new StringBuilder();
-        pieces.forEach(piece -> results.append(piece.getRepresentation()));
-
-        return results;
+    private String createPiecesResults(List<Piece> pieces) {
+        return pieces.stream()
+                .map(Piece::getRepresentation)
+                .collect(Collectors.joining());
     }
 }
--- src/test/java/com/wootecam/chess/BoardTest.java
+++ src/test/java/com/wootecam/chess/BoardTest.java
@@ -1,5 +1,6 @@
 package com.wootecam.chess;
 
+import static com.wootecam.chess.utils.StringUtils.appendNewLine;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -81,13 +82,6 @@ public class BoardTest {
         @Nested
         class 보드를_생성하고_호출하면 {
 
-            private Board board;
-
-            @BeforeEach
-            void setUp() {
-                board = new Board();
-            }
-
             @Test
             void 흰색_검정색_폰의_갯수를_각각_8개로_초기화한다() {
                 // when
@@ -98,6 +92,57 @@ public class BoardTest {
                         () -> assertThat(board.getBlackPawnsResults()).isEqualTo("PPPPPPPP"),
                         () -> assertThat(board.getWhitePawnsResults()).isEqualTo("pppppppp")
                 );
+            }
+        }
+    }
+
+    @Nested
+    class showBoard_메소드는 {
+
+        @Nested
+        class 보드를_생성하고_호출하면 {
+
+            @Test
+            void 현재_보드의_상태를_문자열로_반환한다() {
+                // given
+                board.initialize();
+                String expectedResults = generateDefaultBoardResults();
+
+                // when
+                String currentBoardResults = board.showBoard();
+
+                // then
+                assertThat(currentBoardResults).isEqualTo(expectedResults);
+            }
+
+            private String generateDefaultBoardResults() {
+                String blankRank = appendNewLine("........");
+
+                return appendNewLine("RNBQKBNR") +
+                        appendNewLine("PPPPPPPP") +
+                        blankRank + blankRank + blankRank + blankRank +
+                        appendNewLine("pppppppp") +
+                        appendNewLine("rnbqkbnr");
+            }
+        }
+    }
+
+    @Nested
+    class pieceCount_메소드는 {
+
+        @Nested
+        class 보드를_생성하고_호춣하면 {
+
+            @Test
+            void 현재_보드에_존재하는_기물의_갯수를_반환한다() {
+                // given
+                board.initialize();
+
+                // when
+                int count = board.pieceCount();
+
+                // then
+                assertThat(count).isEqualTo(32);
             }
         }
     }
