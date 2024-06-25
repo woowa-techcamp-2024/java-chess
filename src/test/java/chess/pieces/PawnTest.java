aee9947cf1e544bb4c0e3bf300ce90ae822fcb1d
김수현 <i960107@naver.com>
2024-06-25 13:50:21 +0900

    test: Pawn 표현 테스트 추가

--- src/test/java/chess/pieces/PawnTest.java
+++ src/test/java/chess/pieces/PawnTest.java
@@ -1,28 +1,42 @@
 package chess.pieces;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class PawnTest {
 
-    @DisplayName("흰색/검은색 폰이 생성되어야한다.")
-    @ParameterizedTest
-    @ValueSource(strings = {"white", "black"})
-    void create(String color) {
-        Pawn pawn = new Pawn(color);
-        verifyPawn(pawn, color);
-    }
-
-    private void verifyPawn(final Pawn pawn, final String expectedColor) {
-        Assertions.assertThat(pawn.getColor()).isEqualTo(expectedColor);
-    }
-
+    @DisplayName("기본 생성자는 흰색 폰을 생성하고 표현한다.")
     @Test
-    public void create_기본생성자() {
+    void create_기본생성자() {
         Pawn pawn = new Pawn();
-        org.junit.jupiter.api.Assertions.assertEquals("white", pawn.getColor());
+        assertEquals(Pawn.WHITE_COLOR, pawn.getColor());
+        assertEquals(Pawn.WHITE_REPRESENTATION, pawn.getRepresentation());
+    }
+
+    @DisplayName("흰색/검은색 폰을 생성하고 표현한다.")
+    @ParameterizedTest
+    @MethodSource
+    void create(final String color, final char representation) {
+        verifyPawn(color, representation);
+    }
+
+    private static Stream<Arguments> create() {
+        return Stream.of(
+                Arguments.of(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION),
+                Arguments.of(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION)
+        );
+    }
+
+    private void verifyPawn(final String color, final char representation) {
+        Pawn pawn = new Pawn(color);
+        assertThat(pawn.getColor()).isEqualTo(color);
+        assertThat(pawn.getRepresentation()).isEqualTo(representation);
     }
 }
