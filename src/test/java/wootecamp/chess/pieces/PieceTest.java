4c236f844c0559743a252072f0d3b130e580c701
seminchoi <choicco89@gmail.com>
2024-06-25 16:29:08 +0900

    test: 팩토리메소드로 기물들을 생성하는 테스트 케이스 작성
    - 모든 기물을 올바르게 생성하는지 검증하도록 작성

--- src/test/java/wootecamp/chess/pieces/PieceTest.java
+++ src/test/java/wootecamp/chess/pieces/PieceTest.java
@@ -4,27 +4,34 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PieceTest {
     @Test
-    @DisplayName("생성자 파라미터에 색깔을 전달해서 폰을 생성한다.")
-    void create() {
-        verifyPawn(Piece.WHITE_COLOR, Piece.WHITE_REPRESENTATION);
-        verifyPawn(Piece.BLACK_COLOR, Piece.BLACK_REPRESENTATION);
+    @DisplayName("팩토리 메소드로 기물들을 올바르게 생성하는지 테스트한다.")
+    public void createPieces() {
+        verifyPiece(Piece.createWhitePawn(), Piece.PAWN, Piece.WHITE_COLOR, Piece.WHITE_PAWN_REPRESENTATION);
+        verifyPiece(Piece.createBlackPawn(), Piece.PAWN, Piece.BLACK_COLOR, Piece.BLACK_PAWN_REPRESENTATION);
+
+        verifyPiece(Piece.createWhiteRook(), Piece.ROOK, Piece.WHITE_COLOR, Piece.WHITE_ROOK_REPRESENTATION);
+        verifyPiece(Piece.createBlackRook(), Piece.ROOK, Piece.BLACK_COLOR, Piece.BLACK_ROOK_REPRESENTATION);
+
+        verifyPiece(Piece.createWhiteKnight(), Piece.KNIGHT, Piece.WHITE_COLOR, Piece.WHITE_KNIGHT_REPRESENTATION);
+        verifyPiece(Piece.createBlackKnight(), Piece.KNIGHT, Piece.BLACK_COLOR, Piece.BLACK_KNIGHT_REPRESENTATION);
+
+        verifyPiece(Piece.createWhiteBishop(), Piece.BISHOP, Piece.WHITE_COLOR, Piece.WHITE_BISHOP_REPRESENTATION);
+        verifyPiece(Piece.createBlackBishop(), Piece.BISHOP, Piece.BLACK_COLOR, Piece.BLACK_BISHOP_REPRESENTATION);
+
+        verifyPiece(Piece.createWhiteQueen(), Piece.QUEEN, Piece.WHITE_COLOR, Piece.WHITE_QUEEN_REPRESENTATION);
+        verifyPiece(Piece.createBlackQueen(), Piece.QUEEN, Piece.BLACK_COLOR, Piece.BLACK_QUEEN_REPRESENTATION);
+
+        verifyPiece(Piece.createWhiteKing(), Piece.KING, Piece.WHITE_COLOR, Piece.WHITE_KING_REPRESENTATION);
+        verifyPiece(Piece.createBlackKing(), Piece.KING, Piece.BLACK_COLOR, Piece.BLACK_KING_REPRESENTATION);
     }
 
-    void verifyPawn(final String color, final String representation) {
-        final Piece piece = new Piece(color);
+    private void verifyPiece(final Piece piece, String name, final String color, final String representation) {
+        assertThat(piece.getName()).isEqualTo(name);
         assertThat(piece.getColor()).isEqualTo(color);
-        assertThat(piece.getRepresentation()).isEqualTo(representation);
-    }
-
-    @Test
-    @DisplayName("기본 생성자는 흰색 폰을 생성한다.")
-    void create_defaultConstructor() {
-        Piece piece = new Piece();
-        assertThat(piece.getColor()).isEqualTo(Piece.WHITE_COLOR);
-        assertThat(piece.getRepresentation()).isEqualTo(Piece.WHITE_REPRESENTATION);
+        assertEquals(representation, piece.getRepresentation());
     }
 }
