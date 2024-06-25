1503c5ffd4e9dd20c755c89fdcba378909135fdb
HiiWee <wpdlzhf159@gmail.com>
2024-06-25 11:03:25 +0900

    refactor: board 테스트를 계층형으로 리팩토링
    - 테스트 대상을 두고(Describe)
    - 실제로 놓이게 되는 상황을 설정(Context)
    - 대상을 실행 및 검증(It)

--- src/test/java/com/wootecam/chess/BoardTest.java
+++ src/test/java/com/wootecam/chess/BoardTest.java
@@ -8,7 +8,7 @@ import com.wootecam.chess.pieces.Pawn;
 import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -23,37 +23,53 @@ public class BoardTest {
         board = new Board();
     }
 
-    @ParameterizedTest
-    @MethodSource("generatePawns")
-    void 보드에_폰을_순서대로_추가할_수_있다(List<Pawn> pawns, int pawnIndex, int size) {
-        // when
-        pawns.forEach(board::add);
-        Pawn findPawn = board.findPawn(pawnIndex);
+    @Nested
+    class add_메소드는 {
 
-        // then
-        assertAll(
-                () -> assertThat(board.size()).isEqualTo(size),
-                () -> assertThat(findPawn).isEqualTo(pawns.get(pawnIndex))
-        );
+        @Nested
+        class 보드에_폰을_추가하면 {
+
+            @ParameterizedTest
+            @MethodSource("generatePawns")
+            void 순서가_보장된다(List<Pawn> pawns, int pawnIndex, int size) {
+                // when
+                pawns.forEach(board::add);
+                Pawn findPawn = board.findPawn(pawnIndex);
+
+                // then
+                assertAll(
+                        () -> assertThat(board.size()).isEqualTo(size),
+                        () -> assertThat(findPawn).isEqualTo(pawns.get(pawnIndex))
+                );
+            }
+
+            private static Stream<Arguments> generatePawns() {
+                return Stream.of(
+                        Arguments.of(List.of(new Pawn(Pawn.COLOR_WHITE)), 0, 1),
+                        Arguments.of(List.of(new Pawn(Pawn.COLOR_BLACK), new Pawn(Pawn.COLOR_WHITE)), 0, 2)
+                );
+            }
+        }
     }
 
-    private static Stream<Arguments> generatePawns() {
-        return Stream.of(
-                Arguments.of(List.of(new Pawn(Pawn.COLOR_WHITE)), 0, 1),
-                Arguments.of(List.of(new Pawn(Pawn.COLOR_BLACK), new Pawn(Pawn.COLOR_WHITE)), 0, 2)
-        );
-    }
+    @Nested
+    class findPawn_메소드는 {
 
-    @ParameterizedTest
-    @ValueSource(ints = {-1, 2})
-    void 폰의개수보다_큰_인덱스거나_0미만의_인덱스라면_예외가_발생한다(int index) {
-        // given
-        Pawn pawn = new Pawn(Pawn.COLOR_WHITE);
-        board.add(pawn);
+        @Nested
+        class 폰의_갯수보다_크거나_0미만의_인덱스를_입력하면 {
 
-        // expect
-        assertThatThrownBy(() -> board.findPawn(index))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("폰 인덱스는 0미만이거나 폰의 개수보다 크거나 같을 수 없습니다. size = " + board.size());
+            @ParameterizedTest
+            @ValueSource(ints = {-1, 2})
+            void 예외가_발생한다(int invalidIndex) {
+                // given
+                Pawn pawn = new Pawn(Pawn.COLOR_WHITE);
+                board.add(pawn);
+
+                // expect
+                assertThatThrownBy(() -> board.findPawn(invalidIndex))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("폰 인덱스는 0미만이거나 폰의 개수보다 크거나 같을 수 없습니다. size = " + board.size());
+            }
+        }
     }
 }
