64d1cd237821a41341caa4dc25a239475dab63a3
Lechros <0onionion0@gmail.com>
2024-06-25 10:17:38 +0900

    feat: 체스판 구현 추가
    Board는 전체 체스판을 나타냄, 크기는 8로 고정
    Cell은 체스판의 한 칸을 나타냄

---
+++ src/main/Board.java
@@ -0,0 +1,69 @@
+import piece.Pawn;
+import piece.Piece;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Board {
+
+    public static final int LENGTH = 8;
+
+    private final Cell[][] cells;
+
+    public Board() {
+        this.cells = new Cell[LENGTH][LENGTH];
+        for (int r = 0; r < LENGTH; r++) {
+            for (int c = 0; c < LENGTH; c++) {
+                cells[r][c] = new Cell(r, c);
+            }
+        }
+    }
+
+    Piece get(int r, int c) {
+        return cellAt(r, c).getPiece();
+    }
+
+    void set(int r, int c, Piece piece) {
+        cellAt(r, c).setPiece(piece);
+    }
+
+    protected Cell cellAt(int r, int c) {
+        return cells[r][c];
+    }
+
+    public List<Pawn> findPawns() {
+        return stream().filter(cell -> !cell.isEmpty())
+                .map(cell -> cell.piece)
+                .filter(piece -> piece instanceof Pawn)
+                .map(Pawn.class::cast)
+                .toList();
+    }
+
+    protected Stream<Cell> stream() {
+        return Arrays.stream(cells).flatMap(Arrays::stream);
+    }
+
+    public static class Cell {
+
+        final int r, c;
+        private Piece piece;
+
+        public Cell(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+
+        public boolean isEmpty() {
+            return piece == null;
+        }
+
+        public Piece getPiece() {
+            return piece;
+        }
+
+        private void setPiece(Piece piece) {
+            this.piece = piece;
+        }
+    }
+}
---
+++ src/test/BoardTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+import piece.Color;
+import piece.Pawn;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BoardTest {
+
+    @Test
+    public void create() {
+        Board board = new Board();
+
+        Pawn white = new Pawn(Color.WHITE);
+        board.set(1, 2, white);
+        assertThat(board.get(1, 2)).isEqualTo(white);
+        assertThat(board.findPawns()).hasSize(1);
+        assertThat(board.findPawns().get(0)).isEqualTo(white);
+
+        Pawn black = new Pawn(Color.BLACK);
+        board.set(0, 3, black);
+        assertThat(board.get(0, 3)).isEqualTo(black);
+        assertThat(board.findPawns()).hasSize(2);
+        assertThat(board.findPawns().get(0)).isEqualTo(black);
+    }
+
+}
