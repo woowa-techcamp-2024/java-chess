42b2d183e063ae2496746356a46191ff75ad0d2a
KoKimSS <seungsu8848@ajou.ac.kr>
2024-06-25 11:02:57 +0900

    Revert "feat : Board에 Pawn 추가메소드, 갯수 확인 메소드, Pawn 찾는 메소드 작성"
    This reverts commit 13aa1ff996563ca2fd455f2055e949dba80b8b3a.

--- woowa_camp_chess/src/main/java/org/example/board/Board.java
+++ woowa_camp_chess/src/main/java/org/example/board/Board.java
@@ -1,24 +1,4 @@
 package org.example.board;
 
-import org.example.pieces.Pawn;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class Board {
-
-    static List<Pawn> pawnList = new ArrayList<>();
-
-    public void add(Pawn pawn) {
-        pawnList.add(pawn);
-    }
-
-
-    public int size() {
-        return pawnList.size();
-    }
-
-    public Pawn findPawn(int i) {
-        return pawnList.get(i);
-    }
 }
