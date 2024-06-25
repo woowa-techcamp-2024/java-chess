01a9baa57048d3ede72c01763bc661e80c330078
yudonggeun <ydong98@gmail.com>
2024-06-25 10:16:53 +0900

    feat : 폰 기본 생성자 추가 - 기본 생성자로 폰을 생성하면 색상은 흰색이다.

--- src/test/java/com/example/demo/piece/PawnTest.java
+++ src/test/java/com/example/demo/piece/PawnTest.java
@@ -1,6 +1,7 @@
 package com.example.demo.piece;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -14,5 +15,12 @@ class PawnTest {
     public void create(final String color){
         Pawn pawn = new Pawn(color);
         assertThat(pawn.getColor()).isEqualTo(color);
+    }
+
+    @Test
+    @DisplayName("색을 지정하지 않고 폰을 생성하면 흰색이다.")
+    public void create_기본생성자() {
+        Pawn pawn = new Pawn();
+        assertThat(pawn.getColor()).isEqualTo("white");
     }
 }
