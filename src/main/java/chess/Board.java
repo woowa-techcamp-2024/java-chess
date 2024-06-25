798f4b536ef5f5e0cbca0588013032546a59ffa6
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-06-25 15:17:54 +0900

    [Refactor] Board의 빈칸에 blank piece 추가

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -2,10 +2,11 @@ package chess;
 
 import chess.pieces.ChessPiece;
 import chess.pieces.Piece;
-import chess.pieces.PieceTypes;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static chess.pieces.PieceTypes.*;
 
 public class Board {
     private final int WIDTH = 8;
@@ -42,51 +43,60 @@ public class Board {
     public void initialize(){
         //pawn 셋팅
         for(int w=0;w<WIDTH;w++){
-            Piece whitePawn = Piece.createPiece(PieceTypes.WHITE_PAWN);
-            Piece blackPawn = Piece.createPiece(PieceTypes.BLACK_PAWN);
+            Piece whitePawn = Piece.createPiece(WHITE_PAWN);
+            Piece blackPawn = Piece.createPiece(BLACK_PAWN);
             setPiece(WHITE_PAWN_START_LINE,w,whitePawn);
             setPiece(BLACK_PAWN_START_LINE,w,blackPawn);
         }
 
         //rook 셋팅
-        Piece whiteRookLeft = Piece.createPiece(PieceTypes.WHITE_ROOK);
-        Piece whiteRookRight = Piece.createPiece(PieceTypes.WHITE_ROOK);
-        Piece blackRookLeft = Piece.createPiece(PieceTypes.BLACK_ROOK);
-        Piece blackRookRight = Piece.createPiece(PieceTypes.BLACK_ROOK);
+        Piece whiteRookLeft = Piece.createPiece(WHITE_ROOK);
+        Piece whiteRookRight = Piece.createPiece(WHITE_ROOK);
+        Piece blackRookLeft = Piece.createPiece(BLACK_ROOK);
+        Piece blackRookRight = Piece.createPiece(BLACK_ROOK);
         setPiece(BLACK_START_LINE,0,blackRookLeft);
         setPiece(BLACK_START_LINE,WIDTH-1,blackRookRight);
         setPiece(WHITE_START_LINE,0,whiteRookLeft);
         setPiece(WHITE_START_LINE,WIDTH-1,whiteRookRight);
 
         //night 셋팅
-        Piece whiteNightLeft = Piece.createPiece(PieceTypes.WHITE_KNIGHT);
-        Piece whiteNightRight = Piece.createPiece(PieceTypes.WHITE_KNIGHT);
-        Piece blackNightLeft = Piece.createPiece(PieceTypes.BLACK_KNIGHT);
-        Piece blackNightRight = Piece.createPiece(PieceTypes.BLACK_KNIGHT);
+        Piece whiteNightLeft = Piece.createPiece(WHITE_KNIGHT);
+        Piece whiteNightRight = Piece.createPiece(WHITE_KNIGHT);
+        Piece blackNightLeft = Piece.createPiece(BLACK_KNIGHT);
+        Piece blackNightRight = Piece.createPiece(BLACK_KNIGHT);
         setPiece(BLACK_START_LINE,1,blackNightLeft);
         setPiece(BLACK_START_LINE,WIDTH-2,blackNightRight);
         setPiece(WHITE_START_LINE,1,whiteNightLeft);
         setPiece(WHITE_START_LINE,WIDTH-2,whiteNightRight);
 
         //bishop 셋팅
-        Piece whiteBishopLeft = Piece.createPiece(PieceTypes.WHITE_BISHOP);
-        Piece whiteBishopRight = Piece.createPiece(PieceTypes.WHITE_BISHOP);
-        Piece blackBishopLeft = Piece.createPiece(PieceTypes.BLACK_BISHOP);
-        Piece blackBishopRight = Piece.createPiece(PieceTypes.BLACK_BISHOP);
+        Piece whiteBishopLeft = Piece.createPiece(WHITE_BISHOP);
+        Piece whiteBishopRight = Piece.createPiece(WHITE_BISHOP);
+        Piece blackBishopLeft = Piece.createPiece(BLACK_BISHOP);
+        Piece blackBishopRight = Piece.createPiece(BLACK_BISHOP);
         setPiece(BLACK_START_LINE,2,blackBishopLeft);
         setPiece(BLACK_START_LINE,WIDTH-3,blackBishopRight);
         setPiece(WHITE_START_LINE,2,whiteBishopLeft);
         setPiece(WHITE_START_LINE,WIDTH-3,whiteBishopRight);
 
         //queen & king setting
-        Piece whiteQueen = Piece.createPiece(PieceTypes.WHITE_QUEEN);
-        Piece whiteKing = Piece.createPiece(PieceTypes.WHITE_KING);
-        Piece blackQueen = Piece.createPiece(PieceTypes.BLACK_QUEEN);
-        Piece blackKing = Piece.createPiece(PieceTypes.BLACK_KING);
+        Piece whiteQueen = Piece.createPiece(WHITE_QUEEN);
+        Piece whiteKing = Piece.createPiece(WHITE_KING);
+        Piece blackQueen = Piece.createPiece(BLACK_QUEEN);
+        Piece blackKing = Piece.createPiece(BLACK_KING);
         setPiece(BLACK_START_LINE,3,blackQueen);
         setPiece(BLACK_START_LINE,WIDTH-4,blackKing);
         setPiece(WHITE_START_LINE,3,whiteQueen);
         setPiece(WHITE_START_LINE,WIDTH-4,whiteKing);
+
+        //아무것도 없는 칸들을 blank piece로 채우기
+        for(int x=0;x<HEIGHT;x++){
+            for(int y=0;y<WIDTH;y++){
+                if(board[x][y] == null){
+                    board[x][y] = Piece.createPiece(NO_PIECE);
+                }
+            }
+        }
     }
 
     public String print(){
