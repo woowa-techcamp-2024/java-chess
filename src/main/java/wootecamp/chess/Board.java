5da6f6596ef3f3569214f201147e585b15e87527
seminchoi <choicco89@gmail.com>
2024-06-25 15:21:19 +0900

    feat: 현재 체스판 상태를 문자열로 반환하는 기능 구현
    - 상수값과 반복문을 이용해 구현

--- src/main/java/wootecamp/chess/Board.java
+++ src/main/java/wootecamp/chess/Board.java
@@ -15,22 +15,25 @@ public class Board {
 
     private static final String EMPTY_RANK_MESSAGE = "........";
 
-
     private List<Pawn> whitePawns = new ArrayList<>();
     private List<Pawn> blackPawns = new ArrayList<>();
+    private boolean isInit = false;
 
     public void initialize() {
+        isInit = true;
         initializeWhitePawns();
         initializeBlackPawns();
     }
 
     private void initializeWhitePawns() {
+        whitePawns = new ArrayList<>();
         for (int i = 0; i < BOARD_SIZE; i++) {
             whitePawns.add(new Pawn(Pawn.WHITE_COLOR));
         }
     }
 
     private void initializeBlackPawns() {
+        blackPawns = new ArrayList<>();
         for (int i = 0; i < BOARD_SIZE; i++) {
             blackPawns.add(new Pawn(Pawn.BLACK_COLOR));
         }
@@ -50,5 +53,32 @@ public class Board {
             result.append(blackPawns.get(i).getRepresentation());
         }
         return result.toString();
+    }
+
+
+    public String getBoardState() {
+        if(!isInit) {
+            throw new IllegalStateException("아직 보드가 초기화되지 않았습니다.");
+        }
+
+        StringBuilder builder = new StringBuilder();
+        int finalRank = 1;
+        for (int rank = BOARD_SIZE; rank > 0; rank--) {
+            builder.append(getRankState(rank));
+            if(rank > finalRank) {
+                builder.append("\n");
+            }
+        }
+        return builder.toString();
+    }
+
+    private String getRankState(int rank) {
+        if(rank == WHITE_PAWN_INIT_RANK) {
+            return getInitialStateWhitePawnResult();
+        }
+        if(rank == BLACK_PAWN_INIT_RANK) {
+            return getInitialStateBlackPawnResult();
+        }
+        return EMPTY_RANK_MESSAGE;
     }
 }
