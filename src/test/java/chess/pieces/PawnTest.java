2f724dbb680464e4b20de75c53f9fe943fdbc014
lass9436 <lass9436@gmail.com>
2024-06-25 13:58:20 +0900

    [test] 생성 테스트를 추가했습니다.
    Pawn 을 생성하는 테스트를 추가했습니다.
    이 테스트는 폰의 색깔에 맞춰서 표현이 되기를 기대합니다.

--- src/test/java/chess/pieces/PawnTest.java
+++ src/test/java/chess/pieces/PawnTest.java
@@ -26,5 +26,18 @@ public class PawnTest {
     public void 기본_폰_생성() throws Exception {
         Pawn pawn = new Pawn();
         assertEquals(Pawn.Color.WHITE, pawn.getColor());
+        assertEquals(Pawn.Color.WHITE.getRepresentation(), pawn.getRepresentation());
+    }
+
+    @Test
+    public void 생성() {
+        verifyPawn(Pawn.Color.WHITE, Pawn.Color.WHITE.getRepresentation());
+        verifyPawn(Pawn.Color.BLACK, Pawn.Color.BLACK.getRepresentation());
+    }
+
+    void verifyPawn(Pawn.Color color, final char representation) {
+        final Pawn pawn = new Pawn(color);
+        assertEquals(color, pawn.getColor());
+        assertEquals(representation, pawn.getRepresentation());
     }
 }
