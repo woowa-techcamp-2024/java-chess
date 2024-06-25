213102687599eb9baa50308b952e5d1371a7380f
CodingLuizy <luizy991212@gmail.com>
2024-06-25 14:05:35 +0900

    test: 모든 기물을추가하기 위한 테스트 추가
    - 완성된 보드 형태를 검사하는 테스트
    - 팩토리 메서드를 검사하는 테스트

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -1,30 +1,39 @@
 package chess;
 
 import chess.piece.Pawn;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
+    private Board board;
+
+    @BeforeEach
+    public void setup() {
+        board = new Board();
+    }
 
     @Test
     public void create() throws Exception {
-        Board board = new Board();
-
-        Pawn white = new Pawn(Pawn.WHITE_COLOR);
-        board.add(white);
-        assertEquals(1, board.size());
-        assertEquals(white, board.findPawn(0));
-
-        Pawn black = new Pawn(Pawn.BLACK_COLOR);
-        board.add(black);
-        assertEquals(2, board.size());
-        assertEquals(black, board.findPawn(1));
+        board.initialize();
+        assertEquals(32, board.pieceCount());
+        assertEquals(
+                "\ta\tb\tc\td\te\tf\tg\th\n" +
+                        "8\t♜\t♞\t♝\t♛\t♚\t♝\t♞\t♜\t8\n" +
+                        "7\t♟\t♟\t♟\t♟\t♟\t♟\t♟\t♟\t7\n" +
+                        "6\t.\t.\t.\t.\t.\t.\t.\t.\t6\n" +
+                        "5\t.\t.\t.\t.\t.\t.\t.\t.\t5\n" +
+                        "4\t.\t.\t.\t.\t.\t.\t.\t.\t4\n" +
+                        "3\t.\t.\t.\t.\t.\t.\t.\t.\t3\n" +
+                        "2\t♙\t♙\t♙\t♙\t♙\t♙\t♙\t♙\t2\n" +
+                        "1\t♖\t♘\t♗\t♕\t♔\t♗\t♘\t♖\t1\n" +
+                        "\ta\tb\tc\td\te\tf\tg\th",
+                board.showBoard());
     }
 
     @Test
     public void initialize() throws Exception {
-        Board board = new Board();
         board.initialize();
         assertEquals("♙♙♙♙♙♙♙♙", board.getWhitePawnsResult());
         assertEquals("♟♟♟♟♟♟♟♟", board.getBlackPawnsResult());
