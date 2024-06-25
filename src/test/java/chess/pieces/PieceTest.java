e554a03926ea1b13b8fdf33acbda999f82b7a434
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-06-25 13:59:45 +0900

    [Test] 각 기물들에 대해 black 인지 white인지 확인하는 메서드 검증 테스트 추가

--- src/test/java/chess/pieces/PieceTest.java
+++ src/test/java/chess/pieces/PieceTest.java
@@ -3,16 +3,17 @@ package chess.pieces;
 import org.junit.jupiter.api.Test;
 
 import static chess.pieces.PieceTypes.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PieceTest {
-    private void verifyPiece(final Piece piece,final PieceTypes type){
-        verifyPiece(piece,type.getColor(),type.getRepresentation(),type.getType());
+    private void verifyPiece(final Piece piece, final PieceTypes type) {
+        verifyPiece(piece, type.getColor(), type.getRepresentation(), type.getType());
     }
-    private void verifyPiece(final Piece piece,final String color,final char representation,final String type){
-        assertEquals(color,piece.getColor());
-        assertEquals(representation,piece.getRepresentation());
-        assertEquals(type,piece.getType());
+
+    private void verifyPiece(final Piece piece, final String color, final char representation, final String type) {
+        assertEquals(color, piece.getColor());
+        assertEquals(representation, piece.getRepresentation());
+        assertEquals(type, piece.getType());
     }
 
     @Test
@@ -32,15 +33,71 @@ public class PieceTest {
 
         verifyPiece(whitePawn, WHITE_PAWN);
         verifyPiece(blackPawn, BLACK_PAWN);
-        verifyPiece(whiteKnight,WHITE_KNIGHT);
-        verifyPiece(blackKnight,BLACK_KNIGHT);
-        verifyPiece(whiteRook,WHITE_ROOK);
-        verifyPiece(blackRook,BLACK_ROOK);
-        verifyPiece(whiteBishop,WHITE_BISHOP);
-        verifyPiece(blackBishop,BLACK_BISHOP);
-        verifyPiece(whiteQueen,WHITE_QUEEN);
-        verifyPiece(blackQueen,BLACK_QUEEN);
-        verifyPiece(whiteKing,WHITE_KING);
-        verifyPiece(blackKing,BLACK_KING);
+        verifyPiece(whiteKnight, WHITE_KNIGHT);
+        verifyPiece(blackKnight, BLACK_KNIGHT);
+        verifyPiece(whiteRook, WHITE_ROOK);
+        verifyPiece(blackRook, BLACK_ROOK);
+        verifyPiece(whiteBishop, WHITE_BISHOP);
+        verifyPiece(blackBishop, BLACK_BISHOP);
+        verifyPiece(whiteQueen, WHITE_QUEEN);
+        verifyPiece(blackQueen, BLACK_QUEEN);
+        verifyPiece(whiteKing, WHITE_KING);
+        verifyPiece(blackKing, BLACK_KING);
+    }
+
+    @Test
+    public void isBlackAndIsWhiteTestWithBlackPieces() {
+        Piece blackPawn = Piece.createPiece(BLACK_PAWN);
+        assertTrue(blackPawn.isBlack());
+        assertFalse(blackPawn.isWhite());
+
+        Piece blackKnight = Piece.createPiece(BLACK_KNIGHT);
+        assertTrue(blackKnight.isBlack());
+        assertFalse(blackKnight.isWhite());
+
+
+        Piece blackRook = Piece.createPiece(BLACK_ROOK);
+        assertTrue(blackRook.isBlack());
+        assertFalse(blackRook.isWhite());
+
+        Piece blackBishop = Piece.createPiece(BLACK_BISHOP);
+        assertTrue(blackBishop.isBlack());
+        assertFalse(blackBishop.isWhite());
+
+        Piece blackQueen = Piece.createPiece(BLACK_QUEEN);
+        assertTrue(blackQueen.isBlack());
+        assertFalse(blackQueen.isWhite());
+
+        Piece blackKing = Piece.createPiece(BLACK_KING);
+        assertTrue(blackKing.isBlack());
+        assertFalse(blackKing.isWhite());
+    }
+
+    @Test
+    public void isBlackAndIsWhiteTestWithWhitePieces() {
+        Piece whitePawn = Piece.createPiece(WHITE_PAWN);
+        assertFalse(whitePawn.isBlack());
+        assertTrue(whitePawn.isWhite());
+
+        Piece whiteKnight = Piece.createPiece(WHITE_KNIGHT);
+        assertFalse(whiteKnight.isBlack());
+        assertTrue(whiteKnight.isWhite());
+
+
+        Piece whiteRook = Piece.createPiece(WHITE_ROOK);
+        assertFalse(whiteRook.isBlack());
+        assertTrue(whiteRook.isWhite());
+
+        Piece whiteBishop = Piece.createPiece(WHITE_BISHOP);
+        assertFalse(whiteBishop.isBlack());
+        assertTrue(whiteBishop.isWhite());
+
+        Piece whiteQueen = Piece.createPiece(WHITE_QUEEN);
+        assertFalse(whiteQueen.isBlack());
+        assertTrue(whiteQueen.isWhite());
+
+        Piece whiteKing = Piece.createPiece(WHITE_KING);
+        assertFalse(whiteKing.isBlack());
+        assertTrue(whiteKing.isWhite());
     }
 }
