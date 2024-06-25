11bcd2d7ce7dbe201fd6ea3fc795f5893bc6c9bf
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-06-25 09:47:34 +0900

    [Feat] board 클래스의 initialize 메서드 추가
    처음 초기화 시 white pawn 8개, black pawn 8개 추가하는 로직 구현

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -1,14 +1,21 @@
 package chess;
 
 import chess.pieces.ChessPiece;
+import chess.pieces.Pawn;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Board {
+    private final int WIDTH = 8;
+    private final int HEIGHT = 8;
     private List<ChessPiece> pieces;
+    private List<Pawn> whitePawns;
+    private List<Pawn> blackPawns;
     public Board(){
         pieces = new ArrayList<>();
+        whitePawns = new ArrayList<>();
+        blackPawns = new ArrayList<>();
     }
 
     public int size(){
@@ -22,4 +29,32 @@ public class Board {
     public ChessPiece findPawn(int index){
         return pieces.get(index);
     }
+
+    public void initialize(){
+        for(int w=0;w<WIDTH;w++){
+            Pawn whitePawn = new Pawn(Pawn.WHITE_COLOR);
+            Pawn blackPawn = new Pawn(Pawn.BLACK_COLOR);
+            whitePawns.add(whitePawn);
+            blackPawns.add(blackPawn);
+            pieces.add(whitePawn);
+            pieces.add(blackPawn);
+        }
+    }
+
+    public String getWhitePawnsResult(){
+        return whitePawns.stream()
+                .map(Pawn::getRepresentation)
+                .map(String::valueOf)
+                .reduce((x,y)->x+y)
+                .get();
+    }
+
+    public String getBlackPawnsResult(){
+        return blackPawns.stream()
+                .map(Pawn::getRepresentation)
+                .map(String::valueOf)
+                .reduce((x,y)->x+y)
+                .get();
+    }
+
 }
