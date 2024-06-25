a43ee99cd7373e3aa9555fce4e466ff1b941b9e5
june-777 <wlwhswnsrl96@gmail.com>
2024-06-25 10:31:00 +0900

    feat: Pawn 기본 생성자 추가
    - 요구사항: 색이 없는 Pawn의 기본 색상은 흰색(white)이다.
    - 색상을 상수로 관리

--- src/main/java/woowa/camp/Pawn.java
+++ src/main/java/woowa/camp/Pawn.java
@@ -2,7 +2,14 @@ package woowa.camp;
 
 public class Pawn {
 
+    public static final String WHITE_COLOR = "white";
+    public static final String BLACK_COLOR = "black";
+
     private final String color;
+
+    public Pawn() {
+        this.color = WHITE_COLOR;
+    }
 
     public Pawn(String color) {
         this.color = color;
