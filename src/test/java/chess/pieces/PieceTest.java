50894a3e8a5087cc6b0896cf8fde80ea3d115b12
lass9436 <lass9436@gmail.com>
2024-06-25 16:54:50 +0900

    [test] PieceTest 작성
    PieceTest 를 작성하였습니다.
    팩토리 메서드를 만들어 흰색과 검은색의 폰, 나이트, 비숍, 룩, 퀸, 킹 피스 객체를 생성합니다.
    피스 생성 테스트를 만들어서 각 피스를 테스트하는 코드를 작성했습니다.

---
+++ src/test/java/chess/pieces/PieceTest.java
@@ -0,0 +1,29 @@
+package chess.pieces;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PieceTest {
+
+    @Test
+    public void 피스_생성() {
+        verifyPiece(Piece.createWhitePawn(), Piece.Color.WHITE, Piece.Representation.WHITE_PAWN.getSymbol());
+        verifyPiece(Piece.createBlackPawn(), Piece.Color.BLACK, Piece.Representation.BLACK_PAWN.getSymbol());
+        verifyPiece(Piece.createWhiteKnight(), Piece.Color.WHITE, Piece.Representation.WHITE_KNIGHT.getSymbol());
+        verifyPiece(Piece.createBlackKnight(), Piece.Color.BLACK, Piece.Representation.BLACK_KNIGHT.getSymbol());
+        verifyPiece(Piece.createWhiteBishop(), Piece.Color.WHITE, Piece.Representation.WHITE_BISHOP.getSymbol());
+        verifyPiece(Piece.createBlackBishop(), Piece.Color.BLACK, Piece.Representation.BLACK_BISHOP.getSymbol());
+        verifyPiece(Piece.createWhiteRook(), Piece.Color.WHITE, Piece.Representation.WHITE_ROOK.getSymbol());
+        verifyPiece(Piece.createBlackRook(), Piece.Color.BLACK, Piece.Representation.BLACK_ROOK.getSymbol());
+        verifyPiece(Piece.createWhiteQueen(), Piece.Color.WHITE, Piece.Representation.WHITE_QUEEN.getSymbol());
+        verifyPiece(Piece.createBlackQueen(), Piece.Color.BLACK, Piece.Representation.BLACK_QUEEN.getSymbol());
+        verifyPiece(Piece.createWhiteKing(), Piece.Color.WHITE, Piece.Representation.WHITE_KING.getSymbol());
+        verifyPiece(Piece.createBlackKing(), Piece.Color.BLACK, Piece.Representation.BLACK_KING.getSymbol());
+    }
+
+    private void verifyPiece(Piece piece, Piece.Color color, char symbol) {
+        assertEquals(color, piece.getColor());
+        assertEquals(symbol, piece.getRepresentation().getSymbol());
+    }
+}
