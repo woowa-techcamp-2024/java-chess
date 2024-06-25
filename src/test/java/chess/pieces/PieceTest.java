602bb8187f1ad572210ebdc22cd5a0ed301d288f
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-06-25 13:20:53 +0900

    [Test] 모든 종류의 기물을 생성한 뒤 검증하는 테스트 추가

--- src/test/java/chess/pieces/PieceTest.java
+++ src/test/java/chess/pieces/PieceTest.java
@@ -2,6 +2,7 @@ package chess.pieces;
 
 import org.junit.jupiter.api.Test;
 
+import static chess.pieces.PieceTypes.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PieceTest {
@@ -16,9 +17,30 @@ public class PieceTest {
 
     @Test
     public void create() {
-        Piece white = Piece.createPiece(PieceTypes.WHITE_PAWN);
-        Piece black = Piece.createPiece(PieceTypes.BLACK_PAWN);
-        verifyPiece(white, PieceTypes.WHITE_PAWN);
-        verifyPiece(black, PieceTypes.BLACK_PAWN);
+        Piece whitePawn = Piece.createPiece(WHITE_PAWN);
+        Piece blackPawn = Piece.createPiece(BLACK_PAWN);
+        Piece whiteKnight = Piece.createPiece(WHITE_KNIGHT);
+        Piece blackKnight = Piece.createPiece(BLACK_KNIGHT);
+        Piece whiteRook = Piece.createPiece(WHITE_ROOK);
+        Piece blackRook = Piece.createPiece(BLACK_ROOK);
+        Piece whiteBishop = Piece.createPiece(WHITE_BISHOP);
+        Piece blackBishop = Piece.createPiece(BLACK_BISHOP);
+        Piece whiteQueen = Piece.createPiece(WHITE_QUEEN);
+        Piece blackQueen = Piece.createPiece(BLACK_QUEEN);
+        Piece whiteKing = Piece.createPiece(WHITE_KING);
+        Piece blackKing = Piece.createPiece(BLACK_KING);
+
+        verifyPiece(whitePawn, WHITE_PAWN);
+        verifyPiece(blackPawn, BLACK_PAWN);
+        verifyPiece(whiteKnight,WHITE_KNIGHT);
+        verifyPiece(blackKnight,BLACK_KNIGHT);
+        verifyPiece(whiteRook,WHITE_ROOK);
+        verifyPiece(blackRook,BLACK_ROOK);
+        verifyPiece(whiteBishop,WHITE_BISHOP);
+        verifyPiece(blackBishop,BLACK_BISHOP);
+        verifyPiece(whiteQueen,WHITE_QUEEN);
+        verifyPiece(blackQueen,BLACK_QUEEN);
+        verifyPiece(whiteKing,WHITE_KING);
+        verifyPiece(blackKing,BLACK_KING);
     }
 }
