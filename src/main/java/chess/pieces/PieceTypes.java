306ff06a37790347cdc613b601d56510cf645376
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-06-25 17:00:52 +0900

    [Feat] 기물들의 타입별 점수를 추가

--- src/main/java/chess/pieces/PieceTypes.java
+++ src/main/java/chess/pieces/PieceTypes.java
@@ -18,14 +18,25 @@ public enum PieceTypes {
         WHITE,BLACK,NOCOLOR;
     }
     public enum Type{
-        PAWN('p'),ROOK('r'),KNIGHT('n'),BISHOP('b'),QUEEN('q'),KING('k'),NO_PIECE('.');
+        PAWN('p',1.0),
+        ROOK('r',5.0),
+        KNIGHT('n',2.5),
+        BISHOP('b',3.0),
+        QUEEN('q',9.0),
+        KING('k',0.0)
+        ,NO_PIECE('.',0.0);
         private final char representation;
-        Type(char representation){
+        private final double point;
+        Type(char representation,double point){
             this.representation = representation;
+            this.point = point;
         }
 
         public char getRepresentation(){
             return this.representation;
+        }
+        public double getPoint(){
+            return this.point;
         }
     }
 
@@ -33,12 +44,14 @@ public enum PieceTypes {
     private final Color color;
     private final Type type;
     private final char representation;
+    private final double point;
 
     PieceTypes(Color color, Type type) {
         this.color = color;
         this.type = type;
-        char representation = this.type.getRepresentation();
+        char representation = type.getRepresentation();
         this.representation = color.equals(Color.WHITE) ? representation : Character.toUpperCase(representation);
+        this.point = type.getPoint();
     }
 
     public Color getColor() {
@@ -51,5 +64,9 @@ public enum PieceTypes {
 
     public char getRepresentation() {
         return representation;
+    }
+
+    public double getPoint(){
+        return point;
     }
 }
