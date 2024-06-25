fb8fc1d24545c2f0e52263e339bb2551aba8a845
yudonggeun <ydong98@gmail.com>
2024-06-25 17:08:26 +0900

    refactor : import 정리

--- src/test/java/com/example/demo/context/BoardTest.java
+++ src/test/java/com/example/demo/context/BoardTest.java
@@ -7,11 +7,9 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class BoardTest {
