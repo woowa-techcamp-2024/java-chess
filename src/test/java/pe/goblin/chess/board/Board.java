56fc0b18a7f9067b73d23aa7c5454f6b298ed2c3
donghar <donghar@naver.com>
2024-06-25 09:55:25 +0900

    [Test] Board는 Pawn 이외의 다른 객체를 추가할 수 없다.

--- src/test/java/pe/goblin/chess/board/Board.java
+++ src/test/java/pe/goblin/chess/board/Board.java
@@ -5,6 +5,12 @@ import pe.goblin.chess.pawn.Pawn;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 체스 판에 Pawn 이외의 객체가 추가되지 않도록 한다.
+ * Pawn 이외의 다른 객체를 추가하면 컴파일 에러가 발생한다.
+ *
+ * @see #add(Pawn)
+ */
 public class Board {
     private static final int MAX_PAWN_PIECE = 2;
 
