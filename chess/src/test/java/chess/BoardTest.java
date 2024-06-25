8744b3d0b183d2edc10f6ead5c8f5dcd04dd324e
huiseung <whtjddn75@gmail.com>
2024-06-25 10:30:48 +0900

    [test] BoardTest initialize method 추가 - Pawn 초기 배치 확인

--- chess/src/test/java/chess/BoardTest.java
+++ chess/src/test/java/chess/BoardTest.java
@@ -3,10 +3,14 @@ package chess;
 import org.junit.jupiter.api.Test;
 import pieces.Pawn;
 import pieces.PawnColor;
+import pieces.PieceUnicode;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BoardTest {
+
+    private final int NUM_COL = 8;
+
     @Test
     public void create() throws Exception
     {
@@ -21,5 +25,15 @@ public class BoardTest {
         board.add(blackPawn);
         assertEquals(2, board.getSize());
         assertEquals(blackPawn, board.findPawn(1));
+    }
+
+
+    @Test
+    public void initialize() throws Exception
+    {
+        Board board = new Board();
+        board.initialize();
+        assertEquals(PieceUnicode.WHITE_PAWN.getUnicode().repeat(NUM_COL), board.getWhitePawnsResult());
+        assertEquals(PieceUnicode.BLACK_PAWN.getUnicode().repeat(NUM_COL), board.getBlackPawnsResult());
     }
 }
