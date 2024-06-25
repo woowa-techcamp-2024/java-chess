6b5dde76f5de605e434cbbc2dbeff6c695ec862f
hek-git <ghddmsrl1@naver.com>
2024-06-25 14:21:11 +0900

    test: BoardTest 추가 Board 클래스 추가를 위한 BoardTest 클래스 추가

---
+++ src/test/java/BoardTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BoardTest {
+
+    @Test
+    public void create() throws Exception {
+        Board board = new Board();
+
+        Pawn white = new Pawn(Pawn.WHITE_COLOR);
+        board.add(white);
+        assertEquals(1, board.size());
+        assertEquals(white, board.findPawn(0));
+
+        Pawn black = new Pawn(Pawn.BLACK_COLOR);
+        board.add(black);
+        assertEquals(2, board.size());
+        assertEquals(black, board.findPawn(1));
+    }
+}
