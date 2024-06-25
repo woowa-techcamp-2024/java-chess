dbfed60fe5abd6b18f6f2485b7d44c7b1fa588e7
mirageoasis <kimhw0820@naver.com>
2024-06-25 16:40:39 +0900

    test: type형 추가 및 representation 함수 삭제

--- src/main/java/org/example/pieces/Piece.java
+++ src/main/java/org/example/pieces/Piece.java
@@ -9,26 +9,31 @@ public class Piece {
     }
 
     public enum Type {
-        PAWN, ROOK, KNIGHT, BISHOP, QUEEN, KING, NO_PIECE;
+        PAWN('♙'),
+        ROOK('♖'),
+        KNIGHT('♘'),
+        BISHOP('♗'),
+        QUEEN('♕'),
+        KING('♔'),
+        NO_PIECE(' ');  // 빈 문자로 설정
+
+        private final char representation;
+
+
+        Type(char representation) {
+            this.representation = representation;
+        }
+
+        public char getWhiteRepresentation() {
+            return representation;
+        }
+
+        public char getBlackRepresentation() {
+            final int offsetNumber = 6;
+            return (char)(representation+ offsetNumber);
+        }
     }
 
-    public static final char WHITE_PAWN_REPRESENTATION = '♙';
-    public static final char BLACK_PAWN_REPRESENTATION = '♟';
-
-    public static final char WHITE_KNIGHT_REPRESENTATION = '♘';
-    public static final char BLACK_KNIGHT_REPRESENTATION = '♞';
-
-    public static final char WHITE_ROOK_REPRESENTATION = '♖';
-    public static final char BLACK_ROOK_REPRESENTATION = '♜';
-
-    public static final char WHITE_BISHOP_REPRESENTATION = '♗';
-    public static final char BLACK_BISHOP_REPRESENTATION = '♝';
-
-    public static final char WHITE_QUEEN_REPRESENTATION = '♕';
-    public static final char BLACK_QUEEN_REPRESENTATION = '♛';
-
-    public static final char WHITE_KING_REPRESENTATION = '♔';
-    public static final char BLACK_KING_REPRESENTATION = '♚';
     private final Color color;
     private final char representation;
 
@@ -38,51 +43,51 @@ public class Piece {
     }
 
     public static Piece createWhitePawn() {
-        return new Piece(WHITE, WHITE_PAWN_REPRESENTATION);
+        return new Piece(WHITE, Type.PAWN.getWhiteRepresentation());
     }
 
     public static Piece createBlackPawn() {
-        return new Piece(BLACK, BLACK_PAWN_REPRESENTATION);
+        return new Piece(BLACK, Type.PAWN.getBlackRepresentation());
     }
 
     public static Piece createWhiteKnight() {
-        return new Piece(WHITE, WHITE_KNIGHT_REPRESENTATION);
+        return new Piece(WHITE, Type.KNIGHT.getWhiteRepresentation());
     }
 
     public static Piece createBlackKnight() {
-        return new Piece(BLACK, BLACK_KNIGHT_REPRESENTATION);
+        return new Piece(BLACK, Type.KNIGHT.getBlackRepresentation());
     }
 
     public static Piece createWhiteRook() {
-        return new Piece(WHITE, WHITE_ROOK_REPRESENTATION);
+        return new Piece(WHITE, Type.ROOK.getWhiteRepresentation());
     }
 
     public static Piece createBlackRook() {
-        return new Piece(BLACK, BLACK_ROOK_REPRESENTATION);
+        return new Piece(BLACK, Type.ROOK.getBlackRepresentation());
     }
 
     public static Piece createWhiteBishop() {
-        return new Piece(WHITE, WHITE_BISHOP_REPRESENTATION);
+        return new Piece(WHITE, Type.BISHOP.getWhiteRepresentation());
     }
 
     public static Piece createBlackBishop() {
-        return new Piece(BLACK, BLACK_BISHOP_REPRESENTATION);
+        return new Piece(BLACK, Type.BISHOP.getBlackRepresentation());
     }
 
     public static Piece createWhiteQueen() {
-        return new Piece(WHITE, WHITE_QUEEN_REPRESENTATION);
+        return new Piece(WHITE, Type.QUEEN.getWhiteRepresentation());
     }
 
     public static Piece createBlackQueen() {
-        return new Piece(BLACK, BLACK_QUEEN_REPRESENTATION);
+        return new Piece(BLACK, Type.QUEEN.getBlackRepresentation());
     }
 
     public static Piece createWhiteKing() {
-        return new Piece(WHITE, WHITE_KING_REPRESENTATION);
+        return new Piece(WHITE, Type.KING.getWhiteRepresentation());
     }
 
     public static Piece createBlackKing() {
-        return new Piece(BLACK, BLACK_KING_REPRESENTATION);
+        return new Piece(BLACK, Type.KING.getBlackRepresentation());
     }
 
     public Color getColor() {
