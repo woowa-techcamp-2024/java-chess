e126ba10831370b2afddc4552f5c69367418f2a9
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-06-25 13:23:30 +0900

    [Feat] PieceTypes에 모든 기물에 대한 정보 입력

--- src/main/java/chess/pieces/PieceTypes.java
+++ src/main/java/chess/pieces/PieceTypes.java
@@ -2,7 +2,18 @@ package chess.pieces;
 
 public enum PieceTypes {
     WHITE_PAWN("white","pawn",'p'),
-    BLACK_PAWN("black","pawn",'P');
+    BLACK_PAWN("black","pawn",'P'),
+    WHITE_KNIGHT("white","knight",'n'),
+    BLACK_KNIGHT("black","knight",'N'),
+    WHITE_ROOK("white","rook",'r'),
+    BLACK_ROOK("black","rook",'R'),
+    WHITE_BISHOP("white","bishop",'b'),
+    BLACK_BISHOP("black","bishop",'B'),
+    WHITE_QUEEN("white","queen",'q'),
+    BLACK_QUEEN("black","queen",'Q'),
+    WHITE_KING("white","king",'k'),
+    BLACK_KING("black","king",'K'),
+    ;
 
     private final String color;
     private final String type;
