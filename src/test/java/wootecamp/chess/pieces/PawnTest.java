7f80fff689514252cc9ea7208c5049524858925c
seminchoi <choicco89@gmail.com>
2024-06-25 14:04:49 +0900

    feat: PawnTest가 Pawn의 representation도 검증할 수 있도록 테스트케이스 수정
    - 흰색 Pawn과 검은색 Pawn이 각각의 representation을 가지고 있는지 검증

--- src/test/java/wootecamp/chess/pieces/PawnTest.java
+++ src/test/java/wootecamp/chess/pieces/PawnTest.java
@@ -10,13 +10,14 @@ public class PawnTest {
     @Test
     @DisplayName("생성자 파라미터에 색깔을 전달해서 폰을 생성한다.")
     void create() {
-        verifyPawn(Pawn.WHITE_COLOR);
-        verifyPawn(Pawn.BLACK_COLOR);
+        verifyPawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION);
+        verifyPawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION);
     }
 
-    void verifyPawn(final String color) {
+    void verifyPawn(final String color, final String representation) {
         final Pawn pawn = new Pawn(color);
         assertThat(pawn.getColor()).isEqualTo(color);
+        assertThat(pawn.getRepresentation()).isEqualTo(representation);
     }
 
     @Test
@@ -24,5 +25,6 @@ public class PawnTest {
     void create_defaultConstructor() {
         Pawn pawn = new Pawn();
         assertThat(pawn.getColor()).isEqualTo(Pawn.WHITE_COLOR);
+        assertThat(pawn.getRepresentation()).isEqualTo(Pawn.WHITE_REPRESENTATION);
     }
 }
