ec623f2e6fc16c349f159a5c3bab6a7e541107d1
donghar <donghar@naver.com>
2024-06-25 17:24:58 +0900

    [Test] 전체 기물의 상태를 볼 수 있는 체스판
    BoardTest에 기존의 불필요한 테스트 삭제 및 초기화 테스트 구현

--- src/test/java/pe/goblin/chess/board/BoardTest.java
+++ src/test/java/pe/goblin/chess/board/BoardTest.java
@@ -2,12 +2,10 @@ package pe.goblin.chess.board;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import pe.goblin.chess.piece.Piece;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static pe.goblin.chess.utils.StringUtils.appendNewLine;
 
 public class BoardTest {
     private Board board;
@@ -18,60 +16,17 @@ public class BoardTest {
     }
 
     @Test
-    @DisplayName("체스 판 위에 말이 없는 상태에서 시작한다.")
-    void create_empty() {
-        assertEquals(0, board.size());
-    }
-
-    @Test
-    @DisplayName("Pawn을 체스 판에 추가할 수 있다.")
-    void create() throws ExceedPawnException {
-        Piece white = Piece.createWhitePawn();
-        board.add(white);
-        assertEquals(1, board.size());
-        assertEquals(white, board.findPawn(0));
-
-        Piece black = Piece.createBlackPawn();
-        board.add(black);
-        assertEquals(2, board.size());
-        assertEquals(black, board.findPawn(1));
-    }
-
-    @Nested
-    @DisplayName("Pawn을 추가할 때마다 Pawn의 수가 맞는지 확인한다.")
-    class create_exceed_then_throw {
-        @Test
-        @DisplayName("white pawn을 추가하는 경우")
-        void in_case_of_white_pawn() throws ExceedPawnException {
-            for (int i = Board.MIN_ROWS; i < Board.MAX_ROWS; i++) {
-                board.add(Piece.createWhitePawn());
-            }
-            assertThrows(Exception.class, () -> board.add(Piece.createWhitePawn()));
-        }
-
-        @Test
-        @DisplayName("black pawn을 추가하는 경우")
-        void in_case_of_black_pawn() throws ExceedPawnException {
-            for (int i = Board.MIN_ROWS; i < Board.MAX_ROWS; i++) {
-                board.add(Piece.createBlackPawn());
-            }
-            assertThrows(Exception.class, () -> board.add(Piece.createBlackPawn()));
-        }
-    }
-
-    @Test
     @DisplayName("Board는 초기화할 때 흰색 Pawn과 검은색 Pawn을 생성해 저장할 수 있다.")
-    void initialize() {
+    public void create() throws Exception {
         board.initialize();
-        assertEquals("pppppppp", board.getWhitePawnsResult());
-        assertEquals("PPPPPPPP", board.getBlackPawnsResult());
-    }
-
-    @Test
-    @DisplayName("체스판의 현재 상태를 콘솔 화면에 출력한다")
-    void print() {
-        board.initialize();
-        String result = board.print();
-        System.out.println(result);
+        assertEquals(32, board.pieceCount());
+        String blankRank = appendNewLine("........");
+        assertEquals(
+                appendNewLine("RNBQKBNR") +
+                        appendNewLine("PPPPPPPP") +
+                        blankRank + blankRank + blankRank + blankRank +
+                        appendNewLine("pppppppp") +
+                        appendNewLine("rnbqkbnr"),
+                board.showBoard());
     }
 }
