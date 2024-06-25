e008c56d538803429b5e72f5606e657fed1a9d01
miiiinju1 <gms07073@ynu.ac.kr>
2024-06-25 13:03:25 +0900

    feat: StringUtils에 appendNewLine 메서드 구현, String과 StringBuilder모두 이용할 수 있도록 오버로딩

--- src/main/java/chess/utils/StringUtils.java
+++ src/main/java/chess/utils/StringUtils.java
@@ -4,5 +4,14 @@ public class StringUtils {
 
     public static final String NEWLINE = System.getProperty("line.separator");
 
+    public static StringBuilder appendNewLine(StringBuilder sb) {
+        sb.append(NEWLINE);
+        return sb;
+    }
+
+    public static String appendNewLine(String str) {
+        return str + NEWLINE;
+    }
+
     private StringUtils() {}
 }
