e055d646cb951fff11534f37e61b5c7a02dd8157
KoKimSS <seungsu8848@ajou.ac.kr>
2024-06-25 12:55:19 +0900

    test : 체스판 초기화 테스트 코드 작성

--- woowa_camp_chess/src/test/java/org/example/board/BoardTest.java
+++ woowa_camp_chess/src/test/java/org/example/board/BoardTest.java
@@ -23,4 +23,12 @@ public class BoardTest {
         assertEquals(2, board.size());
         assertEquals(black, board.findPawn(1));
     }
+
+    @Test
+    public void initialize() {
+        Board board = new Board();
+        board.initialize();
+        assertEquals("pppppppp", board.getWhitePawnsResult());
+        assertEquals("PPPPPPPP", board.getBlackPawnsResult());
+    }
 }
