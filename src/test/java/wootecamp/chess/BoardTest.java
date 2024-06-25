014f4ea6b92000cedf5519a96b519237f1c2924c
seminchoi <choicco89@gmail.com>
2024-06-25 17:13:09 +0900

    test: 모든 기물이 초기화 된 체스판을 확인하는 테스트 케이스 작성

--- src/test/java/wootecamp/chess/BoardTest.java
+++ src/test/java/wootecamp/chess/BoardTest.java
@@ -3,9 +3,9 @@ package wootecamp.chess;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import wootecamp.chess.util.StringUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static wootecamp.chess.util.StringUtils.appendNewline;
 
 public class BoardTest {
 
@@ -20,31 +20,15 @@ public class BoardTest {
     @DisplayName("체스판을 초기화한다.")
     void initialize() {
         board.initialize();
-        assertThat(board.getInitialStateWhitePawnResult()).isEqualTo("pppppppp");
-        assertThat(board.getInitialStateBlackPawnResult()).isEqualTo("PPPPPPPP");
-    }
+        assertThat(board.pieceCount()).isEqualTo(32);
 
-    @Test
-    @DisplayName("체스판 초기 상태를 확인한다.")
-    void getBoardState() {
-        final String emptyRank = "........";
-        final String whitePawnRank = "pppppppp";
-        final String blackPawnRank = "PPPPPPPP";
-        final String newline = StringUtils.NEWLINE;
-
-        board.initialize();
-
-        String expectedState = emptyRank + newline +
-                blackPawnRank + newline +
-                emptyRank + newline +
-                emptyRank + newline +
-                emptyRank + newline +
-                emptyRank + newline +
-                whitePawnRank + newline +
-                emptyRank;
-
-        String initBoardState = board.getBoardState();
-        System.out.println(initBoardState);
-        assertThat(initBoardState).isEqualTo(expectedState);
+        String blankRank = appendNewline("........");
+        assertThat(board.showBoard()).isEqualTo(
+                appendNewline("RNBQKBNR") +
+                        appendNewline("PPPPPPPP") +
+                        blankRank + blankRank + blankRank + blankRank +
+                        appendNewline("pppppppp") +
+                        appendNewline("rnbqkbnr")
+                );
     }
 }
