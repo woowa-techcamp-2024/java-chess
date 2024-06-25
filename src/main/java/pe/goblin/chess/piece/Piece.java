4ef90f173f14490b628b6bef31c4a536e3f52f7f
donghar <donghar@naver.com>
2024-06-25 17:24:00 +0900

    [Feat] 팩토리 메소드 생성
    Piece 생성을 위한 팩토리 메서드 구현

--- src/main/java/pe/goblin/chess/piece/Piece.java
+++ src/main/java/pe/goblin/chess/piece/Piece.java
@@ -3,8 +3,18 @@ package pe.goblin.chess.piece;
 public class Piece {
     public static final String WHITE_COLOR = "white";
     public static final String BLACK_COLOR = "black";
-    public static final char WHITE_REPRESENTATION = 'p';
-    public static final char BLACK_REPRESENTATION = 'P';
+    public static final char WHITE_PAWN_REPRESENTATION = 'p';
+    public static final char BLACK_PAWN_REPRESENTATION = 'P';
+    public static final char WHITE_KING_REPRESENTATION = 'k';
+    public static final char BLACK_KING_REPRESENTATION = 'K';
+    public static final char WHITE_QUEEN_REPRESENTATION = 'q';
+    public static final char BLACK_QUEEN_REPRESENTATION = 'Q';
+    public static final char WHITE_ROOK_REPRESENTATION = 'r';
+    public static final char BLACK_ROOK_REPRESENTATION = 'R';
+    public static final char WHITE_BISHOP_REPRESENTATION = 'b';
+    public static final char BLACK_BISHOP_REPRESENTATION = 'B';
+    public static final char WHITE_KNIGHT_REPRESENTATION = 'n';
+    public static final char BLACK_KNIGHT_REPRESENTATION = 'N';
 
     private final String color;
     private final String name;
@@ -17,15 +27,59 @@ public class Piece {
     }
 
     public static Piece createWhitePawn() {
-        return new Piece(WHITE_COLOR, "pawn", WHITE_REPRESENTATION);
+        return new Piece(WHITE_COLOR, "pawn", WHITE_PAWN_REPRESENTATION);
     }
 
     public static Piece createBlackPawn() {
-        return new Piece(BLACK_COLOR, "pawn", BLACK_REPRESENTATION);
+        return new Piece(BLACK_COLOR, "pawn", BLACK_PAWN_REPRESENTATION);
+    }
+
+    public static Piece createWhiteKing() {
+        return new Piece(WHITE_COLOR, "king", WHITE_KING_REPRESENTATION);
+    }
+
+    public static Piece createBlackKing() {
+        return new Piece(BLACK_COLOR, "king", BLACK_KING_REPRESENTATION);
+    }
+
+    public static Piece createWhiteQueen() {
+        return new Piece(WHITE_COLOR, "queen", WHITE_QUEEN_REPRESENTATION);
+    }
+
+    public static Piece createBlackQueen() {
+        return new Piece(BLACK_COLOR, "queen", BLACK_QUEEN_REPRESENTATION);
+    }
+
+    public static Piece createWhiteRook() {
+        return new Piece(WHITE_COLOR, "rook", WHITE_ROOK_REPRESENTATION);
+    }
+
+    public static Piece createBlackRook() {
+        return new Piece(BLACK_COLOR, "rook", BLACK_ROOK_REPRESENTATION);
+    }
+
+    public static Piece createWhiteBishop() {
+        return new Piece(WHITE_COLOR, "bishop", WHITE_BISHOP_REPRESENTATION);
+    }
+
+    public static Piece createBlackBishop() {
+        return new Piece(BLACK_COLOR, "bishop", BLACK_BISHOP_REPRESENTATION);
+    }
+
+    public static Piece createWhiteKnight() {
+        return new Piece(WHITE_COLOR, "knight", WHITE_KNIGHT_REPRESENTATION);
+    }
+
+    public static Piece createBlackKnight() {
+        return new Piece(BLACK_COLOR, "knight", BLACK_KNIGHT_REPRESENTATION);
     }
 
     public String getColor() {
         return this.color;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public char getRepresentation() {
