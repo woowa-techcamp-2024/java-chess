010e2c096a9c17452c82ccef06e79cfcc64c22d0
seminchoi <choicco89@gmail.com>
2024-06-25 10:54:09 +0900

    feat: Board 클래스 구현
    - Pawn들을 저장할 수 있는 멤버변수 선언
    - Pawn을 추가하는 add 메서드 구현
    - Board의 size를 확인할 수 있는 메서드 구현
    - 저장된 Pawn을 반환하는 메서드 구현

---
+++ src/main/java/wootecamp/chess/Board.java
@@ -0,0 +1,20 @@
+package wootecamp.chess;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Board {
+    private List<Pawn> pawns = new ArrayList<>();
+
+    public void add(Pawn pawn) {
+        pawns.add(pawn);
+    }
+
+    public int size() {
+        return pawns.size();
+    }
+
+    public Pawn findPawn(int index) {
+        return pawns.get(index);
+    }
+}
