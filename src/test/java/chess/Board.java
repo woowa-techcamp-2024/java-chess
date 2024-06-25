669c0c2b7854a967102be5d3d31b18ed0d4bb512
lass9436 <lass9436@gmail.com>
2024-06-25 14:14:02 +0900

    [feat] Board 의 initialize 작성하고 add 와 size 와 findPawn 메서드를 수정했습니다.
    원래 하나의 List<Pawn> 필드를 가지고 있었는데, 검은색과 흰색을 구분하기 위해서 리스트가 추가되었습니다.
    그에 맞춰서 add 메서드와 size 메서드, findPawn 메서드를 수정하였습니다.
    Pawn Color 를 기준으로 메서드를 수정하였습니다.

--- src/test/java/chess/Board.java
+++ src/test/java/chess/Board.java
@@ -7,17 +7,51 @@ import java.util.List;
 
 public class Board {
 
-    private final List<Pawn> pawns = new ArrayList<>();
+    private final List<Pawn> whitePawns = new ArrayList<>();
+    private final List<Pawn> blackPawns = new ArrayList<>();
 
     public void add(Pawn pawn) {
-        pawns.add(pawn);
+        if (pawn.getColor() == Pawn.Color.WHITE) {
+            whitePawns.add(pawn);
+        } else {
+            blackPawns.add(pawn);
+        }
     }
 
     public int size() {
-        return pawns.size();
+        return whitePawns.size() + blackPawns.size();
     }
 
-    public Pawn findPawn(int i) {
-        return pawns.get(i);
+    public Pawn findPawn(int i, Pawn.Color color) {
+        if (color == Pawn.Color.WHITE) {
+            return whitePawns.get(i);
+        } else {
+            return blackPawns.get(i);
+        }
+    }
+
+    public void initialize() {
+        whitePawns.clear();
+        blackPawns.clear();
+        for (int i = 0; i < 8; i++) {
+            whitePawns.add(new Pawn(Pawn.Color.WHITE));
+            blackPawns.add(new Pawn(Pawn.Color.BLACK));
+        }
+    }
+
+    public String getWhitePawnsResult() {
+        StringBuilder builder = new StringBuilder();
+        for (Pawn pawn : whitePawns) {
+            builder.append(pawn.getRepresentation());
+        }
+        return builder.toString();
+    }
+
+    public String getBlackPawnsResult() {
+        StringBuilder builder = new StringBuilder();
+        for (Pawn pawn : blackPawns) {
+            builder.append(pawn.getRepresentation());
+        }
+        return builder.toString();
     }
 }
