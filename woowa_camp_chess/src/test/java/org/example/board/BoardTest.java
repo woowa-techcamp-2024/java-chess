5aee37784716a95dae68e9a21a0a0fc3f60aa8a6
woowatech05 <woowatech05@woowatech05ui-MacBookPro.local>
2024-06-25 10:47:15 +0900

    test : 보드판 테스트 코드 작성

--- woowa_camp_chess/src/test/java/org/example/board/BoardTest.java
+++ woowa_camp_chess/src/test/java/org/example/board/BoardTest.java
@@ -6,6 +6,7 @@ import org.example.chess.pieces.Pawn;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+//import static org.example.chess.pieces.Pawn.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BoardTest {
@@ -15,10 +16,10 @@ public class BoardTest {
     public void create() {
         Board board = new Board();
 
-        Pawn white = new Pawn(Pawn.WHITE_COLOR);
+        Pawn white = new Pawn(WHITE_COLOR);
         verifyAddPawn(board, white, 1, 0);
 
-        Pawn black = new Pawn(Pawn.BLACK_COLOR);
+        Pawn black = new Pawn(BLACK_COLOR);
         verifyAddPawn(board, black, 2, 1);
     }
 
