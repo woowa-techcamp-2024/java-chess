c11f9a3b72a61f3ef656891b3b1102b1d4585167
KyuWon  Kim <kyuwon0718@gmail.com>
2024-06-25 17:22:04 +0900

    refactor: board 리팩토링

--- java-chess/src/main/java/org/example/chess/Board.java
+++ java-chess/src/main/java/org/example/chess/Board.java
@@ -1,6 +1,6 @@
 package org.example.chess;
 
-import org.example.chess.pieces.Pawn;
+import org.example.chess.pieces.Piece;
 import org.example.chess.pieces.global.Position;
 
 import java.util.ArrayList;
@@ -9,60 +9,81 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Board {
-    private List<Pawn> pawns;
+    private List<PieceOnBoard> pieces;
 
     public Board() {
-        pawns = new ArrayList<>();
+        pieces = new ArrayList<>();
     }
 
-    public void add(Pawn pawn) {
-        pawns.add(pawn);
+    public void add(Piece piece) {
+        pieces.add(new PieceOnBoard(piece, new Position(0, 0)));
     }
 
-    public void add(Pawn pawn, Position position) {
-        pawns.add(pawn);
-        pawn.setPosition(position);
+    public void addWithPos(Piece piece, Position position) {
+        pieces.add(new PieceOnBoard(piece, position));
     }
 
-    public int size() {
-        return pawns.size();
+    public int pieceCount() {
+        return pieces.size();
     }
 
-    public Pawn findPawn(int idx) {
-        return pawns.get(idx);
+    public Piece findPawn(int idx) {
+        return pieces.get(idx).piece;
     }
 
     public String getWhitePawnsRepresentation() {
-        return pawns.stream().filter(Pawn::isWhite).map(Pawn::getRepresentation).collect(Collectors.joining());
+        return pieces.stream().filter(pieceOnBoard -> pieceOnBoard.piece.isWhite())
+                .filter(pieceOnBoard -> pieceOnBoard.piece.getName() == Piece.Name.PAWN)
+                .map(pieceOnBoard ->pieceOnBoard.piece.getRepresentation()).collect(Collectors.joining());
     }
 
     public String getBlackPawnsRespresentation() {
-        return pawns.stream().filter(Pawn::isBlack).map(Pawn::getRepresentation).collect(Collectors.joining());
+        return pieces.stream().filter(pieceOnBoard -> pieceOnBoard.piece.isBlack())
+                .filter(pieceOnBoard -> pieceOnBoard.piece.getName() == Piece.Name.PAWN)
+                .map(pieceOnBoard -> pieceOnBoard.piece.getRepresentation()).collect(Collectors.joining());
     }
 
     public void initialize() {
-        this.add(new Pawn(Pawn.Color.BLACK), new Position(1, 0));
-        this.add(new Pawn(Pawn.Color.BLACK), new Position(1, 1));
-        this.add(new Pawn(Pawn.Color.BLACK), new Position(1, 2));
-        this.add(new Pawn(Pawn.Color.BLACK), new Position(1, 3));
-        this.add(new Pawn(Pawn.Color.BLACK), new Position(1, 4));
-        this.add(new Pawn(Pawn.Color.BLACK), new Position(1, 5));
-        this.add(new Pawn(Pawn.Color.BLACK), new Position(1, 6));
-        this.add(new Pawn(Pawn.Color.BLACK), new Position(1, 7));
+        this.addWithPos(Piece.createBlackRook(), new Position(0, 0));
+        this.addWithPos(Piece.createBlackKnight(), new Position(0, 1));
+        this.addWithPos(Piece.createBlackBishop(), new Position(0, 2));
+        this.addWithPos(Piece.createBlackQueen(), new Position(0, 3));
+        this.addWithPos(Piece.createBlackKing(), new Position(0, 4));
+        this.addWithPos(Piece.createBlackBishop(), new Position(0, 5));
+        this.addWithPos(Piece.createBlackKnight(), new Position(0, 6));
+        this.addWithPos(Piece.createBlackRook(), new Position(0, 7));
 
-        this.add(new Pawn(Pawn.Color.WHITE), new Position(6, 0));
-        this.add(new Pawn(Pawn.Color.WHITE), new Position(6, 1));
-        this.add(new Pawn(Pawn.Color.WHITE), new Position(6, 2));
-        this.add(new Pawn(Pawn.Color.WHITE), new Position(6, 3));
-        this.add(new Pawn(Pawn.Color.WHITE), new Position(6, 4));
-        this.add(new Pawn(Pawn.Color.WHITE), new Position(6, 5));
-        this.add(new Pawn(Pawn.Color.WHITE), new Position(6, 6));
-        this.add(new Pawn(Pawn.Color.WHITE), new Position(6, 7));
+        this.addWithPos(Piece.createBlackPawn(), new Position(1, 0));
+        this.addWithPos(Piece.createBlackPawn(), new Position(1, 1));
+        this.addWithPos(Piece.createBlackPawn(), new Position(1, 2));
+        this.addWithPos(Piece.createBlackPawn(), new Position(1, 3));
+        this.addWithPos(Piece.createBlackPawn(), new Position(1, 4));
+        this.addWithPos(Piece.createBlackPawn(), new Position(1, 5));
+        this.addWithPos(Piece.createBlackPawn(), new Position(1, 6));
+        this.addWithPos(Piece.createBlackPawn(), new Position(1, 7));
 
-        this.print();
+        this.addWithPos(Piece.createWhitePawn(), new Position(6, 0));
+        this.addWithPos(Piece.createWhitePawn(), new Position(6, 1));
+        this.addWithPos(Piece.createWhitePawn(), new Position(6, 2));
+        this.addWithPos(Piece.createWhitePawn(), new Position(6, 3));
+        this.addWithPos(Piece.createWhitePawn(), new Position(6, 4));
+        this.addWithPos(Piece.createWhitePawn(), new Position(6, 5));
+        this.addWithPos(Piece.createWhitePawn(), new Position(6, 6));
+        this.addWithPos(Piece.createWhitePawn(), new Position(6, 7));
+
+        this.addWithPos(Piece.createWhiteRook(), new Position(7, 0));
+        this.addWithPos(Piece.createWhiteKnight(), new Position(7, 1));
+        this.addWithPos(Piece.createWhiteBishop(), new Position(7, 2));
+        this.addWithPos(Piece.createWhiteQueen(), new Position(7, 3));
+        this.addWithPos(Piece.createWhiteKing(), new Position(7, 4));
+        this.addWithPos(Piece.createWhiteBishop(), new Position(7, 5));
+        this.addWithPos(Piece.createWhiteKnight(), new Position(7, 6));
+        this.addWithPos(Piece.createWhiteRook(), new Position(7, 7));
+
+        this.showBoard();
     }
 
-    public void print() {
+    public String showBoard() {
         StringBuilder sb = new StringBuilder();
 
         String[][] map = new String[8][8];
@@ -71,15 +92,31 @@ public class Board {
             Arrays.fill(row, ".");
         }
 
-        pawns.forEach(pawn -> {
-            Position pos = pawn.getPosition();
-            map[pos.getRow()][pos.getCol()] = pawn.getRepresentation();
+        pieces.forEach(pieceOnBoard -> {
+            Position pos = pieceOnBoard.getPosition();
+            map[pos.getRow()][pos.getCol()] = pieceOnBoard.piece.getRepresentation();
         });
 
         for (String[] row: map) {
             sb.append(String.join("", row)).append(System.lineSeparator());
         }
 
-        System.out.println(sb.toString());
+        System.out.println(sb);
+
+        return sb.toString();
+    }
+
+    private class PieceOnBoard {
+        private Piece piece;
+        private Position position;
+
+        public Position getPosition() {
+            return position;
+        }
+
+        public PieceOnBoard(Piece piece, Position position){
+            this.piece = piece;
+            this.position = position;
+        }
     }
 }
