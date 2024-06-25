7d09062d60a3966d745ed7a51c1de1d0f4c48381
seminchoi <choicco89@gmail.com>
2024-06-25 15:20:47 +0900

    test: 체스판을 초기화 한 뒤 초기 상태를 확인하는 테스트 케이스 작성
    - Pawn만 초기화 한 상태로 Board가 잘 초기화 되어있는지 검증하도록 작성

--- src/test/java/wootecamp/chess/BoardTest.java
+++ src/test/java/wootecamp/chess/BoardTest.java
@@ -22,4 +22,28 @@ public class BoardTest {
         assertThat(board.getInitialStateWhitePawnResult()).isEqualTo("pppppppp");
         assertThat(board.getInitialStateBlackPawnResult()).isEqualTo("PPPPPPPP");
     }
+
+    @Test
+    @DisplayName("체스판 초기 상태를 확인한다.")
+    void getBoardState() {
+        final String emptyRank = "........";
+        final String whitePawnRank = "pppppppp";
+        final String blackPawnRank = "PPPPPPPP";
+        final String newLine = "\n";
+
+        board.initialize();
+
+        String expectedState = emptyRank + newLine +
+                blackPawnRank + newLine +
+                emptyRank + newLine +
+                emptyRank + newLine +
+                emptyRank + newLine +
+                emptyRank + newLine +
+                whitePawnRank + newLine +
+                emptyRank;
+
+        String initBoardState = board.getBoardState();
+        System.out.println(initBoardState);
+        assertThat(initBoardState).isEqualTo(expectedState);
+    }
 }
