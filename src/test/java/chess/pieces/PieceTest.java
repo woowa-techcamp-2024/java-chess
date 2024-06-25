0c6f213b4775b7cb3961e8c28ea4b879db8e24ae
miiiinju1 <gms07073@ynu.ac.kr>
2024-06-25 13:45:28 +0900

    test: PieceTest에 Pawn 이외 모든 Piece Type에 대한 테스트 추가

--- src/test/java/chess/pieces/PieceTest.java
+++ src/test/java/chess/pieces/PieceTest.java
@@ -7,32 +7,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PieceTest {
 
-    @Test
-    @DisplayName("기본 생성자를 이용하면 흰색 폰이 생성되고, 기본 표현이 설정되어야 한다")
-    void create_기본생성자() throws Exception {
-        Piece piece = new Piece();
-        assertEquals(Piece.WHITE_COLOR, piece.getColor());
-        assertEquals(Piece.WHITE_REPRESENTATION, piece.getRepresentation());
-    }
-
-    @Test
-    @DisplayName("생성자를 이용하면 폰이 생성되어야 한다")
-    void create() {
-        verifyPiece(Piece.WHITE_COLOR, Piece.WHITE_REPRESENTATION);
-        verifyPiece(Piece.BLACK_COLOR, Piece.BLACK_REPRESENTATION);
-    }
-
-    @DisplayName("")
+    @DisplayName("정적 팩토리 메서드를 이용하여 원하는 폰을 생성할 수 있다")
     @Test
     void create_piece() {
-        // given
-
-
-        // when
-
-        // then
-        verifyPiece(Piece.createWhitePawn(), Piece.WHITE_COLOR, Piece.WHITE_REPRESENTATION);
-        verifyPiece(Piece.createBlackPawn(), Piece.BLACK_COLOR, Piece.BLACK_REPRESENTATION);
+        verifyPiece(Piece.createWhitePawn(), Piece.WHITE_COLOR, Piece.WHITE_PAWN_REPRESENTATION);
+        verifyPiece(Piece.createBlackPawn(), Piece.BLACK_COLOR, Piece.BLACK_PAWN_REPRESENTATION);
+        verifyPiece(Piece.createWhiteKnight(), Piece.WHITE_COLOR, Piece.WHITE_KNIGHT_REPRESENTATION);
+        verifyPiece(Piece.createBlackKnight(), Piece.BLACK_COLOR, Piece.BLACK_KNIGHT_REPRESENTATION);
+        verifyPiece(Piece.createWhiteBishop(), Piece.WHITE_COLOR, Piece.WHITE_BISHOP_REPRESENTATION);
+        verifyPiece(Piece.createBlackBishop(), Piece.BLACK_COLOR, Piece.BLACK_BISHOP_REPRESENTATION);
+        verifyPiece(Piece.createWhiteRook(), Piece.WHITE_COLOR, Piece.WHITE_ROOK_REPRESENTATION);
+        verifyPiece(Piece.createBlackRook(), Piece.BLACK_COLOR, Piece.BLACK_ROOK_REPRESENTATION);
+        verifyPiece(Piece.createWhiteQueen(), Piece.WHITE_COLOR, Piece.WHITE_QUEEN_REPRESENTATION);
+        verifyPiece(Piece.createBlackQueen(), Piece.BLACK_COLOR, Piece.BLACK_QUEEN_REPRESENTATION);
+        verifyPiece(Piece.createWhiteKing(), Piece.WHITE_COLOR, Piece.WHITE_KING_REPRESENTATION);
+        verifyPiece(Piece.createBlackKing(), Piece.BLACK_COLOR, Piece.BLACK_KING_REPRESENTATION);
     }
 
     private void verifyPiece(final Piece piece, final String color, final char representation) {
@@ -40,9 +29,4 @@ class PieceTest {
         assertEquals(representation, piece.getRepresentation());
     }
 
-    private void verifyPiece(final String color, final char representation) {
-        Piece piece = new Piece(color, representation);
-        assertEquals(color, piece.getColor());
-        assertEquals(representation, piece.getRepresentation());
-    }
 }
