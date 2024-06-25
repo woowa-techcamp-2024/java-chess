713e40d1a5e7c91ed7533a97353998139f29b187
huiseung <whtjddn75@gmail.com>
2024-06-25 17:35:23 +0900

    [refactor] 이모지 코드상에 유니코드 -> 이모지 로 변경

--- chess/src/main/java/pieces/PieceUnicode.java
+++ chess/src/main/java/pieces/PieceUnicode.java
@@ -1,19 +1,19 @@
 package pieces;
 
 public enum PieceUnicode {
-    WHITE_PAWN("\u2659"),
-    WHITE_KNIGHT("\u2658"),
-    WHITE_BISHOP("\u2657"),
-    WHITE_ROOK("\u2656"),
-    WHITE_QUEEN("\u2655"),
-    WHITE_KING("\u2654"),
+    WHITE_PAWN("♙"),
+    WHITE_KNIGHT("♘"),
+    WHITE_BISHOP("♗"),
+    WHITE_ROOK("♖"),
+    WHITE_QUEEN("♕"),
+    WHITE_KING("♔"),
 
-    BLACK_PAWN("\u265f"),
-    BLACK_KNIGHT("\u265e"),
-    BLACK_BISHOP("\u265d"),
-    BLACK_ROOK("\u265c"),
-    BLACK_QUEEN("\u265b"),
-    BLACK_KING("\u265a"),
+    BLACK_PAWN("♟"),
+    BLACK_KNIGHT("♞"),
+    BLACK_BISHOP("♝"),
+    BLACK_ROOK("♜"),
+    BLACK_QUEEN("♛"),
+    BLACK_KING("♚"),
 
     BLANK("."),
     ;
