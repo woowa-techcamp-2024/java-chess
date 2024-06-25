03fb8797a0c44235e6b35c65c2d76014eb6f232d
june-777 <wlwhswnsrl96@gmail.com>
2024-06-25 10:33:29 +0900

    refactor: 테스트에서 사용한 지역변수를 상수로 변경
    - 상수를 사용하도록 변경
    - 테스트 성공 케이스를 의미하는 [Success] prefix를 DisplayName 에 추가

--- src/test/java/woowa/camp/PawnTest.java
+++ src/test/java/woowa/camp/PawnTest.java
@@ -8,22 +8,20 @@ import org.junit.jupiter.api.Test;
 public class PawnTest {
 
     @Test
-    @DisplayName("색상에 맞는 폰이 생성되어야 한다")
+    @DisplayName("[Success] 색상에 맞는 폰이 생성되어야 한다")
     void create() {
-        String white = "white";
-        String black = "black";
-        Pawn pawnWhite = new Pawn(white);
-        Pawn pawnBlack = new Pawn(black);
+        Pawn pawnWhite = new Pawn(Pawn.WHITE_COLOR);
+        Pawn pawnBlack = new Pawn(Pawn.BLACK_COLOR);
 
-        verifyPawnColor(pawnWhite, white);
-        verifyPawnColor(pawnBlack, black);
+        verifyPawnColor(pawnWhite, Pawn.WHITE_COLOR);
+        verifyPawnColor(pawnBlack, Pawn.BLACK_COLOR);
     }
 
     @Test
     @DisplayName("[Success] 색이 없는 Pawn을 생성하는 경우 기본 색상이 흰색(white)이다")
     void default_create_is_white() {
         Pawn pawn = new Pawn();
-        verifyPawnColor(pawn, "white");
+        verifyPawnColor(pawn, Pawn.WHITE_COLOR);
     }
 
     private void verifyPawnColor(final Pawn pawn, final String color) {
