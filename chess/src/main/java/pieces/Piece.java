942091202049bb761d5ec5f84f285b244802d1d6
huiseung <whtjddn75@gmail.com>
2024-06-25 13:18:27 +0900

    [feature] Piece 클래스에 isBlack, isWhite 추가

--- chess/src/main/java/pieces/Piece.java
+++ chess/src/main/java/pieces/Piece.java
@@ -4,4 +4,13 @@ public record Piece(PieceColor color, PieceUnicode represent) {
     public Piece() {
         this(PieceColor.WHITE, PieceUnicode.WHITE_PAWN);
     }
+
+    public boolean isBlack()
+    {
+        return color.equals(PieceColor.BLACK);
+    }
+    public boolean isWhite()
+    {
+        return color.equals(PieceColor.WHITE);
+    }
 }
