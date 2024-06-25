20aa761b49ebc481668a7851418d8f26f8a990b6
김수현 <i960107@naver.com>
2024-06-25 10:34:39 +0900

    test: 기본생성자 테스트 추가
    -컴파일 에러 발생하는 이유: Pawn에 기본생성자가 없기 때문에

--- src/test/java/PawnTest.java
+++ src/test/java/PawnTest.java
@@ -1,12 +1,14 @@
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class PawnTest {
 
-    @DisplayName("흰색/ 검은색 폰이 생성되어야한다.")
+    @DisplayName("흰색/검은색 폰이 생성되어야한다.")
     @ParameterizedTest
     @ValueSource(strings = {"white", "black"})
     void create(String color) {
@@ -16,5 +18,11 @@ class PawnTest {
 
     private void verifyPawn(final Pawn pawn, final String expectedColor) {
         assertThat(pawn.getColor()).isEqualTo(expectedColor);
+    }
+
+    @Test
+    public void create_기본생성자() {
+        Pawn pawn = new Pawn();
+        assertEquals("white", pawn.getColor());
     }
 }
