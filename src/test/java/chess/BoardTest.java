6f86df73655fe5fc0e9301fa63a04b31cbe7f115
김수현 <i960107@naver.com>
2024-06-25 11:18:26 +0900

    test: BoardTest 리팩터링
    - 흰/검 폰 미리 생성해두고 재사용. static으로 선언해서 공유해도 됨. 상태 변경될 위험 없기 때문에
    - ArrayList사용하여 중복 코드 제거
    - assertBoard 메소드 추출해서 검증 부분 분리

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -3,22 +3,41 @@ package chess;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import chess.pieces.Pawn;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class BoardTest {
+class BoardTest {
 
+    private static Pawn whitePawn;
+
+    private static Pawn blackPawn;
+
+    @BeforeAll
+    static void beforeAll() {
+        whitePawn = new Pawn(Pawn.WHITE_COLOR);
+        blackPawn = new Pawn(Pawn.BLACK_COLOR);
+    }
+
+    @DisplayName("체스판에 흰색/검은색 폰을 추가한다.")
     @Test
-    public void create() {
+    void create() {
         Board board = new Board();
 
-        Pawn white = new Pawn(Pawn.WHITE_COLOR);
-        board.add(white);
-        assertEquals(1, board.size());
-        assertEquals(white, board.findPawn(0));
+        AtomicInteger index = new AtomicInteger();
+        List<Pawn> pawns = List.of(whitePawn, blackPawn);
 
-        Pawn black = new Pawn(Pawn.BLACK_COLOR);
-        board.add(black);
-        assertEquals(2, board.size());
-        assertEquals(black, board.findPawn(1));
+        pawns.forEach(pawn -> {
+            board.add(pawn);
+            assertBoard(board, index.getAndIncrement(), pawn);
+        });
+
+    }
+
+    void assertBoard(Board board, int index, Pawn pawn) {
+        assertEquals(index + 1, board.size());
+        assertEquals(pawn, board.findPawn(index));
     }
 }
