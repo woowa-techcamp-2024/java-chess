819adb55ca841132be8757d1e778eb1802ecbf6d
hellomatia <shuaigejp80@gmail.com>
2024-06-25 13:35:27 +0900

    feat: 사용자의 입력에 따라 "start"이면 게임을 시작하고, 게임이 끝난 후 "start"를 입력받으면 재시작, "end"를 입력받으면 게임을 끝내는 기능 추가

---
+++ src/main/java/com/woowatechcamp/chess/Main.java
@@ -0,0 +1,17 @@
+package com.woowatechcamp.chess;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        GameState gameState = GameState.fromString(scanner.nextLine());
+        while (gameState == GameState.START) {
+            Board board = new Board();
+            board.initialize();
+            board.print();
+            gameState = GameState.fromString(scanner.nextLine());
+        }
+    }
+}
