0883990e1a383ff6f30940e2cc8924fa4e8040cf
KoKimSS <seungsu8848@ajou.ac.kr>
2024-06-25 11:22:04 +0900

    feat : Board에 Pawn 추가 메소드, 갯수 확인 메소드, Pawn 찾는 메소드 구현

--- woowa_camp_chess/src/main/java/org/example/board/Board.java
+++ woowa_camp_chess/src/main/java/org/example/board/Board.java
@@ -1,6 +1,24 @@
 package org.example.board;
 
+import org.example.pieces.Pawn;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board {
 
+    static List<Pawn> pawnList = new ArrayList<>();
 
+    public void add(Pawn pawn) {
+        pawnList.add(pawn);
+    }
+
+
+    public int size() {
+        return pawnList.size();
+    }
+
+    public Pawn findPawn(int i) {
+        return pawnList.get(i);
+    }
 }
