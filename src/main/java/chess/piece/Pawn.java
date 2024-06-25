769cbe3b631a8e0e08690f9eef6e91d54a1d2340
CodingLuizy <luizy991212@gmail.com>
2024-06-25 11:14:16 +0900

    feature: 기물의 공통속성을 일반화, 표현 추가
    기물의 공통속성을 일반화 하는 Piece 추상 class 생성
    기물의 표현을 나타내는 메서드 추가

--- src/main/java/chess/piece/Pawn.java
+++ src/main/java/chess/piece/Pawn.java
@@ -1,20 +1,33 @@
 package chess.piece;
 
-public class Pawn {
+public class Pawn extends Piece {
     public static final String WHITE_COLOR = "white";
     public static final String BLACK_COLOR = "black";
+    public static final char WHITE_REPRESENTATION = '♙';
+    public static final char BLACK_REPRESENTATION = '♟';
 
     private final String color;
+    private final char representation;
 
     public Pawn() {
         this(Pawn.WHITE_COLOR);
     }
 
     public Pawn(String color) {
+        this(color, color.equals(WHITE_COLOR) ? WHITE_REPRESENTATION : BLACK_REPRESENTATION);
+    }
+
+    public Pawn(String color, char representation) {
         this.color = color;
+        this.representation = representation;
     }
 
     public String getColor() {
         return color;
+    }
+
+    @Override
+    public char getRepresentation() {
+        return representation;
     }
 }
