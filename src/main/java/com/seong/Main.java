110724113f5d82b28846db30b551aa4e510b5455
hseong3243 <hseong3243@gmail.com>
2024-06-25 13:23:32 +0900

    feat: 체크 게임 시작, 종료 구현
    `main()`을 추가하고 사용자 입력을 받도록 구현함.
    'start' 입력 시 게임 시작, 'end' 입력 시 종료, 이 외에 다른 문자열 입력시 반응이 없도록 함.

--- src/main/java/com/seong/Main.java
+++ src/main/java/com/seong/Main.java
@@ -1,7 +1,27 @@
 package com.seong;
 
+import com.seong.chess.Board;
+import java.util.Scanner;
+
 public class Main {
+
+    public static final String START = "start";
+    public static final String END = "end";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String command = sc.next();
+            Board board;
+            if (command.equals(START)) {
+                board = new Board();
+                board.initialize();
+            } else if (command.equals(END)) {
+                break;
+            } else {
+                continue;
+            }
+            System.out.println(board.print());
+        }
     }
 }
