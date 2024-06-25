3a86fab85d4751c3280b74ee607c1b005705c148
김수현 <i960107@naver.com>
2024-06-25 14:08:59 +0900

    feat: GameControlCommand Enum 추가
    - 잘못된 입력값 방지 및 타입 안정성 추가위해 enum 추가

--- src/main/java/chess/Application.java
+++ src/main/java/chess/Application.java
@@ -1,33 +1,50 @@
 package chess;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Application {
+
+    public enum GameControlCommand {
+        START, END;
+
+
+        public static GameControlCommand valueOfIgnoreCase(String command) {
+            return Arrays.stream(GameControlCommand.values())
+                    .filter(c -> c.name().toLowerCase().equals(command))
+                    .findFirst()
+                    .orElseThrow(() -> new IllegalArgumentException("잘못된 command 입니다."));
+        }
+
+    }
+
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             boolean continueRunning = true;
             while (continueRunning) {
                 System.out.print("start/end 입력: ");
                 String input = scanner.nextLine();
-                continueRunning = execute(input);
+                GameControlCommand command;
+                try {
+                    command = GameControlCommand.valueOfIgnoreCase(input);
+                } catch (IllegalArgumentException exception) {
+                    System.out.println(exception.getMessage());
+                    continue;
+                }
+                continueRunning = execute(command);
             }
         }
 
     }
 
-    private static boolean execute(String input) {
-        switch (input) {
-            case "start": {
+    private static boolean execute(GameControlCommand command) {
+        return switch (command) {
+            case START -> {
                 startGame();
-                return true;
+                yield true;
             }
-            case "end": {
-                return false;
-            }
-            default: {
-                return true;
-            }
-        }
+            case END -> false;
+        };
     }
 
     private static void startGame() {
