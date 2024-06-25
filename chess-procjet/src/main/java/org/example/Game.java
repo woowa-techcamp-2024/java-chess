8400ee8fa717d04315bb8f997615d2feedf51f67
bellringstar <bellringstar@gmail.com>
2024-06-25 12:50:50 +0900

    feat: 게임 시작을 위한 main 클래스 사용자로 부터 입력을 받아 보드를 초기화 해 게임을 시작하거 게임을 종료할 수 있는 메서드

--- chess-procjet/src/main/java/org/example/Game.java
+++ chess-procjet/src/main/java/org/example/Game.java
@@ -1,7 +1,34 @@
 package org.example;
 
-public class Main {
+import java.util.Scanner;
+import org.example.chess.board.Board;
+
+public class Game {
+
+    private static final String START = "start";
+    private static final String END = "end";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean onPlaying = true;
+
+        while (onPlaying) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("명령어를 입력해 주세요");
+            System.out.println("게임 시작 : start | 게임 종료 : end");
+            String command = scanner.nextLine().toLowerCase();
+
+            if (command.equals(START)) {
+                Board board = new Board();
+                board.initialize();
+                board.print();
+                continue;
+            }
+
+            if (command.equals(END)) {
+                onPlaying = false;
+            }
+
+            System.out.println("올바른 명령어를 입력해주세요");
+        }
     }
 }
