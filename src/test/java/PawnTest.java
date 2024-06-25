45e1b96032cf37db62a996bba9b459da458ebf6d
hek-git <ghddmsrl1@naver.com>
2024-06-25 12:41:54 +0900

    feat: 리팩토링 color 문자열 중복 값과 test 로직 중복 제거

--- src/test/java/PawnTest.java
+++ src/test/java/PawnTest.java
@@ -6,11 +6,16 @@ public class PawnTest {
     @Test
     @DisplayName("흰색 폰이 생성되어야 한다.")
     public void create(){
-        Pawn pawn1 = new Pawn("white");
-        assertThat(pawn1.getColor()).isEqualTo("white");
+        String black = "black";
+        String white = "white";
 
-        Pawn pawn2 = new Pawn("black");
-        assertThat(pawn2.getColor()).isEqualTo("black");
-
+        verifyPawn(black);
+        verifyPawn(white);
     }
+
+    private void verifyPawn(final String color) {
+        Pawn pawn = new Pawn(color);
+        assertThat(pawn.getColor()).isEqualTo(color);
+    }
+
 }
