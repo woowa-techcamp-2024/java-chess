81178a5dfc4782113438815f5b9e5ecd2c170b54
miiiinju1 <gms07073@ynu.ac.kr>
2024-06-25 16:48:31 +0900

    test: 해당하는 색상과 종류의 Piece의 개수를 반환하는 메서드의 실패 테스트 생성

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -1,27 +1,35 @@
 package chess;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static chess.pieces.Piece.Color;
+import static chess.pieces.Piece.Type;
 import static chess.utils.StringUtils.NEWLINE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BoardTest {
 
-    public Board board;
-
-    @BeforeEach
-    void setUp() {
-        board = new Board();
-    }
-
     @Test
     @DisplayName("보드를 생성할 수 있다")
     void create() {
+        Board board = new Board();
 
-        assertEquals(32, board.pieceCount());
+        assertEquals(32, board.getTotalPieceCount());
         assertEquals(givenBoardPrint(), board.showBoard());
+    }
+
+    @DisplayName("해당하는 색상과 종류의 Piece 개수를 반환한다")
+    @Test
+    void getPieceCount() {
+        // given
+        Board board = new Board();
+
+        // when
+        int count = board.getPieceCount(Color.WHITE, Type.PAWN);
+
+        // then
+        assertEquals(count, 8);
     }
 
 
