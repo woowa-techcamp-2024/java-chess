f8ace7c394b49136c80f09fda790d4362c84bc97
donghar <donghar@naver.com>
2024-06-25 17:23:31 +0900

    [Test] 팩토리 메소드 생성
    PieceTest를 팩토리 메서드에 맞게 수정

--- src/test/java/pe/goblin/chess/piece/PieceTest.java
+++ src/test/java/pe/goblin/chess/piece/PieceTest.java
@@ -7,18 +7,78 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PieceTest {
     @Test
+    @DisplayName("흰색 킹이 생성되어야 한다.")
+    void createWhiteKing() {
+        verifyPiece(Piece.createWhiteKing(), Piece.WHITE_COLOR, Piece.WHITE_KING_REPRESENTATION);
+    }
+
+    @Test
+    @DisplayName("검은색 킹이 생성되어야 한다.")
+    void createBlackKing() {
+        verifyPiece(Piece.createBlackKing(), Piece.BLACK_COLOR, Piece.BLACK_KING_REPRESENTATION);
+    }
+
+    @Test
+    @DisplayName("흰색 퀸이 생성되어야 한다.")
+    void createWhiteQueen() {
+        verifyPiece(Piece.createWhiteQueen(), Piece.WHITE_COLOR, Piece.WHITE_QUEEN_REPRESENTATION);
+    }
+
+    @Test
+    @DisplayName("검은색 퀸이 생성되어야 한다.")
+    void createBlackQueen() {
+        verifyPiece(Piece.createBlackQueen(), Piece.BLACK_COLOR, Piece.BLACK_QUEEN_REPRESENTATION);
+    }
+
+    @Test
+    @DisplayName("흰색 룩이 생성되어야 한다.")
+    void createWhiteRook() {
+        verifyPiece(Piece.createWhiteRook(), Piece.WHITE_COLOR, Piece.WHITE_ROOK_REPRESENTATION);
+    }
+
+    @Test
+    @DisplayName("검은색 룩이 생성되어야 한다.")
+    void createBlackRook() {
+        verifyPiece(Piece.createBlackRook(), Piece.BLACK_COLOR, Piece.BLACK_ROOK_REPRESENTATION);
+    }
+
+    @Test
+    @DisplayName("흰색 비숍이 생성되어야 한다.")
+    void createWhiteBishop() {
+        verifyPiece(Piece.createWhiteBishop(), Piece.WHITE_COLOR, Piece.WHITE_BISHOP_REPRESENTATION);
+    }
+
+    @Test
+    @DisplayName("검은색 비숍이 생성되어야 한다.")
+    void createBlackBishop() {
+        verifyPiece(Piece.createBlackBishop(), Piece.BLACK_COLOR, Piece.BLACK_BISHOP_REPRESENTATION);
+    }
+
+    @Test
+    @DisplayName("흰색 나이트가 생성되어야 한다.")
+    void createWhiteKnight() {
+        verifyPiece(Piece.createWhiteKnight(), Piece.WHITE_COLOR, Piece.WHITE_KNIGHT_REPRESENTATION);
+    }
+
+    @Test
+    @DisplayName("검은색 나이트가 생성되어야 한다.")
+    void createBlackKnight() {
+        verifyPiece(Piece.createBlackKnight(), Piece.BLACK_COLOR, Piece.BLACK_KNIGHT_REPRESENTATION);
+    }
+
+    @Test
     @DisplayName("흰색 폰이 생성되어야 한다")
-    void createWhitePawnConcrete() {
-        verifyPawn(Piece.createWhitePawn(), Piece.WHITE_COLOR, Piece.WHITE_REPRESENTATION);
+    void createWhitePawn() {
+        verifyPiece(Piece.createWhitePawn(), Piece.WHITE_COLOR, Piece.WHITE_PAWN_REPRESENTATION);
     }
 
     @Test
     @DisplayName("검은색 폰이 생성되어야 한다")
-    void createBlackPawnConcrete() {
-        verifyPawn(Piece.createBlackPawn(), Piece.BLACK_COLOR, Piece.BLACK_REPRESENTATION);
+    void createBlackPawn() {
+        verifyPiece(Piece.createBlackPawn(), Piece.BLACK_COLOR, Piece.BLACK_PAWN_REPRESENTATION);
     }
 
-    void verifyPawn(final Piece piece, final String color, final char representation) {
+    void verifyPiece(final Piece piece, final String color, final char representation) {
         assertEquals(color, piece.getColor());
         assertEquals(representation, piece.getRepresentation());
     }
