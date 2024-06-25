4321ce56e87e691e19c831e7fe1c96aa6de157b4
donghar <donghar@naver.com>
2024-06-25 09:39:24 +0900

    [Test] Board는 체스 판 위의 Pawn을 관리한다.
    체스 판 위에 말이 없는 상태에서 시작한다.
    체스 판에 Pawn을 추가할 수 있다. 이 때 Pawn의 수가 맞는지 확인한다.

---
+++ src/test/java/pe/goblin/board/BoardTest.java
@@ -0,0 +1,41 @@
+package pe.goblin.board;
+
+import org.junit.jupiter.api.Test;
+import pe.goblin.pawn.Pawn;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class BoardTest {
+    @Test
+    void create_empty() {
+        Board board = new Board();
+        assertEquals(0, board.size());
+    }
+
+    @Test
+    void create() throws ExceedPawnException {
+        Board board = new Board();
+
+        Pawn white = new Pawn(Pawn.WHITE_COLOR);
+        board.add(white);
+        assertEquals(1, board.size());
+        assertEquals(white, board.findPawn(0));
+
+        Pawn black = new Pawn(Pawn.BLACK_COLOR);
+        board.add(black);
+        assertEquals(2, board.size());
+        assertEquals(black, board.findPawn(1));
+    }
+
+    @Test
+    void create_exceed_then_throw() throws ExceedPawnException {
+        Board board = new Board();
+        Pawn white1 = new Pawn(Pawn.WHITE_COLOR);
+        board.add(white1);
+        Pawn white2 = new Pawn(Pawn.WHITE_COLOR);
+        board.add(white2);
+        Pawn white3 = new Pawn(Pawn.WHITE_COLOR);
+        assertThrows(Exception.class, () -> board.add(white3));
+    }
+}
