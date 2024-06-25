a6ae25abc8897bf4f1a8fba1df5038d345801917
miiiinju1 <gms07073@ynu.ac.kr>
2024-06-25 14:01:29 +0900

    test: 전체 기물의 상태를 볼 수 있는 체스판의 검증을 위한 실패 테스트

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -3,7 +3,6 @@ package chess;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import chess.pieces.Piece;
 
 import static chess.utils.StringUtils.NEWLINE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,58 +10,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class BoardTest {
 
     public Board board;
-    public Piece white;
-    public Piece black;
 
     @BeforeEach
     void setUp() {
         board = new Board();
-        board.initialize();
-        white = Piece.createWhitePawn();
-        black = Piece.createBlackPawn();
     }
 
     @Test
-    @DisplayName("보드에 폰을 추가할 수 있다")
+    @DisplayName("보드를 생성할 수 있다")
     void create() {
-        board.add(white);
-        assertEquals(1, board.size());
-        assertEquals(white, board.findPawn(0));
+        board.initialize();
 
-        board.add(black);
-        assertEquals(2, board.size());
-        assertEquals(black, board.findPawn(1));
+        assertEquals(32, board.pieceCount());
+        assertEquals(givenBoardPrint(), board.showBoard());
     }
 
-    @Test
-    @DisplayName("보드를 초기화할 수 있다")
-    void initialize() {
-        assertEquals("pppppppp", board.getWhitePawnsResult());
-        assertEquals("PPPPPPPP", board.getBlackPawnsResult());
-    }
 
-    @Test
-    @DisplayName("보드를 출력할 수 있다")
-    void print()  {
-        // given
-        String givenBoard = givenBoardPrint();
-
-        // when
-        String boardPrint = board.print();
-
-        // then
-        assertEquals(givenBoard, boardPrint);
-    }
     private String givenBoardPrint() {
+        String blankRank = "........";
         StringBuilder sb = new StringBuilder();
-            sb.append("........").append(NEWLINE);
+            sb.append("RNBQKBNR").append(NEWLINE);
             sb.append("pppppppp").append(NEWLINE);
-            sb.append("........").append(NEWLINE);
-            sb.append("........").append(NEWLINE);
-            sb.append("........").append(NEWLINE);
-            sb.append("........").append(NEWLINE);
+            sb.append(blankRank).append(NEWLINE);
+            sb.append(blankRank).append(NEWLINE);
+            sb.append(blankRank).append(NEWLINE);
+            sb.append(blankRank).append(NEWLINE);
             sb.append("PPPPPPPP").append(NEWLINE);
-            sb.append("........").append(NEWLINE);
+            sb.append("rnbqkbnr").append(NEWLINE);
         return sb.toString();
     }
 }
