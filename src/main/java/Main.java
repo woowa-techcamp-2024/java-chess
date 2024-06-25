92a8566c311ceeff85f5138b524c2dedf7ea63d3
HyeonSik Choi <chgy2131@naver.com>
2024-06-25 13:02:10 +0900

    feat: 체스게임 시작과 종료를 지원한다. - start를 입력해 체스게임을 시작하고, 체스판의 현재 상태를 콘솔 화면에 출력한다. - end를 입력해 체스 게임을 종료한다.

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+import chess.Board;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        String input;
+        while (true) {
+            input = bf.readLine();
+            if (input.equals("start")) {
+                Board board = new Board();
+                board.initialize();
+                board.print();
+            }
+            if (input.equals("end")) {
+                break;
+            }
+        }
+    }
+}
