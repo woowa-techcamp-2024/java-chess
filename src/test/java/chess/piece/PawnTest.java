a3280f9022a41258afe35ea01377666e601bb504
HyeonJun0530 <hyjn053@gmail.com>
2024-06-25 09:39:31 +0900

    refactor: Pawn color를 상수로 변경
    - color black, white를 Pawn 클래스 안 상수로 변경

--- src/test/java/chess/piece/PawnTest.java
+++ src/test/java/chess/piece/PawnTest.java
@@ -4,22 +4,22 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class PawnTest {
 
     @Test
     @DisplayName("흰색 폰이 생성되어야 한다")
     public void create() {
-        String white = "white";
-        String black = "black";
 
-        verifyPawn(white);
+        verifyPawn(Pawn.WHITE_COLOR);
 
-        verifyPawn(black);
+        verifyPawn(Pawn.BLACK_COLOR);
     }
 
     private void verifyPawn(final String color) {
         Pawn pawn = new Pawn(color);
 
-        Assertions.assertThat(pawn.getColor()).isEqualTo(color);
+        assertThat(pawn.getColor()).isEqualTo(color);
     }
 }
