81c28f351532298e4545ace50d7500f4bbf0db99
mirageoasis <kimhw0820@naver.com>
2024-06-25 11:06:43 +0900

    feat: represetation 추가와 그에 따른 테스트 추가

--- src/main/java/org/example/pieces/Pawn.java
+++ src/main/java/org/example/pieces/Pawn.java
@@ -5,18 +5,28 @@ public class Pawn {
     public static final String WHITE_COLOR = "white";
     public static final String BLACK_COLOR = "black";
 
+    public static final String WHITE_REPRESENTATION = "\u2659";
+    public static final String BLACK_REPRESENTATION = "\u265F";
+
     private final String color;
+    private String representation;
 
     public Pawn(String color) {
         this.color = color;
+        this.representation =
+            color.equals(WHITE_COLOR) ? WHITE_REPRESENTATION : BLACK_REPRESENTATION;
     }
 
     public Pawn() {
-        this.color = WHITE_COLOR;
+        this(WHITE_COLOR);
     }
 
     public String getColor() {
         return color;
+    }
+
+    public String getRepresentation() {
+        return representation;
     }
 
 }
--- src/test/java/org/example/pieces/PawnTest.java
+++ src/test/java/org/example/pieces/PawnTest.java
@@ -1,12 +1,14 @@
 package org.example.pieces;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.stream.Stream;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-
-import static org.assertj.core.api.Assertions.*;
 
 public class PawnTest {
 
@@ -14,22 +16,23 @@ public class PawnTest {
     @Test
     public void create() {
         Pawn pawn = new Pawn();
-        assertThat(pawn.getColor()).isEqualTo(Pawn.WHITE_COLOR);
+        assertEquals(Pawn.WHITE_COLOR, pawn.getColor());
+        assertEquals(Pawn.WHITE_REPRESENTATION, pawn.getRepresentation());
     }
 
     @DisplayName("색깔에 맞는 폰이 생성되어야 한다")
-    @ParameterizedTest(name = "폰의 색깔은 {1} 이어야 한다")
+    @ParameterizedTest(name = "폰의 색깔은 {0}이고 표현은 {1}이어야 한다")
     @MethodSource("argumentsStream")
-    public void create(String nowColor, String expectedColor) {
-        Pawn pawn = new Pawn(nowColor);
-        assertThat(pawn.getColor()).isEqualTo(expectedColor);
+    public void create(String color, String representation) {
+        Pawn pawn = new Pawn(color);
+        assertEquals(color, pawn.getColor());
+        assertEquals(representation, pawn.getRepresentation());
     }
-
 
     private static Stream<Arguments> argumentsStream() {
         return Stream.of(
-            Arguments.arguments(Pawn.WHITE_COLOR, Pawn.WHITE_COLOR),
-            Arguments.arguments(Pawn.BLACK_COLOR, Pawn.BLACK_COLOR)
+            Arguments.arguments(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION),
+            Arguments.arguments(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION)
         );
     }
 
