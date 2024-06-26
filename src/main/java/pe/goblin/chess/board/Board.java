73d4886a71a6db852306aafb50672d9e7e379868
donghar <donghar@naver.com>
2024-06-27 01:01:13 +0900

    [Feat] 주어진 위치의 기물을 조회
    기능 구현

--- src/main/java/pe/goblin/chess/board/Board.java
+++ src/main/java/pe/goblin/chess/board/Board.java
@@ -10,9 +10,9 @@ import java.util.Objects;
 
 public class Board {
     public static final int MIN_ROWS = 0;
-    public static final int MAX_ROWS = 8;
+    public static final int MAX_ROWS = 7;
     public static final int MIN_COLS = 0;
-    public static final int MAX_COLS = 8;
+    public static final int MAX_COLS = 7;
 
     private List<List<Piece>> pieces = Collections.emptyList();
 
@@ -28,23 +28,23 @@ public class Board {
         // 초기 보드 생성
         List<List<Piece>> initialPieces = new ArrayList<>();
         // 각 행을 생성하고 mutable 리스트로 초기화
-        for (int row = MIN_ROWS; row < MAX_ROWS; row++) {
+        for (int row = MIN_ROWS; row <= MAX_ROWS; row++) {
             List<Piece> rowPieces = new ArrayList<>();
             initialPieces.add(rowPieces);
             // 각 열에 null로 초기화된 Piece 추가
-            for (int col = MIN_COLS; col < MAX_COLS; col++) {
+            for (int col = MIN_COLS; col <= MAX_COLS; col++) {
                 rowPieces.add(Piece.createBlank());
             }
         }
         // 백색 말 배치
         Piece[] fullMajorWhitePieces = {Piece.createWhiteRook(), Piece.createWhiteKnight(), Piece.createWhiteBishop(), Piece.createWhiteQueen(), Piece.createWhiteKing(), Piece.createWhiteBishop(), Piece.createWhiteKnight(), Piece.createWhiteRook()};
-        for (int col = MIN_ROWS; col < MAX_ROWS; col++) {
+        for (int col = MIN_ROWS; col <= MAX_ROWS; col++) {
             initialPieces.get(0).set(col, fullMajorWhitePieces[col]);
             initialPieces.get(1).set(col, Piece.createWhitePawn());
         }
         // 흑색 말 배치
         Piece[] fullMajorBlackPieces = {Piece.createBlackRook(), Piece.createBlackKnight(), Piece.createBlackBishop(), Piece.createBlackQueen(), Piece.createBlackKing(), Piece.createBlackBishop(), Piece.createBlackKnight(), Piece.createBlackRook()};
-        for (int col = MIN_ROWS; col < MAX_ROWS; col++) {
+        for (int col = MIN_ROWS; col <= MAX_ROWS; col++) {
             initialPieces.get(7).set(col, fullMajorBlackPieces[col]);
             initialPieces.get(6).set(col, Piece.createBlackPawn());
         }
@@ -72,8 +72,8 @@ public class Board {
 
     public String showBoard() {
         StringBuilder sb = new StringBuilder();
-        for (int row = MAX_ROWS - 1; row >= MIN_ROWS; row--) {
-            for (int col = MIN_ROWS; col < MAX_ROWS; col++) {
+        for (int row = MAX_ROWS; row >= MIN_ROWS; row--) {
+            for (int col = MIN_ROWS; col <= MAX_ROWS; col++) {
                 Piece piece = pieces.get(row).get(col);
                 if (piece == null) {
                     sb.append('.');
@@ -84,5 +84,16 @@ public class Board {
             sb.append(StringUtils.NEW_LINE); // 각 행 끝에 줄 바꿈 추가
         }
         return sb.toString();
+    }
+
+    public Piece findPiece(String pos) {
+        Position position = new Position(pos);
+        return pieces.get(position.row()).get(position.col());
+    }
+
+    private record Position(int row, int col) {
+        Position(String pos) {
+            this(pos.charAt(1) - '1', MAX_COLS - (pos.charAt(0) - 'a'));
+        }
     }
 }
