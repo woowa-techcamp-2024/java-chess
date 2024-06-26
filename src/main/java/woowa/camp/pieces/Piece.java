863bbf0af92eb78df4be34d741c8d266d5253468
june-777 <wlwhswnsrl96@gmail.com>
2024-06-26 15:19:13 +0900

    feat: 이름(종류), 색상에 맞는 Piece 팩터리 메서드 구현
    요구사항:
    - 이름은 기물의 종류를 의미
    - VO 객체
    - 색과 이름을 메서드 파라미터로 갖는 팩토리 메서드
    - 기본 말 생성하는 생성자 제거

--- src/main/java/woowa/camp/pieces/Piece.java
+++ src/main/java/woowa/camp/pieces/Piece.java
@@ -2,14 +2,71 @@ package woowa.camp.pieces;
 
 public class Piece {
 
+    public static final String PAWN = "pawn";
+    public static final String KNIGHT = "knight";
+    public static final String BISHOP = "bishop";
+    public static final String ROOK = "rook";
+    public static final String QUEEN = "queen";
+    public static final String KING = "king";
+
+    private final String name;
     private final Color color;
 
-    public Piece() {
-        this.color = Color.PAWN_WHITE;
+    private Piece(String name, Color color) {
+        this.name = name;
+        this.color = color;
     }
 
-    public Piece(final Color color) {
-        this.color = color;
+    public static Piece createPiece(final String name, final Color color) {
+        return new Piece(name, color);
+    }
+
+    public static Piece createWhitePawn() {
+        return new Piece(PAWN, Color.PAWN_WHITE);
+    }
+
+    public static Piece createBlackPawn() {
+        return new Piece(PAWN, Color.PAWN_BLACK);
+    }
+
+    public static Piece createWhiteKnight() {
+        return new Piece(KNIGHT, Color.KNIGHT_WHITE);
+    }
+
+    public static Piece createBlackKnight() {
+        return new Piece(KNIGHT, Color.KNIGHT_BLACK);
+    }
+
+    public static Piece createWhiteBishop() {
+        return new Piece(BISHOP, Color.BISHOP_WHITE);
+    }
+
+    public static Piece createBlackBishop() {
+        return new Piece(BISHOP, Color.BISHOP_BLACK);
+    }
+
+    public static Piece createWhiteRook() {
+        return new Piece(ROOK, Color.ROOK_WHITE);
+    }
+
+    public static Piece createBlackRook() {
+        return new Piece(ROOK, Color.ROOK_BLACK);
+    }
+
+    public static Piece createWhiteQueen() {
+        return new Piece(QUEEN, Color.QUEEN_WHITE);
+    }
+
+    public static Piece createBlackQueen() {
+        return new Piece(QUEEN, Color.QUEEN_BLACK);
+    }
+
+    public static Piece createWhiteKing() {
+        return new Piece(KING, Color.KING_WHITE);
+    }
+
+    public static Piece createBlackKing() {
+        return new Piece(KING, Color.KING_BLACK);
     }
 
     public String getColor() {
