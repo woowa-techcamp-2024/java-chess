eadaec4d516f7bf969aa994a78c28835f3b8bbe5
hellomatia <shuaigejp80@gmail.com>
2024-06-26 15:56:25 +0900

    feat: 콘솔로 move명령을 입력받으면, 사용자가 지정한 위치에 있는 기물을, 사용자가 원하는 위치로 이동시킬 수 있는 기능 추가

---
+++ src/main/java/com/woowatechcamp/chess/GameCommand.java
@@ -0,0 +1,25 @@
+package com.woowatechcamp.chess;
+
+import java.util.Arrays;
+
+public enum GameCommand {
+    START("start"),
+    END("end"),
+    MOVE("move");
+
+    private final String command;
+
+    GameCommand(String command) {
+        this.command = command;
+    }
+
+    public static GameCommand fromString(String command) {
+        if (command == null || command.isEmpty()) {
+            throw new IllegalArgumentException("Command cannot be null or empty");
+        }
+        return Arrays.stream(GameCommand.values())
+                .filter(value -> value.command.equalsIgnoreCase(command.split(" ")[0]))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("No such command: " + command));
+    }
+}
