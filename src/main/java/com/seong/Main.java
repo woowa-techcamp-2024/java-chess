f501dc06d8112caf1f71454a89e7afa42fa2b328
hseong3243 <hseong3243@gmail.com>
2024-06-26 12:38:34 +0900

    feat: 체스 게임 move 명령 구현
    move 명령 입력 시 문자열을 쪼개서 움직일 기물과 움직일 위치를 계산함.

--- src/main/java/com/seong/Main.java
+++ src/main/java/com/seong/Main.java
@@ -7,19 +7,22 @@ public class Main {
 
     public static final String START = "start";
     public static final String END = "end";
+    public static final String MOVE = "move";
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Board board = new Board();
         while (sc.hasNext()) {
-            String command = sc.next();
-            Board board;
+            String command = sc.nextLine();
             if (command.equals(START)) {
-                board = new Board();
                 board.initialize();
             } else if (command.equals(END)) {
                 break;
-            } else {
-                continue;
+            } else if (command.startsWith(MOVE)) {
+                String[] commands = command.split(" ");
+                String sourcePosition = commands[1];
+                String targetPosition = commands[2];
+                board.move(sourcePosition, targetPosition);
             }
             System.out.println(board.showBoard());
         }
