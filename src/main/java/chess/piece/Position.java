1228b0eb439d4026472cf12f10aa68ba66fdb0fe
HyeonJun0530 <hyjn053@gmail.com>
2024-06-26 19:57:04 +0900

    refactor: 행과 열에 맞게 매개변수 순서 변경

--- src/main/java/chess/piece/Position.java
+++ src/main/java/chess/piece/Position.java
@@ -9,7 +9,7 @@ public record Position(Rank rank, File file) {
         return new Position(Rank.of(row), File.of(col));
     }
 
-    public static Position of(final File file, final Rank rank) {
+    public static Position of(final Rank rank, final File file) {
         return new Position(rank, file);
     }
 }
