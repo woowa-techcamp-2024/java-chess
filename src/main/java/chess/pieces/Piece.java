58160b341859d108b4b10237e895431afccfe6f6
kimhyun5u <22kimhyun5u@gmail.com>
2024-06-26 10:53:29 +0900

    feat: 기물의 이동 구현

--- src/main/java/chess/pieces/Piece.java
+++ src/main/java/chess/pieces/Piece.java
@@ -1,5 +1,7 @@
 package chess.pieces;
 
+import chess.Position;
+
 import java.util.Objects;
 
 public class Piece {
@@ -37,16 +39,27 @@ public class Piece {
     private final String color;
     private final String representation;
     private final double defaultScore;
+    private Position position;
 
     private Piece(String color, String representation, double defaultScore) {
         this.color = color;
         this.representation = representation;
         this.defaultScore = defaultScore;
+    }
 
+    public Piece(String color, String representation, double defaultScore, Position position) {
+        this.color = color;
+        this.representation = representation;
+        this.defaultScore = defaultScore;
+        this.position = position;
     }
 
     public static Piece createBlank() {
         return Piece.of(Color.NOCOLOR.name(), Type.NO_PIECE.getBlackRepresentation(), Type.NO_PIECE.getDefaultPoint());
+    }
+
+    public static Piece createBlank(final Position position) {
+        return Piece.of(Color.NOCOLOR.name(), Type.NO_PIECE.getBlackRepresentation(), Type.NO_PIECE.getDefaultPoint(), position);
     }
 
     public static Piece createWhite(Type type) {
@@ -59,6 +72,10 @@ public class Piece {
 
     public static  Piece of(final String color, final String representation, final double defaultPoint) {
         return new Piece(color, representation, defaultPoint);
+    }
+
+    public static Piece of(final String color, final String representation, final double defaultPoint, final Position position) {
+        return new Piece(color, representation, defaultPoint, position);
     }
 
     public String getColor() {
@@ -79,5 +96,25 @@ public class Piece {
 
     public boolean isWhite() {
         return Objects.equals(color, Color.WHITE.name());
+    }
+
+    public static Piece createWhite(Type type, Position position) {
+        return Piece.of(Color.WHITE.name(), type.getWhiteRepresentation(), type.getDefaultPoint(), position);
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Piece piece = (Piece) o;
+        return Double.compare(defaultScore, piece.defaultScore) == 0 && Objects.equals(color, piece.color) && Objects.equals(representation, piece.representation) && Objects.equals(position, piece.position);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(color, representation, defaultScore, position);
     }
 }
