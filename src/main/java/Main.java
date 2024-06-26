ba1286740f88a1cef3d899242da721507fbe8ef0
kimhyun5u <22kimhyun5u@gmail.com>
2024-06-26 11:06:00 +0900

    feat: main() 메소드에 move 명령 추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,17 +6,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Board board = new Board();
+        boolean isStarted = false;
         while(true) {
             Scanner scanner = new Scanner(System.in);
-
             if (scanner.hasNext()) {
                 String input = scanner.nextLine();
-                if (Objects.equals("start", input)) {
+                if (!isStarted && Objects.equals("start", input)) {
+                    isStarted = true;
                     board.initialize();
                     System.out.println(board.showBoard());
                 }
                 else if (Objects.equals("end", input)) {
+                    isStarted = false;
                     return ;
+                }
+                else if (isStarted && input.startsWith("move")) {
+                    String[] cmd = input.split(" ");
+                    board.move(cmd[1], cmd[2]);
+                    System.out.println(board.showBoard());
                 }
                 else {
                     System.out.println("not in else if = " + input);
