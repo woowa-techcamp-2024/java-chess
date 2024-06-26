210a4677f46751b5798c3c432e833909f71f550a
KoKimSS <seungsu8848@ajou.ac.kr>
2024-06-26 18:05:39 +0900

    test : 점수 계산 테스트 코드 작성

--- woowa_camp_chess/src/main/java/org/example/chess/board/Board.java
+++ woowa_camp_chess/src/main/java/org/example/chess/board/Board.java
@@ -10,18 +10,13 @@ import static org.example.chess.pieces.Piece.createBlank;
 
 public class Board {
 
-    static final int SIZE = 8;
-    static final int EMPTY_INDEX = -1;
-    static char EMPTY_CHAR = '.';
-//    int[][] board = new int[SIZE][SIZE];
     List<List<Piece>> board2 = new ArrayList<>();
-
     List<Piece> pieceList = new ArrayList<>();
     List<Piece> whitePieceList = new ArrayList<>();
     List<Piece> blackPieceList = new ArrayList<>();
 
     public void add(Piece piece) {
-        if(piece.isWhite()){
+        if (piece.isWhite()) {
             System.out.println("화이트추가");
             whitePieceList.add(piece);
         }
@@ -36,18 +31,15 @@ public class Board {
         return pieceList.size();
     }
 
-    public Piece findPawn(int i) {
-        return pieceList.get(i);
-    }
 
     public void initializeV2() {
         for (int i = 0; i < 8; i++) {
             List<Piece> row = new ArrayList<>();
-            if(i!=0&&i!=7) {
+            if (i != 0 && i != 7) {
                 for (int j = 0; j < 8; j++) {
                     row.add(createBlank()); // 빈 칸은 Blank 입력
                 }
-            }else{
+            } else {
                 for (int j = 0; j < 8; j++) {
                     row.add(null); // 말이 들어갈 곳은 Null 입력
                 }
@@ -88,100 +80,6 @@ public class Board {
         System.out.println(showBoard());
     }
 
-
-//    public void initialize() {
-//        for (int i = 0; i < SIZE; i++) {
-//            for (int j = 0; j < SIZE; j++) {
-//                board[i][j] = EMPTY_INDEX;
-//            }
-//        }
-//
-//        // Pawn 기물 초기화
-//        for (int i = 0; i < SIZE; i++) {
-//            Piece whitePawn = createWhitePawn();
-////            whitePieceList.add(whitePawn);
-//            add(whitePawn);
-//            board[6][i] = pieceList.size() - 1;
-//
-//            Piece blackPawn = createBlackPawn();
-////            blackPieceList.add(blackPawn);
-//            add(blackPawn);
-//            board[1][i] = pieceList.size() - 1;
-//        }
-//
-//        // Knight 기물 초기화
-//        Piece whiteKnight1 = createWhiteKnight();
-//        Piece whiteKnight2 = createWhiteKnight();
-//        Piece blackKnight1 = createBlackKnight();
-//        Piece blackKnight2 = createBlackKnight();
-//        add(whiteKnight1);
-//        add(whiteKnight2);
-//        add(blackKnight1);
-//        add(blackKnight2);
-//        board[0][1] = pieceList.size() - 1;
-//        board[0][6] = pieceList.size() - 2;
-//        board[7][1] = pieceList.size() - 3;
-//        board[7][6] = pieceList.size() - 4;
-//
-//        // Bishop 기물 초기화
-//        Piece whiteBishop1 = createWhiteBishop();
-//        Piece whiteBishop2 = createWhiteBishop();
-//        Piece blackBishop1 = createBlackBishop();
-//        Piece blackBishop2 = createBlackBishop();
-//        add(whiteBishop1);
-//        add(whiteBishop2);
-//        add(blackBishop1);
-//        add(blackBishop2);
-//        board[0][2] = pieceList.size() - 1;
-//        board[0][5] = pieceList.size() - 2;
-//        board[7][2] = pieceList.size() - 3;
-//        board[7][5] = pieceList.size() - 4;
-//
-//        // Rook 기물 초기화
-//        Piece whiteRook1 = createWhiteRook();
-//        Piece whiteRook2 = createWhiteRook();
-//        Piece blackRook1 = createBlackRook();
-//        Piece blackRook2 = createBlackRook();
-//        add(whiteRook1);
-//        add(whiteRook2);
-//        add(blackRook1);
-//        add(blackRook2);
-//        board[0][0] = pieceList.size() - 1;
-//        board[0][7] = pieceList.size() - 2;
-//        board[7][0] = pieceList.size() - 3;
-//        board[7][7] = pieceList.size() - 4;
-//
-//        // Queen 기물 초기화
-//        Piece whiteQueen = createWhiteQueen();
-//        Piece blackQueen = createBlackQueen();
-//        add(whiteQueen);
-//        add(blackQueen);
-//        board[0][3] = pieceList.size() - 1;
-//        board[7][3] = pieceList.size() - 2;
-//
-//        // King 기물 초기화
-//        Piece whiteKing = createWhiteKing();
-//        Piece blackKing = createBlackKing();
-//        add(whiteKing);
-//        add(blackKing);
-//        board[0][4] = pieceList.size() - 1;
-//        board[7][4] = pieceList.size() - 2;
-//    }
-    
-//    public String showBoard() {
-//        StringBuilder print = new StringBuilder();
-//
-//        for (int i = 0; i < SIZE; i++) {
-//            StringBuilder row = new StringBuilder();
-//            for (int j = 0; j < SIZE; j++) {
-//                int index = board[i][j];
-//                row.append(index != -1 ? findPawn(index).getRepresentation() : EMPTY_CHAR);
-//            }
-//            print.append(appendNewLine(row.toString()));
-//        }
-//
-//        return print.toString();
-//    }
 
     public String showBoard() {
         StringBuilder sb = new StringBuilder();
@@ -224,8 +122,8 @@ public class Board {
         Position pos = Position.fromStr(position);
 
         // todo : 현재는 임의로 말이 있는 경우 놓지 못하게 하였다.
-        if(locatedPiece.getPieceType()!= PieceType.NO_PIECE) return;
+        if (locatedPiece.getPieceType() != PieceType.NO_PIECE) return;
 
-        board2.get(pos.getRow()).set(pos.getColumn(),piece);
+        board2.get(pos.getRow()).set(pos.getColumn(), piece);
     }
 }
--- woowa_camp_chess/src/main/java/org/example/chess/pieces/PieceType.java
+++ woowa_camp_chess/src/main/java/org/example/chess/pieces/PieceType.java
@@ -2,18 +2,24 @@ package org.example.chess.pieces;
 
 //todo : inner class 에 넣을 것인지 ?
 public enum PieceType {
-    KING('K'),
-    QUEEN('Q'),
-    ROOK( 'R'),
-    BISHOP( 'B'),
-    KNIGHT( 'N'),
-    PAWN( 'P'),
-    NO_PIECE('.');
+    KING('K', 1.0),
+    QUEEN('Q', 9.0),
+    ROOK( 'R', 5.0),
+    BISHOP( 'B', 3.0),
+    KNIGHT( 'N', 2.5),
+    PAWN( 'P', 1.0),
+    NO_PIECE('.', 0.0);
 
     private final char representation;
+    private final double defaultPoint;
 
-    PieceType(char representation) {
+    PieceType(char representation, double defaultPoint) {
         this.representation = representation;
+        this.defaultPoint = defaultPoint;
+    }
+
+    public double getDefaultPoint() {
+        return defaultPoint;
     }
 
     public char getRepresentation() {
