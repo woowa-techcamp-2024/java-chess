bfe690994804251fa9197891dd07fcadce4b9748
donghar <donghar@naver.com>
2024-06-26 11:13:10 +0900

    [Test] Piece에 대한 색과 기물에 따라 분리된 팩토리 메소드에서 enum 사용
    테스트 코드 작성

--- src/test/java/pe/goblin/chess/piece/PieceTest.java
+++ src/test/java/pe/goblin/chess/piece/PieceTest.java
@@ -1,111 +1,32 @@
 package pe.goblin.chess.piece;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import pe.goblin.chess.piece.Piece.Type;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static pe.goblin.chess.piece.Piece.Type.*;
 
 class PieceTest {
     @Test
-    @DisplayName("흰색 킹이 생성되어야 한다.")
-    void createWhiteKing() {
-        verifyPiece(Piece.createWhiteKing(), Piece.WHITE_COLOR, Piece.WHITE_KING_REPRESENTATION);
+    public void create_piece() {
+        verifyPiece(Piece.createWhitePawn(), Piece.createBlackPawn(), PAWN);
+        verifyPiece(Piece.createWhiteKnight(), Piece.createBlackKnight(), KNIGHT);
+        verifyPiece(Piece.createWhiteRook(), Piece.createBlackRook(), ROOK);
+        verifyPiece(Piece.createWhiteBishop(), Piece.createBlackBishop(), BISHOP);
+        verifyPiece(Piece.createWhiteQueen(), Piece.createBlackQueen(), QUEEN);
+        verifyPiece(Piece.createWhiteKing(), Piece.createBlackKing(), KING);
+
+        Piece blank = Piece.createBlank();
+        assertFalse(blank.isWhite());
+        assertFalse(blank.isBlack());
+        assertEquals(Type.NO_PIECE, blank.getType());
     }
 
-    @Test
-    @DisplayName("검은색 킹이 생성되어야 한다.")
-    void createBlackKing() {
-        verifyPiece(Piece.createBlackKing(), Piece.BLACK_COLOR, Piece.BLACK_KING_REPRESENTATION);
-    }
+    private void verifyPiece(final Piece whitePiece, final Piece blackPiece, final Type type) {
+        assertTrue(whitePiece.isWhite());
+        assertEquals(type, whitePiece.getType());
 
-    @Test
-    @DisplayName("흰색 퀸이 생성되어야 한다.")
-    void createWhiteQueen() {
-        verifyPiece(Piece.createWhiteQueen(), Piece.WHITE_COLOR, Piece.WHITE_QUEEN_REPRESENTATION);
-    }
-
-    @Test
-    @DisplayName("검은색 퀸이 생성되어야 한다.")
-    void createBlackQueen() {
-        verifyPiece(Piece.createBlackQueen(), Piece.BLACK_COLOR, Piece.BLACK_QUEEN_REPRESENTATION);
-    }
-
-    @Test
-    @DisplayName("흰색 룩이 생성되어야 한다.")
-    void createWhiteRook() {
-        verifyPiece(Piece.createWhiteRook(), Piece.WHITE_COLOR, Piece.WHITE_ROOK_REPRESENTATION);
-    }
-
-    @Test
-    @DisplayName("검은색 룩이 생성되어야 한다.")
-    void createBlackRook() {
-        verifyPiece(Piece.createBlackRook(), Piece.BLACK_COLOR, Piece.BLACK_ROOK_REPRESENTATION);
-    }
-
-    @Test
-    @DisplayName("흰색 비숍이 생성되어야 한다.")
-    void createWhiteBishop() {
-        verifyPiece(Piece.createWhiteBishop(), Piece.WHITE_COLOR, Piece.WHITE_BISHOP_REPRESENTATION);
-    }
-
-    @Test
-    @DisplayName("검은색 비숍이 생성되어야 한다.")
-    void createBlackBishop() {
-        verifyPiece(Piece.createBlackBishop(), Piece.BLACK_COLOR, Piece.BLACK_BISHOP_REPRESENTATION);
-    }
-
-    @Test
-    @DisplayName("흰색 나이트가 생성되어야 한다.")
-    void createWhiteKnight() {
-        verifyPiece(Piece.createWhiteKnight(), Piece.WHITE_COLOR, Piece.WHITE_KNIGHT_REPRESENTATION);
-    }
-
-    @Test
-    @DisplayName("검은색 나이트가 생성되어야 한다.")
-    void createBlackKnight() {
-        verifyPiece(Piece.createBlackKnight(), Piece.BLACK_COLOR, Piece.BLACK_KNIGHT_REPRESENTATION);
-    }
-
-    @Test
-    @DisplayName("흰색 폰이 생성되어야 한다")
-    void createWhitePawn() {
-        verifyPiece(Piece.createWhitePawn(), Piece.WHITE_COLOR, Piece.WHITE_PAWN_REPRESENTATION);
-    }
-
-    @Test
-    @DisplayName("검은색 폰이 생성되어야 한다")
-    void createBlackPawn() {
-        verifyPiece(Piece.createBlackPawn(), Piece.BLACK_COLOR, Piece.BLACK_PAWN_REPRESENTATION);
-    }
-
-    void verifyPiece(final Piece piece, final String color, final char representation) {
-        assertEquals(color, piece.getColor());
-        assertEquals(representation, piece.getRepresentation());
-    }
-
-    @Nested
-    @DisplayName("체스 말의 색깔을 확인할 수 있다.")
-    class CheckColor{
-        @Test
-        @DisplayName("흰색 폰의 색깔을 확인할 수 있다.")
-        void checkWhitePawnColor() {
-            Piece whitePawn = Piece.createWhitePawn();
-            assertTrue(whitePawn.isWhite());
-            assertFalse(whitePawn.isBlack());
-        }
-        @Test
-        @DisplayName("검은색 폰의 색깔을 확인할 수 있다.")
-        void checkBlackPawnColor() {
-            Piece blackPawn = Piece.createBlackPawn();
-            assertTrue(blackPawn.isBlack());
-            assertFalse(blackPawn.isWhite());
-        }
-    }
-
-    @Test
-    public void getRepresentationPerPiece() throws Exception {
-        assertEquals('p', Piece.Type.PAWN.getWhiteRepresentation());
-        assertEquals('P', Piece.Type.PAWN.getBlackRepresentation());
+        assertTrue(blackPiece.isBlack());
+        assertEquals(type, blackPiece.getType());
     }
 }
