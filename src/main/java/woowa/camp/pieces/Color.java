19fe04e97ef555904b2055dd0f19f97059dc9754
june-777 <wlwhswnsrl96@gmail.com>
2024-06-26 15:21:37 +0900

    feat: 기물별 Color Enum 인스턴스 추가

--- src/main/java/woowa/camp/pieces/Color.java
+++ src/main/java/woowa/camp/pieces/Color.java
@@ -3,7 +3,22 @@ package woowa.camp.pieces;
 public enum Color {
 
     PAWN_WHITE("white", "p"),
-    PAWN_BLACK("black", "P");
+    PAWN_BLACK("black", "P"),
+
+    ROOK_WHITE("white", "r"),
+    ROOK_BLACK("black", "R"),
+
+    KNIGHT_WHITE("white", "n"),
+    KNIGHT_BLACK("black", "N"),
+
+    BISHOP_WHITE("white", "b"),
+    BISHOP_BLACK("black", "B"),
+
+    QUEEN_WHITE("white", "q"),
+    QUEEN_BLACK("black", "Q"),
+
+    KING_WHITE("white", "k"),
+    KING_BLACK("black", "K");
 
     private final String name;
     private final String representation;
