54b33b8ff90047cc395592f0f9540c48d9224669
jcw1031 <jcw001031@gmail.com>
2024-06-26 19:55:49 +0900

    feat: 위치 값을 나타내는 Position 추가

---
+++ src/main/java/com/woopaca/javachess/chess/Position.java
@@ -0,0 +1,28 @@
+package com.woopaca.javachess.chess;
+
+public class Position {
+
+    private final int file;
+    private final int rank;
+
+    public Position(String fileRank) {
+        if (fileRank.length() > 2) {
+            throw new IllegalArgumentException(String.format("[ERROR] 위치 값의 길이가 올바르지 않습니다. file & rank: %s", fileRank));
+        }
+
+        this.file = fileRank.charAt(0) - 'a';
+        this.rank = fileRank.charAt(1) - '0';
+        if (file < 0 || file >= Board.BOARD_SIZE || rank < 0 || rank >= Board.BOARD_SIZE) {
+            throw new IllegalArgumentException(String.format("[ERROR] File 또는 Rank 값이 올바르지 않습니다. file: %c, rank: %c", 'a' + file, '0' + rank));
+        }
+    }
+
+    public int getFileIndex() {
+        return file;
+    }
+
+    public int getRankIndex() {
+        return Board.BOARD_SIZE - rank;
+    }
+
+}
