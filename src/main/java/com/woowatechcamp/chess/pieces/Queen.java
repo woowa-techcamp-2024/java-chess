5359ed460bd6ea5d4487fd4be51e647007808c84
hellomatia <shuaigejp80@gmail.com>
2024-06-26 21:12:06 +0900

    feat: Queen이 이동 명령을 받으면 해당 위치가 움직일 수 있는지 판단하고, 움직일 수 없는 위치면 예외를 발생시키는 기능 추가

--- src/main/java/com/woowatechcamp/chess/pieces/Queen.java
+++ src/main/java/com/woowatechcamp/chess/pieces/Queen.java
@@ -4,4 +4,15 @@ public class Queen extends Piece {
     public Queen(Color color, Position position) {
         super(color, Type.QUEEN, position);
     }
+
+    @Override
+    protected void validateMove(Position newPosition) {
+        int deltaX = Math.abs(getPosition().deltaX(newPosition));
+        int deltaY = Math.abs(getPosition().deltaY(newPosition));
+
+        // Queen can move vertically, horizontally, or diagonally
+        if (!(deltaX == deltaY || deltaX == 0 || deltaY == 0)) {
+            throw new IllegalArgumentException("Queen can only move in straight lines or diagonals.");
+        }
+    }
 }
