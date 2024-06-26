bf6a09d1c092aba790f552639e6dd44770338229
lass9436 <lass9436@gmail.com>
2024-06-26 11:20:30 +0900

    [feat] Piece 수정
    Name 을 Type 으로 수정하고 Type 에 NO_PIECE 를 추가했습니다.
    create 메서드에서도 NO_PIECE 생성 case 를 추가했습니다.

--- src/test/java/chess/pieces/Piece.java
+++ src/test/java/chess/pieces/Piece.java
@@ -2,7 +2,7 @@ package chess.pieces;
 
 public class Piece {
 
-    private final Name name;
+    private final Type type;
 
     private final Color color;
 
@@ -16,7 +16,7 @@ public class Piece {
         return color == Color.BLACK;
     }
 
-    public enum Name { PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING }
+    public enum Type { PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING, NO_PIECE }
 
     public enum Color { WHITE, BLACK }
 
@@ -39,77 +39,82 @@ public class Piece {
         }
     }
 
-    private Piece(Name name, Color color, Representation representation) {
-        this.name = name;
+    private Piece(Type type, Color color, Representation representation) {
+        this.type = type;
         this.color = color;
         this.representation = representation;
     }
 
-    private static Piece create(Name name, Color color) {
-        return switch (name) {
-            case PAWN -> new Piece(name, color, (color == Color.WHITE) ? Representation.WHITE_PAWN : Representation.BLACK_PAWN);
-            case KNIGHT -> new Piece(name, color, (color == Color.WHITE) ? Representation.WHITE_KNIGHT : Representation.BLACK_KNIGHT);
-            case BISHOP -> new Piece(name, color, (color == Color.WHITE) ? Representation.WHITE_BISHOP : Representation.BLACK_BISHOP);
-            case ROOK -> new Piece(name, color, (color == Color.WHITE) ? Representation.WHITE_ROOK : Representation.BLACK_ROOK);
-            case QUEEN -> new Piece(name, color, (color == Color.WHITE) ? Representation.WHITE_QUEEN : Representation.BLACK_QUEEN);
-            case KING -> new Piece(name, color, (color == Color.WHITE) ? Representation.WHITE_KING : Representation.BLACK_KING);
+    private static Piece create(Type type, Color color) {
+        return switch (type) {
+            case PAWN -> new Piece(type, color, (color == Color.WHITE) ? Representation.WHITE_PAWN : Representation.BLACK_PAWN);
+            case KNIGHT -> new Piece(type, color, (color == Color.WHITE) ? Representation.WHITE_KNIGHT : Representation.BLACK_KNIGHT);
+            case BISHOP -> new Piece(type, color, (color == Color.WHITE) ? Representation.WHITE_BISHOP : Representation.BLACK_BISHOP);
+            case ROOK -> new Piece(type, color, (color == Color.WHITE) ? Representation.WHITE_ROOK : Representation.BLACK_ROOK);
+            case QUEEN -> new Piece(type, color, (color == Color.WHITE) ? Representation.WHITE_QUEEN : Representation.BLACK_QUEEN);
+            case KING -> new Piece(type, color, (color == Color.WHITE) ? Representation.WHITE_KING : Representation.BLACK_KING);
+            case NO_PIECE -> new Piece(type, null, null);
         };
     }
 
+    public static Piece createBlank() {
+        return create(Type.NO_PIECE, null);
+    }
+
     public static Piece createWhitePawn(){
-        return create(Name.PAWN, Color.WHITE);
+        return create(Type.PAWN, Color.WHITE);
     }
 
     public static Piece createBlackPawn(){
-        return create(Name.PAWN, Color.BLACK);
+        return create(Type.PAWN, Color.BLACK);
     }
 
     public static Piece createWhiteKnight(){
-        return create(Name.KNIGHT, Color.WHITE);
+        return create(Type.KNIGHT, Color.WHITE);
     }
 
     public static Piece createBlackKnight(){
-        return create(Name.KNIGHT, Color.BLACK);
+        return create(Type.KNIGHT, Color.BLACK);
     }
 
     public static Piece createWhiteBishop(){
-        return create(Name.BISHOP, Color.WHITE);
+        return create(Type.BISHOP, Color.WHITE);
     }
 
     public static Piece createBlackBishop(){
-        return create(Name.BISHOP, Color.BLACK);
+        return create(Type.BISHOP, Color.BLACK);
     }
 
     public static Piece createWhiteRook(){
-        return create(Name.ROOK, Color.WHITE);
+        return create(Type.ROOK, Color.WHITE);
     }
 
     public static Piece createBlackRook(){
-        return create(Name.ROOK, Color.BLACK);
+        return create(Type.ROOK, Color.BLACK);
     }
 
     public static Piece createWhiteQueen(){
-        return create(Name.QUEEN, Color.WHITE);
+        return create(Type.QUEEN, Color.WHITE);
     }
 
     public static Piece createBlackQueen(){
-        return create(Name.QUEEN, Color.BLACK);
+        return create(Type.QUEEN, Color.BLACK);
     }
 
     public static Piece createWhiteKing(){
-        return create(Name.KING, Color.WHITE);
+        return create(Type.KING, Color.WHITE);
     }
 
     public static Piece createBlackKing(){
-        return create(Name.KING, Color.BLACK);
+        return create(Type.KING, Color.BLACK);
     }
 
     public Color getColor() {
         return color;
     }
 
-    public Name getName() {
-        return name;
+    public Type getType() {
+        return type;
     }
 
     public Representation getRepresentation() {
