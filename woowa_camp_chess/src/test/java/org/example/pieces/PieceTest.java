f70867303708dca4cfa1afb931c5cfeb70e77f98
KoKimSS <seungsu8848@ajou.ac.kr>
2024-06-26 11:05:36 +0900

    test : 테스트 코드 Color enum으로 변경

--- woowa_camp_chess/src/test/java/org/example/pieces/PieceTest.java
+++ woowa_camp_chess/src/test/java/org/example/pieces/PieceTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.example.chess.pieces.Piece.*;
+import static org.example.chess.pieces.Piece.Color.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PieceTest {
@@ -14,38 +15,38 @@ class PieceTest {
     @DisplayName("기본 생성자로 폰이 생성되어야 한다")
     public void create_기본생성자() {
         Piece piece = new Piece();
-        assertEquals(WHITE_COLOR, piece.getColor());
+        assertEquals(WHITE, piece.getColor());
         assertEquals(WHITE_REPRESENTATION, piece.getRepresentation());
     }
 
     @Test
     @DisplayName("올바른 색과 코드의 폰이 생성되어야 한다")
     public void create() {
-        verifyPiece(Piece.createBlackPawn(),BLACK_COLOR,PieceType.PAWN);
-        verifyPiece(Piece.createWhitePawn(),WHITE_COLOR,PieceType.PAWN);
+        verifyPiece(Piece.createBlackPawn(),BLACK,PieceType.PAWN);
+        verifyPiece(Piece.createWhitePawn(),WHITE,PieceType.PAWN);
     }
 
     @Test
     @DisplayName("올바른 색과 코드의 피스가 생성되어야 한다")
     public void create_piece() {
-        verifyPiece(Piece.createBlackPawn(),BLACK_COLOR,PieceType.PAWN);
-        verifyPiece(Piece.createWhitePawn(),WHITE_COLOR,PieceType.PAWN);
-        verifyPiece(Piece.createBlackKnight(),BLACK_COLOR,PieceType.KNIGHT);
-        verifyPiece(Piece.createWhiteKnight(),WHITE_COLOR,PieceType.KNIGHT);
-        verifyPiece(Piece.createBlackBishop(),BLACK_COLOR,PieceType.BISHOP);
-        verifyPiece(Piece.createWhiteBishop(),WHITE_COLOR,PieceType.BISHOP);
-        verifyPiece(Piece.createBlackRook(),BLACK_COLOR,PieceType.ROOK);
-        verifyPiece(Piece.createWhiteRook(),WHITE_COLOR,PieceType.ROOK);
-        verifyPiece(Piece.createBlackQueen(),BLACK_COLOR,PieceType.QUEEN);
-        verifyPiece(Piece.createWhiteQueen(),WHITE_COLOR,PieceType.QUEEN);
-        verifyPiece(Piece.createBlackKing(),BLACK_COLOR,PieceType.KING);
-        verifyPiece(Piece.createWhiteKing(),WHITE_COLOR,PieceType.KING);
+        verifyPiece(Piece.createBlackPawn(),BLACK,PieceType.PAWN);
+        verifyPiece(Piece.createWhitePawn(),WHITE,PieceType.PAWN);
+        verifyPiece(Piece.createBlackKnight(),BLACK,PieceType.KNIGHT);
+        verifyPiece(Piece.createWhiteKnight(),WHITE,PieceType.KNIGHT);
+        verifyPiece(Piece.createBlackBishop(),BLACK,PieceType.BISHOP);
+        verifyPiece(Piece.createWhiteBishop(),WHITE,PieceType.BISHOP);
+        verifyPiece(Piece.createBlackRook(),BLACK,PieceType.ROOK);
+        verifyPiece(Piece.createWhiteRook(),WHITE,PieceType.ROOK);
+        verifyPiece(Piece.createBlackQueen(),BLACK,PieceType.QUEEN);
+        verifyPiece(Piece.createWhiteQueen(),WHITE,PieceType.QUEEN);
+        verifyPiece(Piece.createBlackKing(),BLACK,PieceType.KING);
+        verifyPiece(Piece.createWhiteKing(),WHITE,PieceType.KING);
     }
 
     @Test
     @DisplayName("색을 구분한 결과값을 내야한다")
     public void compare_color() {
-        Piece piece = Piece.createPiece(WHITE_COLOR, PieceType.PAWN);
+        Piece piece = Piece.createPiece(WHITE, PieceType.PAWN);
         Assertions.assertTrue(piece.isWhite());
         Assertions.assertFalse(piece.isBlack());
     }
@@ -59,8 +60,8 @@ class PieceTest {
     }
 
 
-    private static void verifyPiece(final Piece piece, final String color, PieceType pieceType) {
+    private static void verifyPiece(final Piece piece, final Color color, PieceType pieceType) {
         assertThat(piece.getColor()).isEqualTo(color);
-        assertThat(piece.getRepresentation()).isEqualTo(color.equals(BLACK_COLOR) ? pieceType.getBlackRepresentation() : (char) (pieceType.getBlackRepresentation() + CASE_DIFFERENCE ));
+        assertThat(piece.getRepresentation()).isEqualTo(color.equals(BLACK) ? pieceType.getBlackRepresentation() : pieceType.getWhiteRepresentation());
     }
 }
