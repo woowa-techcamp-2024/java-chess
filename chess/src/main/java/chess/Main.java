8c80d06822be6e96be49b5f3628a37d390453538
huiseung <whtjddn75@gmail.com>
2024-06-26 11:14:19 +0900

    [feat] main 에 move 추가

--- chess/src/main/java/chess/Main.java
+++ chess/src/main/java/chess/Main.java
@@ -6,22 +6,27 @@ public class Main {
 
     private static final String START = "start";
     private static final String END = "end";
+    private static final String MOVE = "move";
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ChessGame chessGame = new ChessGame();
         while (true)
         {
             String str = scanner.next();
             if(str.equals(START))
             {
-                ChessGame chessGame = new ChessGame();
                 chessGame.initialize();
-                System.out.println(chessGame.showBoard());
             }
             else if(str.equals(END))
             {
                 return;
             }
+            else if(str.startsWith(MOVE))
+            {
+                chessGame.move(scanner.next(), scanner.next());
+            }
+            System.out.println(chessGame.showBoard());
         }
     }
 }
