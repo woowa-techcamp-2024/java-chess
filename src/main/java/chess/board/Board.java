745558a278b9e1dd7efe156eb3a933ea5c617e72
miiiinju1 <gms07073@ynu.ac.kr>
2024-06-26 15:51:10 +0900

    refactor: Board의 초기화 담당하는 private메서드를 클래스 하단으로 이동

--- src/main/java/chess/board/Board.java
+++ src/main/java/chess/board/Board.java
@@ -34,58 +34,6 @@ public class Board {
         pieceCount = 0;
     }
 
-    private void initialize() {
-        pieceCount = INITIAL_PIECE_COUNT;
-        initializeBlackFirstRow();
-        initializeBlackPawns();
-        initializeEmtpyRows(BLANK_ROW_START, BLANK_ROW_END);
-        initializeWhitePawns();
-        initializeWhiteFirstRow();
-        validateRankHeight();
-    }
-
-    private void validateRankHeight() {
-        if(ranks.size() != RANK_HEIGHT) {
-            throw new IllegalArgumentException("한 보드는 8개의 랭크를 가집니다.");
-        }
-    }
-
-    private void initializeEmtpyRows(int start, int end) {
-        IntStream.range(start, end).forEach(i ->
-                ranks.add(Rank.initializeRank(IntStream.range(0, BOARD_WIDTH)
-                        .mapToObj(c -> Piece.createBlank())
-                        .collect(Collectors.toCollection(ArrayList::new)))));
-    }
-
-    private void initializeBlackFirstRow() {
-        List<Piece> firstRow = List.of(
-                Piece.createBlackRook(),
-                Piece.createBlackKnight(),
-                Piece.createBlackBishop(),
-                Piece.createBlackQueen(),
-                Piece.createBlackKing(),
-                Piece.createBlackBishop(),
-                Piece.createBlackKnight(),
-                Piece.createBlackRook());
-
-        ranks.add(Rank.initializeRank(firstRow));
-    }
-
-    private void initializeWhiteFirstRow() {
-        List<Piece> lastRow = List.of(
-                Piece.createWhiteRook(),
-                Piece.createWhiteKnight(),
-                Piece.createWhiteBishop(),
-                Piece.createWhiteQueen(),
-                Piece.createWhiteKing(),
-                Piece.createWhiteBishop(),
-                Piece.createWhiteKnight(),
-                Piece.createWhiteRook());
-
-        ranks.add(Rank.initializeRank(lastRow));
-    }
-
-
     public int getPieceCount(Piece.Color color, Piece.Type type) {
         return ranks.stream().mapToInt(rank -> rank.getPieceCount(color, type)).sum();
     }
@@ -147,5 +95,56 @@ public class Board {
                 .mapToObj(i -> Piece.createBlackPawn())
                 .collect(Collectors.toCollection(ArrayList::new));
         this.ranks.add(Rank.initializeRank(initializedPieces));
+    }
+
+    private void initialize() {
+        pieceCount = INITIAL_PIECE_COUNT;
+        initializeBlackFirstRow();
+        initializeBlackPawns();
+        initializeEmtpyRows(BLANK_ROW_START, BLANK_ROW_END);
+        initializeWhitePawns();
+        initializeWhiteFirstRow();
+        validateRankHeight();
+    }
+
+    private void validateRankHeight() {
+        if(ranks.size() != RANK_HEIGHT) {
+            throw new IllegalArgumentException("한 보드는 8개의 랭크를 가집니다.");
+        }
+    }
+
+    private void initializeEmtpyRows(int start, int end) {
+        IntStream.range(start, end).forEach(i ->
+                ranks.add(Rank.initializeRank(IntStream.range(0, BOARD_WIDTH)
+                        .mapToObj(c -> Piece.createBlank())
+                        .collect(Collectors.toCollection(ArrayList::new)))));
+    }
+
+    private void initializeBlackFirstRow() {
+        List<Piece> firstRow = List.of(
+                Piece.createBlackRook(),
+                Piece.createBlackKnight(),
+                Piece.createBlackBishop(),
+                Piece.createBlackQueen(),
+                Piece.createBlackKing(),
+                Piece.createBlackBishop(),
+                Piece.createBlackKnight(),
+                Piece.createBlackRook());
+
+        ranks.add(Rank.initializeRank(firstRow));
+    }
+
+    private void initializeWhiteFirstRow() {
+        List<Piece> lastRow = List.of(
+                Piece.createWhiteRook(),
+                Piece.createWhiteKnight(),
+                Piece.createWhiteBishop(),
+                Piece.createWhiteQueen(),
+                Piece.createWhiteKing(),
+                Piece.createWhiteBishop(),
+                Piece.createWhiteKnight(),
+                Piece.createWhiteRook());
+
+        ranks.add(Rank.initializeRank(lastRow));
     }
 }
