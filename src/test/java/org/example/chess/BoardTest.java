5404be54cf83223d1f4762eec566cff12a3ee4c0
mirageoasis <kimhw0820@naver.com>
2024-06-26 15:03:45 +0900

    test: 기물의 개수를 세는 테스트 작성

--- src/test/java/org/example/chess/BoardTest.java
+++ src/test/java/org/example/chess/BoardTest.java
@@ -3,9 +3,15 @@ package org.example.chess;
 import static org.example.utils.StringUtils.appendNewLine;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.example.pieces.Piece;
+import java.util.stream.Stream;
+import org.example.pieces.Piece.Color;
+import org.example.pieces.Piece.Type;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class BoardTest {
 
@@ -15,18 +21,11 @@ public class BoardTest {
     @BeforeEach
     public void setUpBoard() {
         board = new Board();
-        //board.initialize();
     }
-
-//    @Test
-//    public void 폰_배열검사() throws Exception {
-//        assertEquals("♙♙♙♙♙♙♙♙♖♘♗♕♔♗♘♖", board.getWhitePawnsResult());
-//        assertEquals("♜♞♝♛♚♝♞♜♟♟♟♟♟♟♟♟", board.getBlackPawnsResult());
-//    }
 
     @Test
     public void create() throws Exception {
-        assertEquals(32, board.pieceCount());
+        assertEquals(32, board.nonEmptyPiece());
         String blankRank = appendNewLine("........");
         assertEquals(
             appendNewLine("♜♞♝♛♚♝♞♜") +
@@ -36,5 +35,36 @@ public class BoardTest {
                 appendNewLine("♖♘♗♕♔♗♘♖"),
             board.showBoard());
 
+    }
+
+
+    @ParameterizedTest
+    @DisplayName("선택한 기물의 개수를 확인한다")
+    @MethodSource("countByQueryArgument")
+    public void countByQuery(
+        Color color,
+        Type type,
+        int count
+    ) {
+        assertEquals(count, board.countByQuery(color, type));
+    }
+
+    private static Stream<Arguments> countByQueryArgument() {
+        // 색상, 기물 종류, 개수
+        return Stream.of(
+            Arguments.arguments(Color.BLACK, Type.PAWN, 8),
+            Arguments.arguments(Color.WHITE, Type.PAWN, 8),
+            Arguments.arguments(Color.BLACK, Type.ROOK, 2),
+            Arguments.arguments(Color.WHITE, Type.ROOK, 2),
+            Arguments.arguments(Color.BLACK, Type.KNIGHT, 2),
+            Arguments.arguments(Color.WHITE, Type.KNIGHT, 2),
+            Arguments.arguments(Color.BLACK, Type.BISHOP, 2),
+            Arguments.arguments(Color.WHITE, Type.BISHOP, 2),
+            Arguments.arguments(Color.BLACK, Type.QUEEN, 1),
+            Arguments.arguments(Color.WHITE, Type.QUEEN, 1),
+            Arguments.arguments(Color.BLACK, Type.KING, 1),
+            Arguments.arguments(Color.WHITE, Type.KING, 1),
+            Arguments.arguments(Color.NO_COLOR, Type.NO_PIECE, 32)
+        );
     }
 }
