b5968688173c12fbf2790eaf2c46e815d039dfe2
donghar <donghar@naver.com>
2024-06-26 10:44:50 +0900

    [Test] 기물의 색, 기물의 종류에 따른 enum
    테스트 코드 작성

--- src/test/java/pe/goblin/chess/piece/PieceTest.java
+++ src/test/java/pe/goblin/chess/piece/PieceTest.java
@@ -102,4 +102,10 @@ class PieceTest {
             assertFalse(blackPawn.isWhite());
         }
     }
+
+    @Test
+    public void getRepresentationPerPiece() throws Exception {
+        assertEquals('p', Piece.Type.PAWN.getWhiteRepresentation());
+        assertEquals('P', Piece.Type.PAWN.getBlackRepresentation());
+    }
 }
