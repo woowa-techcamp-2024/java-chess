cc4ee884a06846b36a90ee582b070c25d5c9cfd8
miiiinju1 <gms07073@ynu.ac.kr>
2024-06-26 12:36:01 +0900

    feat: main() 메소드에 move 명령 추가하여 기물이 이동할 수 있게 구현

--- src/main/java/chess/Main.java
+++ src/main/java/chess/Main.java
@@ -5,15 +5,28 @@ import chess.board.Board;
 import java.util.Scanner;
 
 public class Main {
+
+    private static final String QUIT = "q";
+    private static final String PRINT = "p";
+    private static final String MOVE = "move";
+
+    private static final int MOVE_FROM_INDEX = 1;
+    private static final int MOVE_TO_INDEX = 2;
+
     public static void main(String[] args) {
         Board board = new Board();
 
         Scanner sc = new Scanner(System.in);
         String input;
 
-        while (!(input = sc.nextLine()).equals("q")) {
-            if(input.equals("p")) {
+        while (!(input = sc.nextLine()).equals(QUIT)) {
+            if(input.equals(PRINT)) {
                 System.out.println(board.showBoard());
+            }
+            if(input.startsWith(MOVE)) {
+                String[] split = input.split(" ");
+                // TODO : 입력 시 validation도 필요하지 않을까?
+                board.move(split[MOVE_FROM_INDEX], split[MOVE_TO_INDEX]);
             }
         }
         System.out.println("종료");
