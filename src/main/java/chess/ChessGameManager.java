b7655bc8c9f0338305d05f346aa408ab9b3e7bc0
hek-git <ghddmsrl1@naver.com>
2024-06-27 22:10:28 +0900

    feature: main 메소드에 move 명령 추가 - 명령어 종류 COMMAND enum 클래스로 모아 관리 - console 명령으로 move 명령 입력시 기물 이동 구현

--- src/main/java/chess/ChessGameManager.java
+++ src/main/java/chess/ChessGameManager.java
@@ -2,22 +2,46 @@ package chess;
 
 import chess.board.Board;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ChessGameManager {
 
+    public enum Command {
+        START("start"),
+        END("end"),
+        MOVE("move");
+
+        private final String command;
+
+        Command(String command) {
+            this.command = command;
+        }
+
+        public String getCommand() {
+            return command;
+        }
+    }
+
     public static void main(String[] args) {
+        Board board = new Board();
         Scanner scanner = new Scanner(System.in);
         String command;
         System.out.println("Input start or end!");
+
         while (true) {
-            command = scanner.next();
-            Board board = new Board();
-            if (command.equalsIgnoreCase("start")) {
+            command = scanner.nextLine();
+            if (Command.START.getCommand().equalsIgnoreCase(command)) {
                 board.initialize();
-            } else if (command.equalsIgnoreCase("end")) {
+            } else if (Command.END.getCommand().equalsIgnoreCase(command)) {
                 System.out.println("Game end!");
                 break;
+            } else if(command.startsWith(Command.MOVE.getCommand())) {
+                String[] commands = command.split(" ");
+                if(commands.length != 3) {
+                    throw new IllegalArgumentException("move command requires 2 arguments");
+                }
+                board.move(commands[1], commands[2]);
             } else {
                 System.out.println("Invalid input! input again!");
                 continue;
