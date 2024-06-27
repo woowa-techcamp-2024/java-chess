bcb9d07c8610e78c479674289835925f32a870df
hek-git <ghddmsrl1@naver.com>
2024-06-27 13:31:29 +0900

    test: 기물과 색에 해당하는 기물 개수 반환 test 메서드 추가 - 기물의 색과 종류를 인자로 받아 기물의 개수를 반환하는 countPiece 메서드 구현

--- src/test/java/chess/board/BoardTest.java
+++ src/test/java/chess/board/BoardTest.java
@@ -1,5 +1,7 @@
 package chess.board;
 
+import chess.pieces.Piece.Type;
+import chess.pieces.Piece.Color;
 import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,6 +10,7 @@ import static utils.StringUtils.appendNewLine;
 public class BoardTest {
 
     private Board board;
+    private String blankRank = appendNewLine("........");
 
     @BeforeEach
     public void setup() {
@@ -15,17 +18,24 @@ public class BoardTest {
     }
 
     @Test
+    @DisplayName("체스판을 초기화하고 처음 상태를 출력합니다.")
     public void create() throws Exception {
         board.initialize();
         assertEquals(32, board.pieceCount());
-        String blankRank = appendNewLine("........");
 
         assertEquals(
-            appendNewLine("RNBQKBNR") +
-            appendNewLine("PPPPPPPP") +
-            blankRank + blankRank + blankRank + blankRank +
-            appendNewLine("pppppppp") +
-            appendNewLine("rnbqkbnr"),
-            board.showBoard());
+                appendNewLine("RNBQKBNR") +
+                        appendNewLine("PPPPPPPP") +
+                        blankRank + blankRank + blankRank + blankRank +
+                        appendNewLine("pppppppp") +
+                        appendNewLine("rnbqkbnr"),
+                board.showBoard());
+    }
+
+    @Test
+    @DisplayName("현재 체스판에 있는 기물의 개수를 반환하는 해야합니다.")
+    public void countPiece() throws Exception {
+        board.initialize();
+        assertEquals(board.countPiece(Color.BLACK, Type.BISHOP), 2);
     }
 }
