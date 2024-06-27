fd2027ae02af384266b1a32cc37a7e87889e0841
june-777 <wlwhswnsrl96@gmail.com>
2024-06-27 13:55:57 +0900

    test: 테스트 코드 보강 및 계층형으로 구조 변경

--- src/test/java/woowa/camp/chess/BoardTest.java
+++ src/test/java/woowa/camp/chess/BoardTest.java
@@ -2,6 +2,7 @@ package woowa.camp.chess;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static woowa.camp.chess.BoardConstants.INIT_PIECE_COUNT;
 import static woowa.camp.chess.BoardConstants.MAX_BISHOP;
 import static woowa.camp.chess.BoardConstants.MAX_COL;
 import static woowa.camp.chess.BoardConstants.MAX_KING;
@@ -14,6 +15,7 @@ import static woowa.camp.chess.BoardConstants.MAX_ROW;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -35,125 +37,155 @@ public class BoardTest {
         black = Piece.createBlackPieceOf(Type.PAWN);
     }
 
-    @Test
-    @DisplayName("[Success] Pawn을 체스판에 추가할 수 있다.")
-    void board_add_pawn() {
-        board.add(white);
-        verifyBoardGetPieceCount(board, 1);
-        verifyGetPawn(board, 0, white);
+    @Nested
+    @DisplayName("Describe_체스판에 기물을 추가하는 기능은")
+    class AddPieceTest {
 
-        board.add(black);
-        verifyBoardGetPieceCount(board, 2);
-        verifyGetPawn(board, 1, black);
+        @Test
+        @DisplayName("[Success] 기물을 추가하면 기물 개수도 반영된다.")
+        void board_add_pawn() {
+            board.initialize();
+            board.addPiece(white, Position.mapBy("a6"));
+            verifyBoardGetPieceCount(board, INIT_PIECE_COUNT.getCount() + 1);
+            verifyGetPiece(board, "a6", white);
+
+            board.addPiece(black, Position.mapBy("b6"));
+            verifyBoardGetPieceCount(board, INIT_PIECE_COUNT.getCount() + 2);
+            verifyGetPiece(board, "b6", black);
+        }
+
+        private void verifyBoardGetPieceCount(Board board, int expectedBoardSize) {
+            int pieceCount = board.getPieceCount();
+            assertThat(pieceCount).isEqualTo(expectedBoardSize);
+        }
+
+        private void verifyGetPiece(Board board, String position, Piece expectedPiece) {
+            assertThat(board.getPieceBy(position)).isEqualTo(expectedPiece);
+        }
+
     }
 
-    private void verifyBoardGetPieceCount(Board board, int expectedBoardSize) {
-        assertThat(board.getPieceCount()).isEqualTo(expectedBoardSize);
+    @Nested
+    @DisplayName("Describe_체스판의 기물을 찾는 기능은")
+    class GetPieceByTest {
+
+        @Test
+        @DisplayName("[Success] 체스위치에 대한 기물을 찾는다.")
+        void findPawn() {
+            board.initialize();
+            assertThat(Piece.createBlackPieceOf(Type.ROOK)).isEqualTo(board.getPieceBy("a8"));
+            assertThat(Piece.createBlackPieceOf(Type.ROOK)).isEqualTo(board.getPieceBy("h8"));
+            assertThat(Piece.createWhitePieceOf(Type.ROOK)).isEqualTo(board.getPieceBy("a1"));
+            assertThat(Piece.createWhitePieceOf(Type.ROOK)).isEqualTo(board.getPieceBy("h1"));
+        }
+
+        @Test
+        @DisplayName("[Exception] 올바르지 않은 범위이면, 예외가 발생한다.")
+        void findOutOfRange() {
+            verifyOutOfRangeGetPiece(board, "`8");
+            verifyOutOfRangeGetPiece(board, "a9");
+            verifyOutOfRangeGetPiece(board, "h9");
+            verifyOutOfRangeGetPiece(board, "i8");
+            verifyOutOfRangeGetPiece(board, "i1");
+            verifyOutOfRangeGetPiece(board, "h0");
+            verifyOutOfRangeGetPiece(board, "a0");
+            verifyOutOfRangeGetPiece(board, "`1");
+        }
+
+        private void verifyOutOfRangeGetPiece(Board board, String outOfPosition) {
+            assertThatThrownBy(() -> board.getPieceBy(outOfPosition)).isInstanceOf(
+                    IllegalArgumentException.class);
+        }
+
     }
 
-    private void verifyGetPawn(Board board, int findPawnIndex, Piece expectedPiece) {
-        assertThat(board.getPawn(findPawnIndex)).isEqualTo(expectedPiece);
-    }
+    @Nested
+    @DisplayName("Describe_체스판을 초기화하는 기능은")
+    class InitializeTest {
 
-    @Test
-    @DisplayName("[Exception] 체스판의 Pawn을 찾을 때 올바르지 않은 범위이면, 예외가 발생한다.")
-    void findOutOfRange() {
-        board.add(white);
-        board.add(black);
+        @Test
+        @DisplayName("[Success] 초기화한 Board가 관리하고 있는 Pawn의 결과를 확인")
+        void getPiecesResult() {
+            board.initialize();
+            String expectedWhitePawnsResult = "pppppppp";
+            String expectedBlackPawnsResult = "PPPPPPPP";
 
-        int lowerBound = -1;
-        int upperBound = board.getPieceCount();
+            assertThat(board.getPiecesResult(Type.PAWN, Color.WHITE)).isEqualTo(expectedWhitePawnsResult);
+            assertThat(board.getPiecesResult(Type.PAWN, Color.BLACK)).isEqualTo(expectedBlackPawnsResult);
+        }
 
-        verifyOutOfRangeGetPawn(board, lowerBound, upperBound);
-    }
+        @Test
+        @DisplayName("[Success] 초기화한 Board가 가지고 있는 검은색 Pawn과 흰색 Pawn은 각각 8개이다.")
+        void initialPawnGetPieceCount() {
+            board.initialize();
+            int expectedPawnsCount = MAX_PAWN.getCount();
+            verifyInitialPawnsCount(board, expectedPawnsCount);
+        }
 
-    private void verifyOutOfRangeGetPawn(Board board, int lowerBound, int upperBound) {
-        assertThatThrownBy(() -> board.getPawn(lowerBound)).isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> board.getPawn(upperBound)).isInstanceOf(IllegalArgumentException.class);
-    }
+        private void verifyInitialPawnsCount(Board board, int expectedPawnsCount) {
+            assertThat(board.getPiecesResult(Type.PAWN, Color.WHITE).length()).isEqualTo(expectedPawnsCount);
+            assertThat(board.getPiecesResult(Type.PAWN, Color.BLACK).length()).isEqualTo(expectedPawnsCount);
+        }
 
-    @Test
-    @DisplayName("[Success] 초기화한 Board이 관리하고 있는 Pawn의 결과를 확인")
-    void getPiecesResult() {
-        board.initialize();
-        String expectedWhitePawnsResult = "pppppppp";
-        String expectedBlackPawnsResult = "PPPPPPPP";
+        @Test
+        @DisplayName("[Success] 초기화한 Board의 크기는 8 x 8 이다.")
+        void initialBoardGetPieceCount() {
+            board.initialize();
 
-        assertThat(board.getPiecesResult(Type.PAWN, Color.WHITE)).isEqualTo(expectedWhitePawnsResult);
-        assertThat(board.getPiecesResult(Type.PAWN, Color.BLACK)).isEqualTo(expectedBlackPawnsResult);
-    }
+            int resultBoardRowSize = board.getBoardRowSize();
+            int resultBoardColSize = board.getBoardColSize();
+            int expectedBoardRowSize = MAX_ROW.getCount();
+            int expectedBoardColSize = MAX_COL.getCount();
 
-    @Test
-    @DisplayName("[Success] 초기화한 Board가 가지고 있는 검은색 Pawn과 흰색 Pawn은 각각 8개이다.")
-    void initialPawnGetPieceCount() {
-        board.initialize();
-        int expectedPawnsCount = MAX_PAWN.getCount();
-        verifyInitialPawnsCount(board, expectedPawnsCount);
-    }
+            assertThat(resultBoardRowSize).isEqualTo(expectedBoardRowSize);
+            assertThat(resultBoardColSize).isEqualTo(expectedBoardColSize);
+        }
 
-    private void verifyInitialPawnsCount(Board board, int expectedPawnsCount) {
-        assertThat(board.getPiecesResult(Type.PAWN, Color.WHITE).length()).isEqualTo(expectedPawnsCount);
-        assertThat(board.getPiecesResult(Type.PAWN, Color.BLACK).length()).isEqualTo(expectedPawnsCount);
-    }
+        @Test
+        @DisplayName("[Success] 초기화한 Board의 기물 위치 확인")
+        void initialBoardStatus() {
+            board.initialize();
+            String result = board.showBoard();
+            String expectedInitialBoardState = """
+                    RNBQKBNR
+                    PPPPPPPP
+                    ........
+                    ........
+                    ........
+                    ........
+                    pppppppp
+                    rnbqkbnr
+                    """;
+            assertThat(result).isEqualTo(expectedInitialBoardState);
+        }
 
-    @Test
-    @DisplayName("[Success] 초기화한 Board의 크기는 8 x 8 이다.")
-    void initialBoardGetPieceCount() {
-        board.initialize();
+        @ParameterizedTest
+        @MethodSource("providePiecesAndCounts")
+        @DisplayName("[Success] 초기화한 Board의 기물 개수 확인")
+        void initialGetPieceCount(Type type, Color color, int expectedCount) {
+            board.initialize();
+            Piece piece = Piece.createPiece(type, color);
+            int actualCount = board.getPieceCount(piece.getType(), color);
+            assertThat(actualCount).isEqualTo(expectedCount);
+        }
 
-        int resultBoardRowSize = board.getBoardRowSize();
-        int resultBoardColSize = board.getBoardColSize();
-        int expectedBoardRowSize = MAX_ROW.getCount();
-        int expectedBoardColSize = MAX_COL.getCount();
+        static Stream<Arguments> providePiecesAndCounts() {
+            return Stream.of(
+                    Arguments.of(Type.KING, Color.BLACK, MAX_KING.getCount()),
+                    Arguments.of(Type.QUEEN, Color.BLACK, MAX_QUEEN.getCount()),
+                    Arguments.of(Type.ROOK, Color.BLACK, MAX_ROOK.getCount()),
+                    Arguments.of(Type.KNIGHT, Color.BLACK, MAX_KNIGHT.getCount()),
+                    Arguments.of(Type.BISHOP, Color.BLACK, MAX_BISHOP.getCount()),
+                    Arguments.of(Type.PAWN, Color.BLACK, MAX_PAWN.getCount()),
 
-        assertThat(resultBoardRowSize).isEqualTo(expectedBoardRowSize);
-        assertThat(resultBoardColSize).isEqualTo(expectedBoardColSize);
-    }
-
-    @Test
-    @DisplayName("[Success] 초기화한 Board의 기물 위치 확인")
-    void initialBoardStatus() {
-        board.initialize();
-        String result = board.showBoard();
-        String expectedInitialBoardState = """
-                RNBQKBNR
-                PPPPPPPP
-                ........
-                ........
-                ........
-                ........
-                pppppppp
-                rnbqkbnr
-                """;
-        assertThat(result).isEqualTo(expectedInitialBoardState);
-    }
-
-    @ParameterizedTest
-    @MethodSource("providePiecesAndCounts")
-    @DisplayName("[Success] 초기화한 Board의 기물 개수 확인")
-    void initialGetPieceCount(Type type, Color color, int expectedCount) {
-        board.initialize();
-        Piece piece = Piece.createPiece(type, color);
-        int actualCount = board.getPieceCount(piece.getType(), color);
-        assertThat(actualCount).isEqualTo(expectedCount);
-    }
-
-    static Stream<Arguments> providePiecesAndCounts() {
-        return Stream.of(
-                Arguments.of(Type.KING, Color.BLACK, MAX_KING.getCount()),
-                Arguments.of(Type.QUEEN, Color.BLACK, MAX_QUEEN.getCount()),
-                Arguments.of(Type.ROOK, Color.BLACK, MAX_ROOK.getCount()),
-                Arguments.of(Type.KNIGHT, Color.BLACK, MAX_KNIGHT.getCount()),
-                Arguments.of(Type.BISHOP, Color.BLACK, MAX_BISHOP.getCount()),
-                Arguments.of(Type.PAWN, Color.BLACK, MAX_PAWN.getCount()),
-
-                Arguments.of(Type.KING, Color.WHITE, MAX_KING.getCount()),
-                Arguments.of(Type.QUEEN, Color.WHITE, MAX_QUEEN.getCount()),
-                Arguments.of(Type.ROOK, Color.WHITE, MAX_ROOK.getCount()),
-                Arguments.of(Type.KNIGHT, Color.WHITE, MAX_KNIGHT.getCount()),
-                Arguments.of(Type.BISHOP, Color.WHITE, MAX_BISHOP.getCount()),
-                Arguments.of(Type.PAWN, Color.WHITE, MAX_PAWN.getCount())
-        );
+                    Arguments.of(Type.KING, Color.WHITE, MAX_KING.getCount()),
+                    Arguments.of(Type.QUEEN, Color.WHITE, MAX_QUEEN.getCount()),
+                    Arguments.of(Type.ROOK, Color.WHITE, MAX_ROOK.getCount()),
+                    Arguments.of(Type.KNIGHT, Color.WHITE, MAX_KNIGHT.getCount()),
+                    Arguments.of(Type.BISHOP, Color.WHITE, MAX_BISHOP.getCount()),
+                    Arguments.of(Type.PAWN, Color.WHITE, MAX_PAWN.getCount())
+            );
+        }
     }
 
 }
