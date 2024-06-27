3c7ce392f6ea2f08f169d9b5900bdeb26c890dc7
mirageoasis <kimhw0820@naver.com>
2024-06-27 12:53:57 +0900

    feat: 게임 진행 가능하게 Main함수 수정

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,23 +2,51 @@ package org.example;
 
 import java.util.Scanner;
 import org.example.chess.Board;
+import org.example.chess.Position;
+import org.example.utils.Command;
 
 public class Main {
 
     public static void main(String[] args) {
-        while (true) {
+        Board board = null;
+        boolean isGameStarted = false;
+        boolean isGameEnded = false;
+
+        while (!isGameEnded) {
             Scanner scanner = new Scanner(System.in);
             System.out.print("Enter a command: ");
-            String cmd = scanner.nextLine();
+            String[] cmds = scanner.nextLine().split(" ");
 
-            if(cmd.equals("end")) {
-                break;
-            }else if (cmd.equals("start")) {
-                // 게임 시작
-                // 체스판 초기화
-                Board board = new Board();
-            }else{
-                System.out.println("Invalid command");
+            Command command = Command.fromString(cmds[0]);
+
+            switch (command) {
+                case START:
+                    board = new Board();
+                    board.initialize();
+                    isGameStarted = true;
+                    System.out.println("Game started.");
+                    System.out.println(board.showBoard());
+                    break;
+                case MOVE:
+                    if (isGameStarted) {
+                        if (cmds.length != 3) {
+                            System.out.println("Invalid command");
+                            continue;
+                        }
+                        board.move(new Position(cmds[1]), new Position(cmds[2]));
+
+                        System.out.println(board.showBoard());
+                    } else {
+                        System.out.println("Game has not started yet.");
+                    }
+                    break;
+                case END:
+                    isGameEnded = true;
+                    break;
+                case INVALID:
+                default:
+                    System.out.println("Invalid command");
+                    break;
             }
         }
     }
--- src/main/java/org/example/utils/Command.java
+++ src/main/java/org/example/utils/Command.java
@@ -1,2 +1,23 @@
-package org.example.utils;public enum Command {
+package org.example.utils;
+
+public enum Command {
+    START("start"),
+    END("end"),
+    MOVE("move"),
+    INVALID("invalid");
+
+    private final String command;
+
+    Command(String command) {
+        this.command = command;
+    }
+
+    public static Command fromString(String command) {
+        for (Command cmd : Command.values()) {
+            if (cmd.command.equalsIgnoreCase(command)) {
+                return cmd;
+            }
+        }
+        return INVALID;
+    }
 }
