a48ae261d955843f05988f1f4dddc70821a97159
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-06-28 01:28:24 +0900

    [Feat] representation을 이용하여 type을 반환하는 메서드 구현

--- src/main/java/chess/pieces/PieceTypes.java
+++ src/main/java/chess/pieces/PieceTypes.java
@@ -3,41 +3,44 @@ package chess.pieces;
 import java.util.List;
 
 public enum PieceTypes {
-    WHITE_PAWN(Color.WHITE,Type.PAWN,Direction.whitePawnDirection(),false),
-    BLACK_PAWN(Color.BLACK,Type.PAWN,Direction.blackPawnDirection(),false),
-    WHITE_KNIGHT(Color.WHITE,Type.KNIGHT,Direction.knightDirection(),false),
-    BLACK_KNIGHT(Color.BLACK,Type.KNIGHT,Direction.knightDirection(),false),
-    WHITE_ROOK(Color.WHITE,Type.ROOK,Direction.linearDirection(),true),
-    BLACK_ROOK(Color.BLACK,Type.ROOK,Direction.linearDirection(),true),
-    WHITE_BISHOP(Color.WHITE,Type.BISHOP,Direction.diagonalDirection(),true),
-    BLACK_BISHOP(Color.BLACK,Type.BISHOP,Direction.diagonalDirection(),true),
-    WHITE_QUEEN(Color.WHITE,Type.QUEEN,Direction.everyDirection(),true),
-    BLACK_QUEEN(Color.BLACK,Type.QUEEN,Direction.everyDirection(),true),
-    WHITE_KING(Color.WHITE,Type.KING,Direction.everyDirection(),false),
-    BLACK_KING(Color.BLACK,Type.KING,Direction.everyDirection(),false),
-    NO_PIECE(Color.NOCOLOR,Type.NO_PIECE,Direction.noDirection(),false);
-    public enum Color{
-        WHITE,BLACK,NOCOLOR;
+    WHITE_PAWN(Color.WHITE, Type.PAWN, Direction.whitePawnDirection(), false),
+    BLACK_PAWN(Color.BLACK, Type.PAWN, Direction.blackPawnDirection(), false),
+    WHITE_KNIGHT(Color.WHITE, Type.KNIGHT, Direction.knightDirection(), false),
+    BLACK_KNIGHT(Color.BLACK, Type.KNIGHT, Direction.knightDirection(), false),
+    WHITE_ROOK(Color.WHITE, Type.ROOK, Direction.linearDirection(), true),
+    BLACK_ROOK(Color.BLACK, Type.ROOK, Direction.linearDirection(), true),
+    WHITE_BISHOP(Color.WHITE, Type.BISHOP, Direction.diagonalDirection(), true),
+    BLACK_BISHOP(Color.BLACK, Type.BISHOP, Direction.diagonalDirection(), true),
+    WHITE_QUEEN(Color.WHITE, Type.QUEEN, Direction.everyDirection(), true),
+    BLACK_QUEEN(Color.BLACK, Type.QUEEN, Direction.everyDirection(), true),
+    WHITE_KING(Color.WHITE, Type.KING, Direction.everyDirection(), false),
+    BLACK_KING(Color.BLACK, Type.KING, Direction.everyDirection(), false),
+    NO_PIECE(Color.NOCOLOR, Type.NO_PIECE, Direction.noDirection(), false);
+
+    public enum Color {
+        WHITE, BLACK, NOCOLOR;
     }
-    public enum Type{
-        PAWN('p',1.0),
-        ROOK('r',5.0),
-        KNIGHT('n',2.5),
-        BISHOP('b',3.0),
-        QUEEN('q',9.0),
-        KING('k',0.0)
-        ,NO_PIECE('.',0.0);
+
+    public enum Type {
+        PAWN('p', 1.0),
+        ROOK('r', 5.0),
+        KNIGHT('n', 2.5),
+        BISHOP('b', 3.0),
+        QUEEN('q', 9.0),
+        KING('k', 0.0), NO_PIECE('.', 0.0);
         private final char representation;
         private final double point;
-        Type(char representation,double point){
+
+        Type(char representation, double point) {
             this.representation = representation;
             this.point = point;
         }
 
-        public char getRepresentation(){
+        public char getRepresentation() {
             return this.representation;
         }
-        public double getPoint(){
+
+        public double getPoint() {
             return this.point;
         }
     }
@@ -48,16 +51,25 @@ public enum PieceTypes {
     private final char representation;
     private final Course course;
 
-    PieceTypes(Color color, Type type,List<Direction> direction,boolean recursive) {
+    PieceTypes(Color color, Type type, List<Direction> direction, boolean recursive) {
         this.color = color;
         this.type = type;
         char representation = type.getRepresentation();
         this.representation = color.equals(Color.WHITE) ? representation : Character.toUpperCase(representation);
-        this.course = new Course(direction,recursive);
+        this.course = new Course(direction, recursive);
     }
 
     public Color getColor() {
         return color;
+    }
+
+    public PieceTypes fromRepresentation(char representation) {
+        for (PieceTypes pieceTypes : values()) {
+            if (pieceTypes.getRepresentation() == representation) {
+                return pieceTypes;
+            }
+        }
+        return null;
     }
 
     public Type getType() {
