dc0add49d29d05344ca7654f6f0a3401a713d1cb
donghar <donghar@naver.com>
2024-06-27 23:21:08 +0900

    [Test] 체스 프로그램 점수 계산하기
    테스트 코드 작성

--- src/test/java/pe/goblin/chess/board/BoardTest.java
+++ src/test/java/pe/goblin/chess/board/BoardTest.java
@@ -55,4 +55,28 @@ public class BoardTest {
         assertEquals(piece, board.findPiece(position));
         System.out.println(board.showBoard());
     }
+
+    @Test
+    public void calculatePoint() throws Exception {
+        board.initializeEmpty();
+
+        addPiece("b6", Piece.createBlackPawn());
+        addPiece("e6", Piece.createBlackQueen());
+        addPiece("b8", Piece.createBlackKing());
+        addPiece("c8", Piece.createBlackRook());
+
+        addPiece("f2", Piece.createWhitePawn());
+        addPiece("g2", Piece.createWhitePawn());
+        addPiece("e1", Piece.createWhiteRook());
+        addPiece("f1", Piece.createWhiteKing());
+
+        assertEquals(15.0, board.calculatePoint(Piece.Color.BLACK), 0.01);
+        assertEquals(7.0, board.calculatePoint(Piece.Color.WHITE), 0.01);
+
+        System.out.println(board.showBoard());
+    }
+
+    private void addPiece(String position, Piece piece) {
+        board.move(position, piece);
+    }
 }
