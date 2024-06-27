0c369d38e7e847a651d0940b558433f7cf94abbe
huiseung <whtjddn75@gmail.com>
2024-06-27 15:32:35 +0900

    [refactoing] debug 편의를 위해 Position toString 재정의

--- chess/src/main/java/chess/Position.java
+++ chess/src/main/java/chess/Position.java
@@ -56,9 +56,6 @@ public class Position {
 
     @Override
     public String toString() {
-        return "Position{" +
-                "x=" + x +
-                ", y=" + y +
-                '}';
+        return "Position(" + (char) (x + 'a') + (8-y) + ")";
     }
 }
