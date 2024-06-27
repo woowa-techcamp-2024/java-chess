f4b69f2073b777fe9b8073565b22048da21a393e
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-06-27 09:02:33 +0900

    [Fix] Direction 방향을 내 코드에 맞게 수정

--- src/main/java/chess/pieces/Direction.java
+++ src/main/java/chess/pieces/Direction.java
@@ -5,23 +5,23 @@ import java.util.Collections;
 import java.util.List;
 
 public enum Direction {
-    NORTH(0, 1),
-    NORTHEAST(1, 1),
-    EAST(1, 0),
-    SOUTHEAST(1, -1),
-    SOUTH(0, -1),
-    SOUTHWEST(-1, -1),
-    WEST(-1, 0),
-    NORTHWEST(-1, 1),
+    NORTH(-1, 0),
+    NORTHEAST(-1, 1),
+    EAST(0, 1),
+    SOUTHEAST(1, 1),
+    SOUTH(1, 0),
+    SOUTHWEST(1, -1),
+    WEST(0, -1),
+    NORTHWEST(-1, -1),
 
-    NNE(1, 2),
-    NNW(-1, 2),
-    SSE(1, -2),
-    SSW(-1, -2),
-    EEN(2, 1),
-    EES(2, -1),
-    WWN(-2, 1),
-    WWS(-2, -1);
+    NNE(-2, 1),
+    NNW(-2, -1),
+    SSE(2, 1),
+    SSW(2, -1),
+    EEN(-1, 2),
+    EES(1, 2),
+    WWN(-1, -2),
+    WWS(1, -2);
 
     private int xDegree;
     private int yDegree;
@@ -55,11 +55,11 @@ public enum Direction {
         return Arrays.asList(NNE, NNW, SSE, SSW, EEN, EES, WWN, WWS);
     }
 
-    public static List<Direction> whitePawnDirection() {
+    public static List<Direction> blackPawnDirection() {
         return Arrays.asList(NORTH, NORTHEAST, NORTHWEST);
     }
 
-    public static List<Direction> blackPawnDirection() {
+    public static List<Direction> whitePawnDirection() {
         return Arrays.asList(SOUTH, SOUTHEAST, SOUTHWEST);
     }
 
