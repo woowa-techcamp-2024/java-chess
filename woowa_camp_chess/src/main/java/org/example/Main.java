280097067cd534375bd342ce45bd3549b0eafe1d
KoKimSS <seungsu8848@ajou.ac.kr>
2024-06-27 11:01:49 +0900

    feat : 콘솔 화면 move 명령 추가

--- woowa_camp_chess/src/main/java/org/example/Main.java
+++ woowa_camp_chess/src/main/java/org/example/Main.java
@@ -12,16 +12,26 @@ public class Main {
         System.out.println("체스 게임에 오신것을 환영합니다.");
         while (true){
             System.out.println("원하시는 값을 입력하여 주시기 바랍니다.");
-            System.out.println("시작 : start, 종료 : end");
+            System.out.println("시작 : start, 종료 : end, 말 이동 : move");
             String input = scan.next();
+            switch (input){
+                case "start" :
+                    // 전역 변수에 새로운 Board 객체를 할당함
+                    board = new Board();
+                    board.initialize();
+                    break;
 
-            if(input.equals("start")){
-                // 전역 변수에 새로운 Board 객체를 할당함
-                board = new Board();
-                board.initialize();
+                case "move" :
+                    System.out.print("선택 포지션 : ");
+                    String srcPosition = scan.next();
+                    System.out.print("이동 포지션 : ");
+                    String desPosition = scan.next();
+                    board.move(srcPosition,desPosition);
+                    System.out.println(board.showBoard());
+                    break;
+
+                case "end" : return;
             }
-
-            if(input.equals("end")) break;
         }
 
     }
