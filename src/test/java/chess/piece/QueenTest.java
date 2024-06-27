0f36d1ee4ea095b41a2bd9be52cd9216791e4001
HyeonJun0530 <hyjn053@gmail.com>
2024-06-27 15:28:25 +0900

    test: Piece Type에 따른 테스트 추가
    - 각 기물 테스트 추가
    - 기물에 따른 이동 관련 테스트 추가

---
+++ src/test/java/chess/piece/QueenTest.java
@@ -0,0 +1,34 @@
+package chess.piece;
+
+import chess.piece.rule.Direction;
+import chess.piece.rule.PieceMove;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static chess.piece.PieceTest.verifyPiece;
+import static org.junit.jupiter.api.Assertions.*;
+
+class QueenTest {
+
+    @Test
+    @DisplayName("퀸 기물이 정상적으로 생성된다.")
+    public void create_piece() {
+        verifyPiece(Queen.create(PieceColor.WHITE), Queen.create(PieceColor.BLACK), Type.QUEEN);
+    }
+
+    @Test
+    @DisplayName("기물이 기물 이동 방향과 이동 거리를 가진다.")
+    public void move_piece() {
+        PieceMove whiteMove =Queen.create(PieceColor.WHITE).getMoveable();
+        PieceMove blackMove = Queen.create(PieceColor.BLACK).getMoveable();
+
+        assertAll(
+                () -> assertEquals(whiteMove.directions(), blackMove.directions()),
+                () -> assertEquals(whiteMove.distance(), blackMove.distance()),
+                () -> assertTrue(whiteMove.directions().containsAll(Direction.diagonalDirection())),
+                () -> assertTrue(whiteMove.directions().containsAll(Direction.linearDirection())),
+                () -> assertEquals(whiteMove.distance(), 8)
+        );
+    }
+
+}
