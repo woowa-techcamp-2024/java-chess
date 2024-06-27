0b393fcbf910152aa3f3c8b1da2de2482d464f17
hek-git <ghddmsrl1@naver.com>
2024-06-27 10:35:30 +0900

    refactor: Piece에 대한 색과 기물에 따라 분리된 팩토리 메소드에서 enum 사용 - 팩토리 메소드에서 Piece 생성시에 추가한 ENUM 타입을 기반으로 색과 Type을 경정한다. - Piece를 사용하는 Board, Board Test 클래스 또한 리팩토링하였다.

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -30,23 +30,23 @@ public class Board {
             }
         }
 
-        MAP[0][0] = Piece.BLACK_ROOK_REPRESENTATION;
-        MAP[0][1] = Piece.BLACK_KNIGHT_REPRESENTATION;
-        MAP[0][2] = Piece.BLACK_BISHOP_REPRESENTATION;
-        MAP[0][3] = Piece.BLACK_QUEEN_REPRESENTATION;
-        MAP[0][4] = Piece.BLACK_KING_REPRESENTATION;
-        MAP[0][5] = Piece.BLACK_BISHOP_REPRESENTATION;
-        MAP[0][6] = Piece.BLACK_KNIGHT_REPRESENTATION;
-        MAP[0][7] = Piece.BLACK_ROOK_REPRESENTATION;
+        MAP[0][0] = Piece.Type.ROOK.getBlackRepresentation();
+        MAP[0][1] = Piece.Type.KNIGHT.getBlackRepresentation();
+        MAP[0][2] = Piece.Type.BISHOP.getBlackRepresentation();
+        MAP[0][3] = Piece.Type.QUEEN.getBlackRepresentation();
+        MAP[0][4] = Piece.Type.KING.getBlackRepresentation();
+        MAP[0][5] = Piece.Type.BISHOP.getBlackRepresentation();
+        MAP[0][6] = Piece.Type.KNIGHT.getBlackRepresentation();
+        MAP[0][7] = Piece.Type.ROOK.getBlackRepresentation();
 
-        MAP[7][0] = Piece.WHITE_ROOK_REPRESENTATION;
-        MAP[7][1] = Piece.WHITE_KNIGHT_REPRESENTATION;
-        MAP[7][2] = Piece.WHITE_BISHOP_REPRESENTATION;
-        MAP[7][3] = Piece.WHITE_QUEEN_REPRESENTATION;
-        MAP[7][4] = Piece.WHITE_KING_REPRESENTATION;
-        MAP[7][5] = Piece.WHITE_BISHOP_REPRESENTATION;
-        MAP[7][6] = Piece.WHITE_KNIGHT_REPRESENTATION;
-        MAP[7][7] = Piece.WHITE_ROOK_REPRESENTATION;
+        MAP[7][0] = Piece.Type.ROOK.getWhiteRepresentation();
+        MAP[7][1] = Piece.Type.KNIGHT.getWhiteRepresentation();
+        MAP[7][2] = Piece.Type.BISHOP.getWhiteRepresentation();
+        MAP[7][3] = Piece.Type.QUEEN.getWhiteRepresentation();
+        MAP[7][4] = Piece.Type.KING.getWhiteRepresentation();
+        MAP[7][5] = Piece.Type.BISHOP.getWhiteRepresentation();
+        MAP[7][6] = Piece.Type.KNIGHT.getWhiteRepresentation();
+        MAP[7][7] = Piece.Type.ROOK.getWhiteRepresentation();
 
         initBlackPawn();
         initWhitePawn();
@@ -70,13 +70,13 @@ public class Board {
 
     private void initBlackPawn() {
         for (int i = 0; i < BOARD_SIZE; i++) {
-            MAP[BLACK_PAWN_RANK][i] = Piece.BLACK_PAWN_REPRESENTATION;
+            MAP[BLACK_PAWN_RANK][i] = Piece.Type.PAWN.getBlackRepresentation();
         }
     }
 
     private void initWhitePawn() {
         for (int i = 0; i < BOARD_SIZE; i++) {
-            MAP[WHITE_PAWN_RANK][i] = Piece.WHITE_PAWN_REPRESENTATION;
+            MAP[WHITE_PAWN_RANK][i] = Piece.Type.PAWN.getWhiteRepresentation();
         }
     }
 
