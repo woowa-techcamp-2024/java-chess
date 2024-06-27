5ec46623411a2fa65c36945df2d450ab10cd5900
lass9436 <lass9436@gmail.com>
2024-06-27 11:28:15 +0900

    [refactor] Rank 클래스 분리
    Board 클래스에서 내부 클래스로 Rank 클래스를 가지고 있었습니다.
    요구사항 구현으로 Board 자체의 복잡도가 증가함에 따라 가독성을 해치는 문제가 있었습니다.
    따라서 Rank 클래스를 별도의 파일로 분리했습니다.

--- src/test/java/chess/Board.java
+++ src/test/java/chess/Board.java
@@ -11,55 +11,6 @@ public class Board {
     private final List<Rank> map = new ArrayList<>();
 
 
-    public static class Rank {
-
-        private final List<Piece> pieces = new ArrayList<>();
-
-        public enum Type { WHITE_PAWN, BLACK_PAWN, WHITE_ROOK_TO_KING, BLACK_ROOK_TO_KING, NO_PIECE;}
-        public List<Piece> getPieces() {
-            return pieces;
-        }
-
-        public Rank(Type type) {
-            if (type == Type.WHITE_PAWN) {
-                for (int i = 0; i < 8; i++) {
-                    pieces.add(Piece.createWhitePawn());
-                }
-            }
-            else if (type == Type.BLACK_PAWN) {
-                for (int i = 0; i < 8; i++) {
-                    pieces.add(Piece.createBlackPawn());
-                }
-            }
-            else if (type == Type.WHITE_ROOK_TO_KING) {
-                pieces.add(Piece.createWhiteRook());
-                pieces.add(Piece.createWhiteKnight());
-                pieces.add(Piece.createWhiteBishop());
-                pieces.add(Piece.createWhiteQueen());
-                pieces.add(Piece.createWhiteKing());
-                pieces.add(Piece.createWhiteBishop());
-                pieces.add(Piece.createWhiteKnight());
-                pieces.add(Piece.createWhiteRook());
-            }
-            else if (type == Type.BLACK_ROOK_TO_KING) {
-                pieces.add(Piece.createBlackRook());
-                pieces.add(Piece.createBlackKnight());
-                pieces.add(Piece.createBlackBishop());
-                pieces.add(Piece.createBlackQueen());
-                pieces.add(Piece.createBlackKing());
-                pieces.add(Piece.createBlackBishop());
-                pieces.add(Piece.createBlackKnight());
-                pieces.add(Piece.createBlackRook());
-            }
-            else if (type == Type.NO_PIECE) {
-                for (int i = 0; i < 8; i++) {
-                    pieces.add(Piece.createBlank());
-                }
-            }
-        }
-
-
-    }
     public Board() {}
     public void initializeEmpty() {
         map.add(new Rank(Rank.Type.NO_PIECE));
