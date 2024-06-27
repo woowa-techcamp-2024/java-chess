83bcccc7d32ce9d228328a8b9cab4b0b80104daf
HiiWee <wpdlzhf159@gmail.com>
2024-06-27 14:04:45 +0900

    feat: ChessApplication에 이동 명령 추가
    - 추가 정보를 위해 이동 명령 전후로 체스판 출력

--- src/main/java/com/wootecam/chess/ChessApplication.java
+++ src/main/java/com/wootecam/chess/ChessApplication.java
@@ -5,16 +5,34 @@ import com.wootecam.chess.pieces.Rank;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class ChessApplication {
 
-
     private static final String START_COMMAND = "start";
     private static final String END_COMMAND = "end";
+    private static final String MOVE_COMMAND = "move";
 
     public static void main(String[] args) {
         Scanner inputReader = new Scanner(System.in);
+        Board board = initialize();
 
+        printStartMessage();
+        while (isContinue(inputReader.nextLine())) {
+            board.print();
+            System.out.println("명령을 입력하세요.");
+            String command = inputReader.nextLine();
+
+            if (command.startsWith(MOVE_COMMAND)) {
+                moveCommand(command, board);
+            }
+
+            board.print();
+            printStartMessage();
+        }
+    }
+
+    private static Board initialize() {
         List<Rank> ranks = new ArrayList<>();
         ranks.add(Rank.createBlackOtherPieces());
         ranks.add(Rank.createPawns(Color.BLACK));
@@ -26,11 +44,11 @@ public class ChessApplication {
         ranks.add(Rank.createWhiteOtherPieces());
         CoordinatesExtractor extractor = new CoordinatesExtractor();
 
-        Board board = new Board(ranks, extractor);
+        return new Board(ranks, extractor);
+    }
 
-        while (isContinue(inputReader.nextLine())) {
-            board.print();
-        }
+    private static void printStartMessage() {
+        System.out.println("시작하려면 start를 종료하려면 end를 입력하세요.");
     }
 
     private static boolean isContinue(String input) {
@@ -42,5 +60,13 @@ public class ChessApplication {
         }
         String message = String.format("잘못된 입력입니다. ('%s', '%s'만 가능) input = %s", START_COMMAND, END_COMMAND, input);
         throw new IllegalArgumentException(message);
+    }
+
+    private static void moveCommand(final String command, final Board board) {
+        StringTokenizer moveToken = new StringTokenizer(command.substring(MOVE_COMMAND.length()));
+        String startCoordinates = moveToken.nextToken();
+        String targetCoordinates = moveToken.nextToken();
+
+        board.move(startCoordinates, targetCoordinates);
     }
 }
