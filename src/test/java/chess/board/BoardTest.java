e3aa449c85738fc36832c06dcff7e4ff5ba0f4aa
hek-git <ghddmsrl1@naver.com>
2024-06-27 16:41:43 +0900

    test: 보드판에 남아있는 기물들을 색과 점수에 따라 정렬하는 테스트 메서드 추가 - 보드판에 남아있는 기물들을 색과 점수를 주어 정렬을 검증하는 sortPieces 메서드를 추가합니다.

--- src/test/java/chess/board/BoardTest.java
+++ src/test/java/chess/board/BoardTest.java
@@ -105,6 +105,31 @@ public class BoardTest {
         System.out.println(board.showBoard());
     }
 
+    @Test
+    @DisplayName("현재 남아있는 기물들을 색과 점수에 따라 정렬합니다.")
+    public void sortPieces() throws Exception {
+        board.initializeEmpty();
+
+        addPiece("b6", Piece.createBlackPawn());
+        addPiece("e6", Piece.createBlackQueen());
+        addPiece("b8", Piece.createBlackKing());
+        addPiece("c8", Piece.createBlackRook());
+
+        addPiece("f2", Piece.createWhitePawn());
+        addPiece("h3", Piece.createWhitePawn());
+        addPiece("f3", Piece.createWhitePawn());
+        addPiece("g4", Piece.createWhiteQueen());
+        addPiece("f4", Piece.createWhiteKnight());
+        addPiece("g2", Piece.createWhitePawn());
+        addPiece("e1", Piece.createWhiteRook());
+        addPiece("f1", Piece.createWhiteKing());
+
+        assertEquals("KPRQ", new Rank(board.sortPiece(Color.BLACK, Order.ASC)).toString());
+        assertEquals("kppppnrq", new Rank(board.sortPiece(Color.WHITE, Order.ASC)).toString());
+
+        System.out.println(board.showBoard());
+    }
+
     private void addPiece(String position, Piece piece) {
         board.move(position, piece);
     }
