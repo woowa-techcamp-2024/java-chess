49be1d5e35e8c10440a312049046ae88a896b182
HiiWee <wpdlzhf159@gmail.com>
2024-06-28 10:16:51 +0900

    refactor: 상수 분리 및 변수명 재정의
    - 폰이 이동할 수 있는 최대 거리에 대한 상수
    - 시작하는 piece의 이름을 명확하게 변경

--- src/main/java/com/wootecam/chess/PieceMoveVerifier.java
+++ src/main/java/com/wootecam/chess/PieceMoveVerifier.java
@@ -9,36 +9,39 @@ import java.util.stream.IntStream;
 
 public class PieceMoveVerifier {
 
-    public void verifyMove(final Piece piece,
+    public static final int PAWN_FIRST_MOVE_MAX_AMOUNT = 2;
+
+    public void verifyMove(final Piece startPiece,
                            final Piece targetPiece,
                            final Position startPosition,
                            final Position targetPosition,
                            final Function<Position, Piece> pieceFinder) {
-        Direction direction = piece.findDirection(startPosition, targetPosition)
+        Direction direction = startPiece.findDirection(startPosition, targetPosition)
                 .orElseThrow(() -> new IllegalArgumentException("현재 기물에 대한 알맞은 방향을 찾을 수 없습니다."));
 
-        if (piece.isTypeOf(Type.PAWN)) {
-            verifyPawnMove(startPosition, targetPosition, piece, targetPiece, direction, pieceFinder);
+        if (startPiece.isTypeOf(Type.PAWN)) {
+            verifyPawnMove(startPosition, targetPosition, startPiece, targetPiece, direction, pieceFinder);
         }
-        if (piece.isTypeOf(Type.KNIGHT)) {
-            verifyKnight(piece, targetPiece);
+        if (startPiece.isTypeOf(Type.KNIGHT)) {
+            verifyKnight(startPiece, targetPiece);
         }
-        verifyOtherMove(startPosition, targetPosition, piece, targetPiece, direction, pieceFinder);
+        verifyOtherMove(startPosition, targetPosition, startPiece, targetPiece, direction, pieceFinder);
     }
 
     private void verifyPawnMove(final Position startPosition,
                                 final Position targetPosition,
-                                final Piece piece,
+                                final Piece startPiece,
                                 final Piece targetPiece,
                                 final Direction direction,
                                 final Function<Position, Piece> pieceFinder) {
-        validateSameTeam(piece, targetPiece);
+        validateSameTeam(startPiece, targetPiece);
         if (direction.isPawnAttackDirection() && targetPiece.isBlank()) {
             throw new IllegalArgumentException("폰은 빈칸을 공격할 수 없습니다.");
         }
-        if (direction.isPawnMoveDirection() && startPosition.calculateDistanceWith(targetPosition) == 2
-                && isBlockWithOtherPiece(startPosition, direction,
-                startPosition.calculateDistanceWith(targetPosition), pieceFinder)) {
+        if (direction.isPawnMoveDirection()
+                && startPosition.calculateDistanceWith(targetPosition) == PAWN_FIRST_MOVE_MAX_AMOUNT
+                && isBlockWithOtherPiece(startPosition, direction, startPosition.calculateDistanceWith(targetPosition),
+                pieceFinder)) {
             throw new IllegalArgumentException("폰이 2칸 이동할때 방문하는 칸에 기물이 있으면 안됩니다.");
         }
         if (direction.isPawnMoveDirection() && isBlockWithOtherPiece(startPosition, direction,
@@ -47,32 +50,32 @@ public class PieceMoveVerifier {
         }
     }
 
-    private void verifyKnight(final Piece piece, final Piece targetPiece) {
-        validateSameTeam(piece, targetPiece);
+    private void verifyKnight(final Piece startPiece, final Piece targetPiece) {
+        validateSameTeam(startPiece, targetPiece);
     }
 
     private void verifyOtherMove(final Position startPosition,
                                  final Position targetPosition,
-                                 final Piece piece,
+                                 final Piece startPiece,
                                  final Piece targetPiece,
                                  final Direction direction,
                                  final Function<Position, Piece> pieceFinder) {
         int moveCountExceptLastPiece = startPosition.calculateDistanceWith(targetPosition) - 1;
 
-        validateSameTeam(piece, targetPiece);
+        validateSameTeam(startPiece, targetPiece);
         if (isBlockWithOtherPiece(startPosition, direction, moveCountExceptLastPiece, pieceFinder)) {
             throw new IllegalArgumentException("이동하는 중간에 다른 기물이 있으면 안됩니다.");
         }
     }
 
-    private void validateSameTeam(final Piece piece, final Piece targetPiece) {
-        if (isSameTeam(piece, targetPiece)) {
+    private void validateSameTeam(final Piece startPiece, final Piece targetPiece) {
+        if (isSameTeam(startPiece, targetPiece)) {
             throw new IllegalArgumentException("같은 팀의 기물이 있는곳으로 이동할 수 없습니다.");
         }
     }
 
-    private boolean isSameTeam(final Piece piece, final Piece targetPiece) {
-        return targetPiece.getColor() == piece.getColor();
+    private boolean isSameTeam(final Piece startPiece, final Piece targetPiece) {
+        return targetPiece.getColor() == startPiece.getColor();
     }
 
     private boolean isBlockWithOtherPiece(final Position startPosition,
